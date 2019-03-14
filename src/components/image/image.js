2abc4935e9d3f67a0198dd1f3cd82ab60dc52c7c
Glitch (community) <none>
2019-03-14 17:32:33 +0000

    ✊🌁 Checkpoint ./src/components/image/image.js:617503/396

--- src/components/image/image.js
+++ src/components/image/image.js
@@ -5,10 +5,20 @@ import styles from './image.styl';
 
 const cx = classNames.bind(styles);
 
-const Image = ({ src, className, alt, role, ...otherProps }) => {
+/*
+If we have a src set - figure out how big the screen is and apply the correct image
+If we don't, just display the image with width/height props
+* srcSet = [
+  'large' => ['width' => '100%', src='']
+]
+*/
+
+
+const Image = ({ src, className, alt, role, width, height }) => {
   let classes = cx({
     [className]: className !== undefined,
-    'image': true
+    'width': width,
+    'height': height
   });
   // If no alt, we assume this is just for visuals
   role = !role && alt === '' ? 'presentation' : '';
@@ -27,8 +37,8 @@ Image.propTypes = {
 Image.defaultProps = {
   role: '',
   srcSet: [],
-  width: undefined,
-  height: undefined,
+  width: '100%',
+  height: '100%'
 };
 
 export default Image;
