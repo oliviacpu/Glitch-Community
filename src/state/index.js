8f6272e4389d8dbfc26b4a065899daa9718f6919
Glitch (community) <none>
2019-03-18 21:21:04 +0000

    😅🐄 Checkpoint ./src/state/index.js:271885/2310 ./package.json:271885/6547

--- src/state/index.js
+++ src/state/index.js
@@ -1,2 +1,36 @@
-import { configureStore } from 'redux-starter-kit'
+import { configureStore, createSlice, getDefaultMiddleware } from 'redux-starter-kit';
+import { before }
 
+const currentUser = createSlice({
+  initialState: {
+    id: 0,
+    login: null,
+    name: null,
+    description: '',
+    color: '#aaa',
+    avatarUrl: null,
+    avatarThumbnailUrl: null,
+    hasCoverImage: false,
+    coverColor: null,
+    emails: [],
+    features: [],
+    projects: [],
+    teams: [],
+    collections: [],
+  },
+  reducers: {},
+});
+
+const sentryMiddleware = (store) => (next) => (action) => {
+
+}
+
+const store = configureStore({
+  reducers: {
+    currentUser,
+  },
+  middleware: [...getDefaultMiddleware()],
+  devTools: true,
+});
+
+export default store;
