39dc86faca52cddeeba85ce4ae927135290c1826
Glitch (community) <none>
2019-03-19 14:04:57 +0000

    😼🏜 Checkpoint ./src/state/context.js:271885/4837 ./package.json:271885/29

--- src/state/context.js
+++ src/state/context.js
@@ -1,19 +1,27 @@
-import React, { useContext, createContext, useState } from 'react'
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { bindActionCreators } from 'redux';
 
-const ReduxContext = createContext()
+export const ReduxContext = createContext();
 
-export const Provider = ({ store }) => 
+export const Provider = ({ store, children }) => <ReduxContext.Provider value={store}>{children}</ReduxContext.Provider>;
 
-export const useReduxStore = () => useContext(ReduxContext)
+export const useReduxStore = () => useContext(ReduxContext);
 
-export function useReduxSelector (selector) {
-  const store = useReduxStore()
-  const [state, setState] = useState(() => selector(store.getState()))
+export function useReduxSelector(selector) {
+  const store = useReduxStore();
+  const [state, setState] = useState(() => selector(store.getState()));
   useEffect(() => {
     return store.subscribe(() => {
-      const nextState = selector(store.getState())
-      if (nextState !== state) { setState(nextState) }
-    })
-  }, [store])
-  return state
+      const nextState = selector(store.getState());
+      if (nextState !== state) {
+        setState(nextState);
+      }
+    });
+  }, [store]);
+  return state;
+}
+
+export function useActions(actionCreators) {
+  const { dispatch } = useReduxStore();
+  return bindActionCreators(actionCreators, dispatch);
 }
