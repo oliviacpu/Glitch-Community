12c70130eec73d4794376ec43b79ca3ab8dd761a
Glitch (community) <none>
2019-03-19 23:04:54 +0000

    🚡🎨 Checkpoint ./src/components/image/image.js:617503/9078

--- src/components/image/image.js
+++ src/components/image/image.js
@@ -6,40 +6,62 @@ import styles from './image.styl';
 const cx = classNames.bind(styles);
 
 /**
-  * 🖼️ Image Component
-  *
-  * @param {string} src - Image source
-  * @param {object} srcSet - Responsive image source set
-  * @param {alt} alt - Alternative text
-  * @param {number} width - Image width
-  * @param {number} height - Image height
-  * @param {string} role - Image role (typically presentation)
-  * @param {function} onClick - action if the image has one
-  * @param {object} extraClassNames - extra classes to be passed down  to the component
-  * @param {boolean} backgroundImage - If we want the image to be rendered as a background image
-*/
-const imageTag = () => {}
-const backgroundTag = () => {}
+ * 🖼️ Image Component
+ *
+ * @param {string} src - Image source
+ * @param {object} srcSet - Responsive image source set
+ * @param {alt} alt - Alternative text
+ * @param {number} width - Image width
+ * @param {number} height - Image height
+ * @param {string} role - Image role (typically presentation)
+ * @param {function} onClick - action if the image has one
+ * @param {object} className - extra classes to be passed down  to the component
+ * @param {boolean} backgroundImage - If we want the image to be rendered as a background image
+ */
 
-const Image = ({ src, className, alt, role, width, height }) => {
+const Image = ({ src, srcSet, className, alt, role, width, onClick, onKeyUp, height }) => {
   const classes = cx({
-    [className]: className !== undefined,
+    classNames,
   });
-  return <img src={src} width={width} height={height} className={classes} alt={alt} role={role} otherProps />;
+  return (
+    <img
+      src={src}
+      srcSet={srcSet.length ? srcSet : undefined}
+      width={width || undefined}
+      height={height || undefined}
+      className={classes}
+      alt={alt}
+      role={role}
+      onClick={onClick || false}
+      onKeyUp={onKeyUp || false}
+    />
+  );
 };
 
 Image.propTypes = {
   alt: PropTypes.string.isRequired,
+  srcSet: PropTypes.instanceOf(Object),
   role: PropTypes.string,
   src: PropTypes.string.isRequired,
   width: PropTypes.number,
   height: PropTypes.number,
+  backgroundImage: PropTypes.boo,
+  backgroundRatio: PropTypes.number,
+  onClick: PropTypes.func,
+  extraClassNames: PropTypes.instanceOf(Object)
 };
 
 Image.defaultProps = {
+  src: '',
+  srcSet: {},
+  sizes: '',
+  alt: '',
+  backgroundImage: false,
+  backgroundRatio: 50,
   role: '',
-  width: '100%',
-  height: '100%',
+  width: '',
+  height: '',
+  extraClassNames: {}
 };
 
 export default Image;
