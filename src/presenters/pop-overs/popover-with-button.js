44c30715bb53da8a19a2944f2231b19253efb25f
Glitch (profuse-tea) <none>
2019-04-15 17:52:03 +0000

    🍎🌽 Checkpoint ./src/presenters/pop-overs/popover-container.js:97325/402 ./src/presenters/pop-overs/popover-with-button.js:97325/96

--- src/presenters/pop-overs/popover-with-button.js
+++ src/presenters/pop-overs/popover-with-button.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import PopoverContainer from './popover-container';
 
-const PopoverWithButton = ({ dataTrack, containerClass, buttonClass, buttonText, children: renderChildren }) => (
-  <PopoverContainer>
+const PopoverWithButton = ({ dataTrack, containerClass, buttonClass, buttonText, children: renderChildren, onShow }) => (
+  <PopoverContainer onShow={onShow}>
     {({ visible, togglePopover }) => (
       <div className={`button-wrap ${containerClass}`}>
         <button className={buttonClass} data-track={dataTrack} onClick={togglePopover} type="button">
@@ -21,12 +21,14 @@ PopoverWithButton.propTypes = {
   dataTrack: PropTypes.string,
   buttonText: PropTypes.node.isRequired,
   children: PropTypes.func.isRequired,
+  onShow: PropTypes.func,
 };
 
 PopoverWithButton.defaultProps = {
   buttonClass: '',
   containerClass: '',
   dataTrack: '',
+  onShow: null,
 };
 
 export default PopoverWithButton;
