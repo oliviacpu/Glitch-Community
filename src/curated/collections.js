fe3c0a56059bd300afc1f6400be7887310fc7bd0
Glitch (community) <none>
2019-04-17 19:42:29 +0000

    more unused styles and put collections as they should be

--- src/curated/collections.js
+++ src/curated/collections.js
@@ -1,10 +1,11 @@
+
 // Collections shown on the homepage and in the more ideas area
 
 // What collections to show in the expanded area on the homepage
 export const featuredCollections = [
-  { owner: 'glitch', name: 'sketch-with-p-5' },
-  { owner: 'glitch', name: 'code-with-comics' },
-  { owner: 'glitch', name: 'draw-with-music' },
+  { owner: 'glitch', name: 'glitch-this-week-april-17-2019' },
+  { owner: 'glitch', name: 'digital-deliciousness' },
+  { owner: 'glitch', name: 'apps-for-writers' }
 ];
 
 // More ideas is populated from this team
