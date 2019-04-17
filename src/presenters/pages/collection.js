57115891443923544d141640c8cb912e1702f860
Glitch (community) <none>
2019-04-17 19:39:53 +0000

    🏣👯 Checkpoint ./src/presenters/pages/collection.js:433767/6 ./src/presenters/featured-collections.js:433767/300

--- src/presenters/pages/collection.js
+++ src/presenters/pages/collection.js
@@ -5,7 +5,7 @@ import { Redirect } from 'react-router-dom';
 
 import Helmet from 'react-helmet';
 import Text from 'Components/text/text';
-import ProjectItem from 'Components/project/project-item';
+// import ProjectItem from 'Components/project/project-item';
 import Image from 'Components/images/image';
 import NotFound from 'Components/errors/not-found';
 import { ProfileItem } from 'Components/profile/profile-list';
@@ -30,7 +30,7 @@ import { useAPI } from '../../state/api';
 import { useCurrentUser } from '../../state/current-user';
 
 import MoreCollectionsContainer from '../more-collections';
-import Note from '../note';
+// import Note from '../note';
 
 import { getSingleItem, getAllPages } from '../../../shared/api';
 
