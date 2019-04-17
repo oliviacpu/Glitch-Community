5ae725ba16772f16e054daebb106d2acb7c13a72
Glitch (community-staging) <none>
2019-04-17 19:53:23 +0000

    🐬🌦 Checkpoint ./src/components/collection/collection-item.js:867163/443 ./src/components/collection/collection-item.styl:867163/87

--- src/components/collection/collection-item.js
+++ src/components/collection/collection-item.js
@@ -6,6 +6,7 @@ import Markdown from 'Components/text/markdown';
 import Button from 'Components/buttons/button';
 import { ProfileItem } from 'Components/profile/profile-list';
 
+import { isDarkColor } from '../models/collection';
 import CollectionAvatar from '../../presenters/includes/collection-avatar';
 
 import styles from './collection-item.styl';
@@ -30,26 +31,25 @@ const CollectionItem = ({ collection, showCurator }) => (
     </div>
     
     <CollectionLink collection={collection} className={styles.bubbleContainer} style={collectionColorStyles(collection)}>
-      <div className={styles.smallNameDescriptionArea}>
+      <div className={styles.collectionContainer}>
         <div className={styles.nameArea}>
           <div className={styles.collectionAvatarContainer}>
             <CollectionAvatar color={collection.coverColor} collectionId={collection.id} />
           </div>
           <div className={styles.collectionNameWrap}>
             <Button decorative>
-              {collection.private && <PrivateIcon />}
               <div className={styles.collectionName}>{collection.name}</div>
             </Button>
           </div>
         </div>
-        <div className={styles.description}>
+        <div className={styles.description} style={{color: isDarkColor(collection.coverColor) ? 'white': ''}}>
           <Markdown>{collection.description || ' '}</Markdown>
         </div>
       </div>
-      <div className={styles.smallProjectCount}>
-        <Pluralize count={collection.projects.length} singular="project" /> →
-      </div>
     </CollectionLink>
+    
+    <ProjectsPreview collection={collection} isAuthorized={isAuthorized} />
+    
   </div>
 );
 
