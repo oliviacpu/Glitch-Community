460af2d8ecf015598647c128205e4bdb4c18cf85
Glitch (superb-guide) <none>
2019-04-22 18:42:10 +0000

    👧😀 Checkpoint ./stories/inputs.js:97325/1227

--- stories/inputs.js
+++ stories/inputs.js
@@ -6,6 +6,7 @@ import TextInput from 'Components/inputs/text-input';
 import TextArea from 'Components/inputs/text-area';
 import WrappingTextInput from 'Components/inputs/wrapping-text-input';
 
+import OptimisticTextInput from 'Components/fields/optimistic-text-input';
 import ProjectDomainInput from 'Components/fields/project-domain-input';
 import TeamNameInput from 'Components/fields/team-name-input';
 import TeamUrlInput from 'Components/fields/team-url-input';
@@ -20,7 +21,20 @@ inputStory.add('with error', () => <TextInput placeholder="glitch" error="That t
 inputStory.add('text area', () => <TextArea placeholder="This is a multiline text field" error="Reason is required" />);
 inputStory.add('wrapping text', () => <WrappingTextInput placeholder="This is a text field that wraps instead of scrolling" error="An error could go here!" />);
 
+const OptimisticProps = ({ children }) => {
+  const [value, setValue] = React.useState('');
+  const onChange = async (newValue) => {
+    await new Promise((resolve) => setTimeout(resolve, 100));
+    if (newValue === 'error') {
+      throw 'error is no good!';
+    }
+    setValue(newValue);
+  };
+  return children(props);
+};
+
 const fieldStory = storiesOf('Text Fields', module);
+fieldStory.add('optimistic input', () => <OptimisticTextInput {...useOptimisticProps()} />);
 fieldStory.add('project domain', () => <ProjectDomainInput />);
 fieldStory.add('team name', () => <TeamNameInput />);
 fieldStory.add('team url', () => <TeamUrlInput />);
