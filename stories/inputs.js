0648451da536377ecbf68823f9185951e04b3ba4
Glitch (superb-guide) <none>
2019-04-22 18:10:12 +0000

    ⏱🎏 Checkpoint ./stories/inputs.js:97325/635

--- stories/inputs.js
+++ stories/inputs.js
@@ -3,10 +3,12 @@ import { storiesOf } from '@storybook/react';
 
 import TextInput from 'Components/inputs/text-input';
 import TextArea from 'Components/inputs/text-area';
+import WrappingTextInput from 'Components/inputs/wrapping-text-input';
 
 storiesOf('Text Input', module)
-  .add('regular', () => <TextInput placeholder="type something!" />)
-  .add('prefix', () => <TextInput placeholder="type something!" prefix="@" />)
+  .add('text input', () => <TextInput placeholder="type something!" />)
+  .add('affixes', () => <TextInput placeholder="type something!" prefix="#" postfix="#" />)
   .add('search', () => <TextInput type="search" opaque={true} search={true} placeholder="bots, apps, users" />)
   .add('with error', () => <TextInput placeholder="glitch" error="That team already exists" />)
-  .add('text area', () => <TextArea placeholder="[Something here] doesn't seem appropriate for Glitch because..." error="Reason is required" />);
+  .add('text area', () => <TextArea placeholder="[Something here] doesn't seem appropriate for Glitch because..." error="Reason is required" />)
+  .add('wrapping text', () => <WrappingTextInput placeholder="[Something here] doesn't seem appropriate for Glitch because..." error="Reason is required" />);
