f19bad5ffd65361eb35fa557cade6f1337b39d78
Glitch (superb-guide) <none>
2019-04-22 18:21:06 +0000

    🏖🐇 Checkpoint ./stories/inputs.js:97325/760 ./src/components/global.styl:97325/550

--- stories/inputs.js
+++ stories/inputs.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import 'Components/global.styl';
 
 import TextInput from 'Components/inputs/text-input';
 import TextArea from 'Components/inputs/text-area';
 import WrappingTextInput from 'Components/inputs/wrapping-text-input';
 
-storiesOf('Text Input', module)
-  .add('text input', () => <TextInput placeholder="type something!" />)
-  .add('affixes', () => <TextInput placeholder="type something!" prefix="#" postfix="#" />)
-  .add('search', () => <TextInput type="search" opaque={true} search={true} placeholder="bots, apps, users" />)
-  .add('with error', () => <TextInput placeholder="glitch" error="That team already exists" />)
-  .add('text area', () => <TextArea placeholder="[Something here] doesn't seem appropriate for Glitch because..." error="Reason is required" />)
-  .add('wrapping text', () => <WrappingTextInput placeholder="[Something here] doesn't seem appropriate for Glitch because..." error="Reason is required" />);
+import ProjectDomainInput from 'Components/fields/project-domain-input';
+
+const inputStory = storiesOf('Text Input', module);
+inputStory.add('text input', () => <TextInput placeholder="type something!" />);
+inputStory.add('affixes', () => <TextInput placeholder="type something!" prefix="#" postfix="#" />);
+inputStory.add('search', () => <TextInput type="search" opaque={true} search={true} placeholder="bots, apps, users" />);
+inputStory.add('with error', () => <TextInput placeholder="glitch" error="That team already exists" />);
+inputStory.add('text area', () => <TextArea placeholder="[Something here] doesn't seem appropriate for Glitch because..." error="Reason is required" />);
+inputStory.add('wrapping text', () => <WrappingTextInput placeholder="This is a text field that wraps instead of scrolling" error="An error could go here!" />);
+
+const fieldStory = storiesOf('Text Fields', module);
+fieldStory.add('project domain', () => <ProjectDomainInput />);
