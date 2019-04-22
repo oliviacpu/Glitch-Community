5a970fea53aa2432e9aeaaf468b5ed4243561d2f
Glitch (community) <none>
2019-04-22 19:33:43 +0000

    😎🔖 Checkpoint ./src/components/text/markdown.js:301412/3559

--- src/components/text/markdown.js
+++ src/components/text/markdown.js
@@ -20,30 +20,34 @@ const md = ({ allowImages }) => {
   return mdIt.use(markdownEmoji).use(markdownSanitizer);
 };
 
+const stripHtml = (html) => {
+  const regex = /<\/?([a-z][a-z0-9]*)\b[^>]*>?/gi;
+  return html ? html.replace(regex, '').trim() : '';
+};
+
 /**
  * Markdown Component
  */
 const Markdown = ({ children, length, allowImages, renderAsPlaintext }) => {
   let rendered = md({ allowImages }).render(children || '');
+  let className = styles.markdownContent;
+
   if (length > 0) {
     rendered = truncate(rendered, length, { ellipsis: '…' });
   }
+
   if (renderAsPlaintext) {
-    console.log('renderAsPlaintext');
     rendered = stripHtml(rendered);
+    className = '';
   }
+
   return (
     <span
-      className={!renderAsPlaintext ? styles.markdownContent : ''}
+      className={className}
       dangerouslySetInnerHTML={{ __html: rendered }} // eslint-disable-line react/no-danger
     />
   );
 };
-
-function stripHtml (html) {
-  const regex = /<\/?([a-z][a-z0-9]*)\b[^>]*>?/gi;
-  return html ? html.replace(regex, '').trim() : '';
-}
 
 Markdown.propTypes = {
   /** element(s) to display in the button */
