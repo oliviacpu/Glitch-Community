c41f2dc278b80c52771093be8d37620477193c0a
Glitch (superb-guide) <none>
2019-04-23 20:00:18 +0000

    🚖👜 Checkpoint ./src/components/inputs/markdown-input.js:97325/211

---
+++ src/components/inputs/markdown-input.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import TextArea from './text-area';
+import Markdown from '../text/markdown';
+
+const MarkdownInput = ({}) => {
+};
+
