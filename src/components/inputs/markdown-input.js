1f488e17a510e084b977ec5923beb825919fe7dd
Glitch (superb-guide) <none>
2019-04-23 20:11:22 +0000

    🏃😼 Checkpoint ./src/components/inputs/markdown-input.js:97325/1352 ./src/components/inputs/text-area.js:97325/197

--- src/components/inputs/markdown-input.js
+++ src/components/inputs/markdown-input.js
@@ -4,6 +4,32 @@ import PropTypes from 'prop-types';
 import TextArea from './text-area';
 import Markdown from '../text/markdown';
 
-const MarkdownInput = ({}) => {
+const MarkdownInput = ({ onChange, placeholder, value }) => {
+  const [focused, setFocused] = React.useState(false);
+  if (focused) {
+    return (
+      <TextArea
+        onBlur={() => setFocused(false)}
+        onChange={onChange}
+        placeholder={placeholder}
+        value={value}
+      />
+    );
+  }
+  return (
+    <p
+      placeholder={placeholder}
+      aria-label={placeholder}
+      role="textbox" // eslint-disable-line jsx-a11y/no-noninteractive-element-to-interactive-role
+      tabIndex={0}
+      onFocus={() => setFocused(true)}
+    >
+      <Markdown>{value}</Markdown>
+    </p>
+  );
 };
 
+MarkdownInput.propTypes = {
+};
+
+export default MarkdownInput;
