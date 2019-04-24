798a0c85862f48cde9ef12b08016ab48f53b93ea
Glitch (community) <none>
2019-04-24 20:30:00 +0000

    stories

--- stories/index.js
+++ stories/index.js
@@ -31,6 +31,7 @@ import CoverContainer from 'Components/containers/cover-container';
 import MoreIdeas from 'Components/more-ideas';
 import 'Components/profile-list/story';
 import 'Components/search-form/story';
+import 'Components/profile-container/story';
 import { users, teams, projects, collections } from './data';
 import { withState, provideContext } from './util';
 
