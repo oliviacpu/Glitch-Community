5544ee188bf04c15e73e231760516c3274f644d1
Glitch (community) <none>
2019-04-24 18:56:04 +0000

    😽👷 Checkpoint ./src/components/profile-container/styles.styl:271885/757 ./src/components/profile-container/index.js:271885/255

--- src/components/profile-container/index.js
+++ src/components/profile-container/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 import CoverContainer from 'Components/containers/cover-container';
 import ProfileList from 'Components/profile-list';
@@ -30,8 +31,10 @@ const ProfileContainer = ({ avatarStyle, avatarActions, type, item, coverActions
   <CoverContainer type={type} item={item} buttons={<TrackedButtonGroup items={coverActions} />}>
     <div className={styles.profileWrap}>
       <div className={styles.avatarContainer}>
-        <div className={styles.avatar} style={avatarStyle} />
-        <TrackedButtonGroup items={avatarActions} />
+        <div className={classnames(styles.avatar, styles[type])} style={avatarStyle} />
+        <div className={styles.avatarButtons}>
+          <TrackedButtonGroup items={avatarActions} />
+        </div>
       </div>
       <div className={styles.profileInfo}>{children}</div>
     </div>
@@ -45,7 +48,7 @@ const ProfileContainer = ({ avatarStyle, avatarActions, type, item, coverActions
 
 ProfileContainer.propTypes = {
   item: PropTypes.object.isRequired,
-  type: PropTypes.oneOfType(['user', 'team']).isRequired,
+  type: PropTypes.oneOf(['user', 'team']).isRequired,
   children: PropTypes.node.isRequired,
   avatarStyle: PropTypes.object,
   avatarActions: PropTypes.object,
