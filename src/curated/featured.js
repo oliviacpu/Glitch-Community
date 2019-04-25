8df89e16e5a374334a2d3d1e9cb4240a4957ad8c
Glitch (community-staging) <none>
2019-04-25 15:01:03 +0000

    💓🚄 Checkpoint ./src/curated/featured.js:11/1229

--- src/curated/featured.js
+++ src/curated/featured.js
@@ -14,18 +14,18 @@ Example:
 // make sure image urls use https
 export default [
   {
-    title: 'Which Domain Costs More?',
-    img: 'https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fdomainpricing_final.jpg?1549281323718',
-    link: 'https://glitch.com/~domain-pricing'
-  },
-  {
-    title: 'Just Take A Moment',
-    img: 'https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2FYourMomentofZen.jpg?1541072010180',
-    link: 'https://glitch.com/~your-moment-of-zen'  
+    title: 'Get to the Escape Pod!',
+    img: 'https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fglitch_space_extinguisher.jpg?1552302001273',
+    link: 'https://glitch.com/~space-extinguisher'
   },  
   {
-    title: 'I\'d Totally Eat That!',
-    img: 'https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Ffood-hype.jpg?1540206366423',
-    link: 'https://glitch.com/~food-hype'
+    title: 'Make Your Own Planet',
+    img: 'https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fplanetgenerator.jpg?1538392449068',
+    link: 'https://glitch.com/~planet-generator'
+  },
+  {
+    title: 'Step into the Metaverse... D\'oh',
+    img: 'https://cdn.glitch.com/02863ac1-a499-4a41-ac9c-41792950000f%2Fglitch_homer.jpg?1552302001952',
+    link: 'https://glitch.com/~homer-metaverse'  
   }
 ];
