197f1ce391c1b98815b8ea393bbad85575bf77f9
Glitch (superb-guide) <none>
2019-04-25 20:28:53 +0000

    🚙😺 Checkpoint ./src/presenters/pop-overs/edit-collection-color-pop.js:97325/197

--- src/presenters/pop-overs/edit-collection-color-pop.js
+++ src/presenters/pop-overs/edit-collection-color-pop.js
@@ -28,7 +28,7 @@ class EditCollectionColorPop extends React.Component {
     this.handleChange = this.handleChange.bind(this); // for when user enters in custom hex
     this.keyPress = this.keyPress.bind(this); // handles enter key for custom hex
     this.getRandomColor = this.getRandomColor.bind(this); // gets random color
-    this.changeColor = throttle(this.changeColor.bind(this), 250); // get update from system color picker
+    this.changeColor = throttle(this.changeColor.bind(this), 100); // get update from system color picker
     this.update = this.props.updateColor;
   }
 
@@ -71,9 +71,6 @@ class EditCollectionColorPop extends React.Component {
   }
 
   changeColor(color) {
-    // upate the UI manually to prevent lag
-    document.getElementById('color-picker').value = color;
-    document.getElementById('color-picker').style.backgroundColor = color;
     this.setState({ color });
     this.setState({ query: color });
     this.update(color);
@@ -98,7 +95,7 @@ class EditCollectionColorPop extends React.Component {
               value={this.state.query}
               onChange={this.handleChange}
               onKeyPress={this.keyPress}
-              placeholder="Custom color hex"
+              placeholder="Hex"
               labelText="Custom color hex"
               error={this.state.error ? 'Invalid Hex' : null}
             />
