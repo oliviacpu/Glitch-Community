8eabfffaa4d4280bd6c4c3b2f4cc65a60a147cac
Glitch (community) <none>
2019-04-25 16:59:01 +0000

    🚆🌃 Checkpoint ./src/components/header/story.js:271885/1788 ./src/components/header/header.styl:271885/27

--- src/components/header/story.js
+++ src/components/header/story.js
@@ -4,7 +4,16 @@ import { users, projects } from '../../../stories/data';
 import { provideContext } from '../../../stories/util';
 import Header from './index';
 
+const mockAPI = {
+  async get(url) {
+    return { data: [] };
+  },
+};
+
 storiesOf('Header', module)
-  .add('logged out', provideContext({ currentUser: { projects: [] } }, () => <Header />))
-  .add('logged in', provideContext({ currentUser: { ...users.modernserf, projects: [] } }, () => <Header />))
-  .add('logged in with projects', provideContext({ currentUser: { ...users.modernserf, projects: Object.values(projects) } }, () => <Header />));
+  .add('logged out', provideContext({ currentUser: { projects: [] }, api: mockAPI }, () => <Header />))
+  .add('logged in', provideContext({ currentUser: { ...users.modernserf, projects: [] }, api: mockAPI }, () => <Header />))
+  .add(
+    'logged in with projects',
+    provideContext({ currentUser: { ...users.modernserf, projects: Object.values(projects) }, api: mockAPI }, () => <Header />),
+  );
