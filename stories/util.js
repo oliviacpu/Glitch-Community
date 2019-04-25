3521f5f112ac99048da14276e4b12edbf8927fe3
Glitch (community) <none>
2019-04-25 16:26:01 +0000

    🏨🔮 Checkpoint ./stories/util.js:271885/19237 ./src/presenters/includes/user-prefs.js:271885/7 ./src/presenters/includes/dev-toggles.js:271885/9

--- stories/util.js
+++ stories/util.js
@@ -1,5 +1,8 @@
 import { Context as CurrentUserContext } from '../src/state/current-user';
 import { Context as APIContext } from '../src/state/api';
+import { Context as UserPrefsContext } from '../src/presenters/includes/user-prefs';
+import { Context as DevTogglesCOntext } from '../src/presenters/includes/dev-toggles';
+
 /* eslint-disable import/prefer-default-export */
 
 import React, { useState } from 'react';
@@ -12,10 +15,30 @@ export const withState = (initState, Component) => {
   return () => <WrappedComponent />;
 };
 
-export const provideContext = ({ currentUser = {}, api = {} } = {}, Component) => () => (
-  <CurrentUserContext.Provider value={{ currentUser }}>
-    <APIContext.Provider value={api}>
-      <Component />
-    </APIContext.Provider>
-  </CurrentUserContext.Provider>
+const log = console.log.bind(console);
+
+const defaultUserPrefs = {
+  prefs: {},
+  set: log,
+};
+
+const defaultDevToggles = {
+  enabledToggles: [],
+  toggleData: log,
+  setEnabledToggles: log,
+};
+
+export const provideContext = (
+  { userPrefs = defaultUserPrefs, devToggles = defaultDevToggles, currentUser = {}, api = {} } = {},
+  Component,
+) => () => (
+  <UserPrefsContext.Provider value={userPrefs}>
+    <DevTogglesContext.Provider value={devToggles}>
+      <CurrentUserContext.Provider value={{ currentUser }}>
+        <APIContext.Provider value={api}>
+          <Component />
+        </APIContext.Provider>
+      </CurrentUserContext.Provider>
+    </DevTogglesContext.Provider>
+  </UserPrefsContext.Provider>
 );
