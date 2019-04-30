75d04fe1b65a6f56cd1e27e218c5df112893020d
Glitch (community-staging) <none>
2019-04-30 12:52:10 +0000

    📟🏛 Checkpoint ./src/curated/collections.js:11/77

--- src/curated/collections.js
+++ src/curated/collections.js
@@ -3,9 +3,9 @@
 
 // What collections to show in the expanded area on the homepage
 export const featuredCollections = [
-  { owner: 'glitch', name: 'glitch-this-week-april-24-2019' },
-  { owner: 'glitch', name: 'relish-the-randomosity' },
-  { owner: 'glitch', name: 'games-made-with-phaser' },
+  { owner: 'glitch', name: 'glitch-this-week-april-30-2019' },
+  { owner: 'glitch', name: 'draw-with-music' },
+  { owner: 'glitch', name: 'twitter-treats' },
 ];
 
 // More ideas is populated from this team
