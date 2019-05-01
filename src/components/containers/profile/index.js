e9c1ed0ddab196e7c6a938e6944ffde363176e51
Glitch (community-staging) <none>
2019-05-01 21:58:10 +0000

    👩‍👩‍👦🍀 Checkpoint ./src/components/containers/profile/index.js:355867/323 ./src/components/containers/profile/team.js:355867/2087

--- src/components/containers/profile/index.js
+++ src/components/containers/profile/index.js
@@ -1,3 +1,9 @@
-import Project from './project'
-import Team from './team'
-import 
+import Project from './project';
+import Team from './team';
+import User from './user';
+
+export default {
+  Project,
+  Team,
+  User,
+};
