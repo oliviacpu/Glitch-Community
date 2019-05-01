0cb73a11a65abc1546f7c27648011337d52a1794
Glitch (superb-guide) <none>
2019-05-01 16:16:28 +0000

    🌓🍏 Checkpoint ./shared/api.js:97325/165

--- shared/api.js
+++ shared/api.js
@@ -33,7 +33,8 @@ const getAllPages = async (api, url) => {
     hasMore = data.hasMore;
     url = data.nextPage;
     if (hasMore && !data.lastOrderValue) {
-      console.warn('Rest api lastOrderValue was unexpectedly empty', url);
+      console.warn('Rest api lastOrderValue is empty', url);
+      // can't use sentry since this is shared... should it throw?
       break;
     }
   }
