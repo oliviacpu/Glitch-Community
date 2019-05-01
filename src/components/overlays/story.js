347eb1615590a2385f7d3fec0e2bbbc0339ae5c5
Glitch (quixotic-rocket) <none>
2019-05-01 14:47:34 +0000

    🐴👖 Checkpoint ./src/components/overlays/story.js:97325/797 ./src/components/overlays/overlay.styl:97325/17

--- src/components/overlays/story.js
+++ src/components/overlays/story.js
@@ -2,4 +2,21 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import 'Components/global.styl';
 
-const story = stor
+import Overlay from './overlay';
+import OverlaySection from './overlay-section';
+
+const story = storiesOf('Overlays', module);
+
+story.add('generic', () => (
+  <Overlay>
+    <OverlaySection type="info">
+      Title
+    </OverlaySection>
+    <OverlaySection type="actions">
+      Content Section 1
+    </OverlaySection>
+    <OverlaySection type="actions">
+      Content Section 2
+    </OverlaySection>
+  </Overlay>
+));
