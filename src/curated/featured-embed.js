62e6f45f03a438f29065d05c74a6ccf2617d7fbf
Glitch (community-staging) <none>
2019-05-08 16:06:38 +0000

    🍵🙅 Checkpoint ./src/curated/featured-embed.js:11/59

--- src/curated/featured-embed.js
+++ src/curated/featured-embed.js
@@ -1,11 +1,11 @@
 const body =
-  '<p>Build a Material theme allows you to style and remix themeable components, so you can create your own custom version of Material Design.</p><p>Define your theme, see it in action, and get the code.</p>';
+  '<p>Style and remix themeable components, so you can create your own custom version of Material Design.</p><p>Define your theme, see it in action, and get the code.</p>';
 
 // make sure image urls use https
 export default {
   image: 'https://culture-zine.glitch.me/culture/content/images/2019/04/Glitch_Illo_MaterialApps-Headline-v1--1--4.png',
   mask: 'mask-4',
-  title: 'Build your own Material Theme',
+  title: 'Build Your Own Material Theme',
   appDomain: 'material-theme-builder',
   blogUrl: '/build-material-design-themes-on-glitch/',
   body,
