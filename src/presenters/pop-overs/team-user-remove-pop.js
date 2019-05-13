c7d205f5bc0aba89afa20346b0cd3a9eb476e390
Glitch (community-staging) <none>
2019-05-13 18:54:10 +0000

    💸🐠 Checkpoint ./src/presenters/pop-overs/team-user-remove-pop.js:271885/53192 ./src/models/team.js:271885/5286

--- src/presenters/pop-overs/team-user-remove-pop.js
+++ src/presenters/pop-overs/team-user-remove-pop.js
@@ -2,29 +2,20 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import Loader from 'Components/loader';
+import { getAvatarThumbnailUrl, getDisplayName } from 'Models/user';
+import { getAvatarUrl as getProjectAvatarUrl } from 'Models/project';
 import { useTrackedFunc } from '../segment-analytics';
 import { NestedPopoverTitle } from './popover-nested';
-import { getAvatarThumbnailUrl, getDisplayName } from '../../models/user';
-import { getAvatarUrl as getProjectAvatarUrl } from '../../models/project';
 
-const TeamUserRemoveButton = ({ user, removeUser }) => {
-  return (
-    <button className="button-small has-emoji" onClick={r} type="button">
-      Remove <img className="emoji avatar" src={getAvatarThumbnailUrl(user)} alt={user.login} style={{ backgroundColor: user.color }} />
-    </button>
-  );
-};
+const TeamUserRemoveButton = ({ user, removeUser }) => (
+  <button className="button-small has-emoji" onClick={removeUser} type="button">
+    Remove <img className="emoji avatar" src={getAvatarThumbnailUrl(user)} alt={user.login} style={{ backgroundColor: user.color }} />
+  </button>
+);
 
-function TeamUserRemovePop({ user, removeUser, userTeamProjects: userTeamProjectsResponse, togglePopover }) {
-  const [selectedProjects, setSelectedProjects] = useState(new Set());
-
-  const onRemoveUser = useTrackedFunc(() => {
-    togglePopover();
-    removeUser(Array.from(selectedProjects));
-  }, 'Remove from Team submitted');
-
+const TeamUserProjectsToggle = ({ userTeamProjects, selectedProjects, setSelectedProjects }) => {
   function selectAllProjects() {
-    const userTeamProjects = userTeamProjects.data || [];
+    const userTeamProjects = userTeamProjectsResponse.data || [];
     setSelectedProjects(new Set(userTeamProjects.map((p) => p.id)));
   }
 
@@ -43,6 +34,40 @@ function TeamUserRemovePop({ user, removeUser, userTeamProjects: userTeamProject
       return nextSelectedProjects;
     });
   }
+  return (
+    <>
+      <p className="action-description">Also remove them from these projects</p>
+      <div className="projects-list">
+        {userTeamProjects.map((project) => (
+          <label key={project.id} htmlFor={`remove-user-project-${project.id}`}>
+            <input
+              className="checkbox-project"
+              type="checkbox"
+              id={`remove-user-project-${project.id}`}
+              checked={this.state.selectedProjects.has(project.id)}
+              value={project.id}
+              onChange={this.handleCheckboxChange}
+            />
+            <img className="avatar" src={getProjectAvatarUrl(project.id)} alt="" />
+            {project.domain}
+          </label>
+        ))}
+      </div>
+      {userTeamProjects.length > 1 && (
+        <button className="button-small" type="button" onClick={allProjectsSelected ? unselectAllProjects : selectAllProjects}>
+          {allProjectsSelected ? 'Unselect All' : 'Select All'}
+        </button>
+      )}
+    </>
+  );
+};
+function TeamUserRemovePop({ user, removeUser, userTeamProjects: userTeamProjectsResponse, togglePopover }) {
+  const [selectedProjects, setSelectedProjects] = useState(new Set());
+
+  const onRemoveUser = useTrackedFunc(() => {
+    togglePopover();
+    removeUser(Array.from(selectedProjects));
+  }, 'Remove from Team submitted');
 
   const userTeamProjects = userTeamProjectsResponse.data || [];
   const allProjectsSelected = userTeamProjects.every((p) => selectedProjects.has(p.id));
@@ -52,30 +77,12 @@ function TeamUserRemovePop({ user, removeUser, userTeamProjects: userTeamProject
     projects = <Loader />;
   } else if (userTeamProjects.length > 0) {
     projects = (
-      <>
-        <p className="action-description">Also remove them from these projects</p>
-        <div className="projects-list">
-          {userTeamProjects.map((project) => (
-            <label key={project.id} htmlFor={`remove-user-project-${project.id}`}>
-              <input
-                className="checkbox-project"
-                type="checkbox"
-                id={`remove-user-project-${project.id}`}
-                checked={this.state.selectedProjects.has(project.id)}
-                value={project.id}
-                onChange={this.handleCheckboxChange}
-              />
-              <img className="avatar" src={getProjectAvatarUrl(project.id)} alt="" />
-              {project.domain}
-            </label>
-          ))}
-        </div>
-        {userTeamProjects.length > 1 && (
-          <button className="button-small" type="button" onClick={allProjectsSelected ? unselectAllProjects : selectAllProjects}>
-            {allProjectsSelected ? 'Unselect All' : 'Select All'}
-          </button>
-        )}
-      </>
+      <TeamUserProjectsToggle
+        userTeamProjects={userTeamProjects}
+        allProjectsSelected={allProjectsSelected}
+        unselectAllProjects={unselectAllProjects}
+        selectAllProjects={selectAllProjects}
+      />
     );
   }
 
