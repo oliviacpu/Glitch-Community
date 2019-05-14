25d48e962ebca055fad27423ad794a76875e7dfd
Glitch (community-staging) <none>
2019-05-14 14:58:06 +0000

    🚂🦄 Checkpoint ./src/components/collection/collection-item.js:271885/9317 ./src/components/deleted-projects/index.js:271885/6697 ./src/components/animation-container/index.js:271885/3754

--- src/components/animation-container/index.js
+++ src/components/animation-container/index.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import styles from './animations.styl';
 
 const types = ['slideDown', 'slideUp'];
 
-const AnimationContainer = ({ type, active, children, className, ...props }) => (
-  <div className={classnames(className, active && styles[type])} {...props}>
-    {children}
-  </div>
-);
+const AnimationContainer = ({ type, children, className, onAnimationEnd }) => {
+  const [state, setState] = useState({ active: false, handlerArgs: [] });
 
+  return (
+    <div className={classnames(className, state.active && styles[type])} onAnimationEnd={() => onAnimationEnd(...state.handlerArgs)}>
+      {children((...handlerArgs) => setState({ active: true, handlerArgs }))}
+    </div>
+  );
+};
 AnimationContainer.propTypes = {
   type: PropTypes.oneOf(types).isRequired,
-  active: PropTypes.bool.isRequired,
-  children: PropTypes.node.isRequired,
+  children: PropTypes.func.isRequired,
+  onAnimationEnd: PropTypes.func.isRequired,
   className: PropTypes.string,
 };
 
