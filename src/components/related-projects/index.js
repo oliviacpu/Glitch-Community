27783903bf3ae318855531d1c3ead5279c67eb47
Glitch (community-staging) <none>
2019-05-20 17:11:47 +0000

    🏏🏝 Checkpoint ./src/components/related-projects/index.js:271885/9178

--- src/components/related-projects/index.js
+++ src/components/related-projects/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { sampleSize, difference } from 'lodash';
 
@@ -23,16 +23,30 @@ RelatedProjectsBody.propTypes = {
   projects: PropTypes.array.isRequired,
 };
 
+
+const getProjects = {
+  team: {
+    pinned: (id) => `/v1/teams/by/id/pinnedProjects
+  }
+}
+
 const getTeam = (api, id) => api.get(`teams/${id}`).then(({ data }) => data);
 const getTeamPins = (api, id) => api.get(`teams/${id}/pinned-projects`).then(({ data }) => data);
 const getUser = (api, id) => api.get(`users/${id}`).then(({ data }) => data);
 const getUserPins = (api, id) => api.get(`users/${id}/pinned-projects`).then(({ data }) => data);
 
 async function getProjects(api, { type, id, ignoreProjectId }) {
-  const getPins = type === 'team' ? getTeamPins : getUserPins
-  const getAllProjects = type === 'team' ? getTeam : getUser
+  const getPins = type === 'team' ? getTeamPins : getUserPins;
+  const getAllProjects = type === 'team' ? getTeam : getUser;
 
-  const pins = await getPins(api, id);
+  const [pins, recents] = await Promise.all([
+    api.get(`/v1/${type}s/by/id/pinnedProjects?id=${id}`).then(res => res.data.items),
+    api.get(`/v1/${type}s/by/id/projects?id=${id}`).then(res => res.data.items),
+  ])
+  
+  const allProjects = pins.concat(recents).filter(project => project.id !== ignoreProjectId)
+  const sample = sampleSize(allProjects,
+  
   const pinIds = pins.map((pin) => pin.projectId);
   let ids = sampleSize(difference(pinIds, [ignoreProjectId]), PROJECT_COUNT);
 
@@ -52,65 +66,57 @@ async function getProjects(api, { type, id, ignoreProjectId }) {
   return null;
 }
 
-function useSample (items, count) {
-  const [sample, setSample] = useState(sampleSize
+function useSample(items, count) {
+  const [sample, setSample] = useState([]);
+  useEffect(() => {
+    setSample(sampleSize(items, count));
+  }, [count, ...items.map((item) => item.id)]);
+  return sample;
 }
 
+function RelatedProjects({ teams: allTeams, users: allUsers, ignoreProjectId }) {
+  const teams = useSample(allTeams, 1);
+  const users = useSample(allUsers, 2 - teams.length);
 
-function RelatedProjects ({ teams, users, ignoreProjectId }) {
-  constructor(props) {
-    super(props);
-    const teams = sampleSize(props.teams, 1);
-    const users = sampleSize(props.users, 2 - teams.length);
-    this.state = { teams, users };
+  if (!teams.length && !users.length) {
+    return null;
   }
-
-  render() {
-    const sampledTeams = useSample(teams, 1);
-    const sampledUsers = useSample(users, 2 - teams.length);
-    
-    
-    const { teams, users } = this.state;
-    if (!teams.length && !users.length) {
-      return null;
-    }
-    return (
-      <ul className="related-projects">
-        {teams.map((team) => (
-          <li key={team.id}>
-            <DataLoader get={(api) => this.getProjects(api, team.id, getTeamPins, getTeam)}>
-              {(projects) =>
-                projects && (
-                  <>
-                    <h2>
-                      <TeamLink team={team}>More by {team.name} →</TeamLink>
-                    </h2>
-                    <RelatedProjectsBody projects={projects} type="team" item={team} />
-                  </>
-                )
-              }
-            </DataLoader>
-          </li>
-        ))}
-        {users.map((user) => (
-          <li key={user.id}>
-            <DataLoader get={(api) => this.getProjects(api, user.id, getUserPins, getUser)}>
-              {(projects) =>
-                projects && (
-                  <>
-                    <h2>
-                      <UserLink user={user}>More by {getDisplayName(user)} →</UserLink>
-                    </h2>
-                    <RelatedProjectsBody projects={projects} type="user" item={user} />
-                  </>
-                )
-              }
-            </DataLoader>
-          </li>
-        ))}
-      </ul>
-    );
-  }
+  return (
+    <ul className="related-projects">
+      {teams.map((team) => (
+        <li key={team.id}>
+          <DataLoader get={(api) => getProjects(api, { type: 'team', id: team.id, ignoreProjectId })}>
+            {(projects) =>
+              projects && (
+                <>
+                  <h2>
+                    <TeamLink team={team}>More by {team.name} →</TeamLink>
+                  </h2>
+                  <RelatedProjectsBody projects={projects} type="team" item={team} />
+                </>
+              )
+            }
+          </DataLoader>
+        </li>
+      ))}
+      {users.map((user) => (
+        <li key={user.id}>
+          <DataLoader get={(api) => getProjects(api, { type: 'user', id: user.id, ignoreProjectId })}>
+            {(projects) =>
+              projects && (
+                <>
+                  <h2>
+                    <UserLink user={user}>More by {getDisplayName(user)} →</UserLink>
+                  </h2>
+                  <RelatedProjectsBody projects={projects} type="user" item={user} />
+                </>
+              )
+            }
+          </DataLoader>
+        </li>
+      ))}
+    </ul>
+  );
 }
 RelatedProjects.propTypes = {
   ignoreProjectId: PropTypes.string.isRequired,
