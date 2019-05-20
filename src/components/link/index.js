1e4a331d6442eecad4c3689076478a8ac1146110
Glitch (community-staging) <none>
2019-05-20 15:39:11 +0000

    move wrapping-link to link component

--- src/components/link/index.js
+++ src/components/link/index.js
@@ -7,8 +7,10 @@ import { getLink as getCollectionLink } from 'Models/collection';
 import { getLink as getProjectLink } from 'Models/project';
 import { getLink as getTeamLink } from 'Models/team';
 import { getLink as getUserLink } from 'Models/user';
+import WrappingLink from './wrapping-link';
 
-/* global */
+export { WrappingLink };
+
 const external = window.EXTERNAL_ROUTES ? Array.from(window.EXTERNAL_ROUTES) : [];
 
 const Link = React.forwardRef(({ to, children, ...props }, ref) => {
