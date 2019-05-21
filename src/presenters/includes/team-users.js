e7332564cdcaba4435cf27bfcf5bb4ad64df554c
Glitch (community-staging) <none>
2019-05-21 14:42:40 +0000

    🕷👣 Checkpoint ./src/presenters/includes/team-users.js:271885/55

--- src/presenters/includes/team-users.js
+++ src/presenters/includes/team-users.js
@@ -196,7 +196,8 @@ const JoinTeam = ({ onClick }) => (
 
 const useInvitees = createAPIHook(async (api, team, currentUserIsOnTeam) => {
   if (!currentUserIsOnTeam) return [];
-
+  // if (!team.tokens.length) return [];
+  
   try {
     const idString = team.tokens.map(({ userId }) => `id=${userId}`).join(',');
     const { data } = await api.get(`v1/users/by/id?${idString}`);
