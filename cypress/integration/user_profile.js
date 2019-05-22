068075510539c9a1104192d22f6c835ad4a56141
Olivia Computer <olivia@glitch.com>
2019-05-22 06:14:04 -0700

    Two emoji and the word Checkpoint

--- cypress/integration/user_profile.js
+++ cypress/integration/user_profile.js
@@ -1,123 +1,129 @@
 /// <reference types="Cypress" />
 
-import util from '../support/util'
+import { itemsResponse, makeTestUser, makeTestCollection, makeTestProject } from '../support/util'
 
-let testProject
-let testCollection
+const testUser = makeTestUser({ persistentToken: Cypress.env('GLITCH_TOKEN') })
+
+const featuredProject = makeTestProject({
+  id: 'featured-project-id',
+  domain: 'hello-webpage',
+  description: 'I will be featured and unfeatured, my domain _has_ to be "hello-webpage"',
+  permissions: [
+    {
+      userId: testUser.id,
+      accessLevel: 30,
+    },
+  ]
+})
+const pinnedProject = makeTestProject({
+  id: 'pinned-project-id',
+  domain: 'pinned-project',
+  description: 'I will be pinned and unpinned',
+  permissions: [
+    {
+      userId: testUser.id,
+      accessLevel: 30,
+    },
+  ]
+})
+const collectedProject = makeTestProject({
+  id: 'collected-project-id',
+  domain: 'collected-project',
+  description: 'I will be added and removed from a collection',
+  permissions: [
+    {
+      userId: testUser.id,
+      accessLevel: 30,
+    },
+  ]
+})
+const deletedProject = makeTestProject({
+  id: 'deleted-project-id',
+  domain: 'deleted-project',
+  description: 'I am a project that will be deleted',
+  permissions: [
+    {
+      userId: testUser.id,
+      accessLevel: 30,
+    },
+  ]
+})
+
+const projects = [featuredProject, pinnedProject, collectedProject, deletedProject]
+
+const testCollection = makeTestCollection()
 
 describe('User profile', () => {
+  beforeEach(() => {
+    cy.server()
+  })
+
   context('when signed in', () => {
+    beforeEach(() => {
+      cy.signIn()
+    })
+
     context('when on your own profile', () => {
       beforeEach(() => {
-        cy.signIn()
+        cy.route('GET', '**/v1/teams/by/url?url=*', {})
 
-        cy.server()
+        cy.route('GET', '**/collections?userId=*', [testCollection])
+        cy.route('GET', '**/collections?id=*', itemsResponse([testCollection]))
 
-        testProject = util.makeTestProject()
-        testCollection = util.makeTestCollection()
-        
-        cy.route('GET', '**/v1/projects/by/id*', {
-          items: [
-            testProject
-          ]
+        cy.route('GET', '**/projects/byIds?ids=*', [projects])
+
+        cy.route('GET', `**/users/by/id?id=${testUser.id}`, {
+          [testUser.id]: testUser
         })
 
-
-        cy.route('GET', '**/v1/users/by/login/projects?login*', {
-          items: [
-            testProject,
-          ]
+        cy.route('GET', '**/v1/users/by/login?login*', {
+          [testUser.login]: testUser
         })
 
-        cy.route('GET', '**/v1/projects/by/id/collections*', {
-          items: []
+        cy.route('GET', '**/v1/users/by/persistentToken?persistentToken*', {
+          [testUser.persistentToken]: testUser
         })
 
-        cy.route('GET', '**/v1/users/by/id/collections?id*', {
-          items: [],
-        })
+        cy.route('GET', '**/v1/users/by/id/collections?id*', itemsResponse([testCollection]))
+        cy.route('GET', '**/v1/users/by/id/projects?id*', itemsResponse(projects))
+        cy.route('GET', '**/v1/users/by/id/teams?id*', itemsResponse())
 
-        cy.route('GET', '**/v1/users/by/login/collections?login*', {
-          items: [],
-        })
+        cy.route('GET', '**/v1/users/by/login/pinnedProjects?login*', itemsResponse())
+        cy.route('GET', '**/v1/users/by/login/collections?login*', itemsResponse([testCollection]))
+        cy.route('GET', '**/v1/users/by/login/projects?login*', itemsResponse(projects))
+        cy.route('GET', '**/v1/users/by/login/teams?login*', itemsResponse())
 
-        cy.route('GET', '**/collections?userId=*', [])
+        cy.route('GET', '**/v1/projects/by/id*', itemsResponse(projects))
+        cy.route('GET', '**/v1/projects/by/id/collections?id*', itemsResponse([]))
+
+        cy.route('PATCH', '**/users/*', { featuredProjectId: featuredProject.id }).as('featureProject')
+        cy.route('POST', '**/users/*/pinned-projects/*', { projectId: pinnedProject.id }).as('pinProject')
+        cy.route('DELETE', '**/users/*/pinned-projects/*', 'OK').as('unpinProject')
+        cy.route('GET', '**/user/deleted-projects', []).as('getDeletedProjects')
+        cy.route('PATCH', `**/collections/${testCollection.id}/add/${collectedProject.id}`, "OK").as('collectProject')
+        cy.route('GET', `**/projects/${deletedProject.id}?showDeleted=true`, 'OK')
+        cy.route('DELETE', `**/projects/${deletedProject.id}`, 'OK').as('deleteProject')
       })
 
-      it('can feature projects', () => {
-        cy.route('PATCH', '**/users/*', { featuredProjectId: testProject.id })
-
-        cy.visit('/@olivia')
+      it.only('can perform project actions', () => {
+        cy.visit(`/@${testUser.login}`)
 
         cy.get('[data-cy=featured-project]').should('not.exist')
+        cy.get('[data-cy=pinned-projects]').should('not.exist')
 
         cy.get('[data-cy=recent-projects]').should('exist').within(() => {
           cy.get('ul').find('li').first().within(() => {
             cy.get('.project-options').click()
             cy.contains('Feature').click()
+            cy.wait('@featureProject')
           })
-        })
 
-        cy.get('[data-cy=featured-project]').should('exist')
-      })
-
-      it('can add pin', () => {
-        cy.route('POST', '**/users/*/pinned-projects/*', { projectId: testProject.id })
-
-        cy.visit('/@olivia')
-
-        cy.get('[data-cy=pinned-projects]').should('not.exist')
-
-        cy.get('[data-cy=recent-projects]').should('exist').within(() => {
           cy.get('ul').find('li').first().within(() => {
             cy.get('.project-options').click()
             cy.contains('Pin').should('exist').click()
+            cy.wait('@pinProject')
           })
-        })
 
-        cy.get('[data-cy=pinned-projects]').should('exist')
-      })
-
-      it('can remove pin', () => {
-        cy.route('GET', '**/v1/users/by/login/pinnedProjects*', {
-          items: [
-            testProject
-          ]
-        })
-        cy.route('DELETE', '**/users/3803619/pinned-projects/*', 'OK')
-
-        cy.visit('/@olivia')
-
-        cy.get('[data-cy=pinned-projects]').should('exist').within(() => {
-          cy.get('ul').find('li').first().within(() => {
-            cy.get('.project-options').click()
-            cy.contains('Un-Pin').should('exist').click()
-          })
-        })
-
-        cy.get('[data-cy=pinned-projects]').should('not.exist')
-      })
-
-      it('can add project to collection', () => {
-        cy.visit('/@olivia')
-
-        cy.route('GET', '**/v1/users/by/id/collections?id*', {
-          items: [
-            testCollection,
-          ],
-        })
-
-        cy.route('GET', '**/v1/users/by/login/collections?login*', {
-          items: [
-            testCollection,
-          ],
-        })
-
-        cy.route('GET', '**/collections?userId=*', [testCollection])
-
-        cy.route('PATCH', '**/collections/*/add/*', "OK")
-
-        cy.get('[data-cy=recent-projects]').should('exist').within(() => {
           cy.get('ul').find('li').first().within(() => {
             cy.get('.project-options').click()
             cy.contains('Add to Collection').should('exist').click()
@@ -126,34 +132,40 @@ describe('User profile', () => {
               {
                 ...testCollection,
                 projects: [
-                  testProject
+                  collectedProject
                 ]
               },
-            ])
+            ]).as('collections')
 
             cy.get('.add-project-to-collection-pop').should('exist').within(() => {
               cy.get('.results').find('li').first().click()
+              cy.wait('@collections')
             })
           })
-        })
-      })
 
-      xit('can leave project', () => { })
-
-      it('can delete project', () => {
-        cy.route('DELETE', '**/projects/*', 'OK').as('deleteProject')
-        cy.route('GET', '**/user/deleted-projects', [testProject]).as('getDeletedProject')
-
-        cy.visit('/@olivia')
-
-        cy.get('[data-cy=recent-projects]').within(() => {
-          cy.get('ul').find('li').first().within(() => {
+          cy.get('ul').find('li').next().within(() => {
             cy.get('.project-options').click()
             cy.contains('Delete Project').should('exist').click()
+            cy.wait('@deleteProject')
           })
         })
 
-        cy.wait('@deleteProject')
+        cy.get('[data-cy=featured-project]').should('exist').within(() => {
+          cy.get('.project-options').click()
+
+          cy.route('PATCH', '**/users/*', { featuredProjectId: null }).as('unfeatureProject')
+
+          cy.contains('Un-feature').should('exist').click()
+          cy.wait('@unfeatureProject')
+        })
+
+        cy.get('[data-cy=pinned-projects]').should('exist').within(() => {
+          cy.get('ul').find('li').first().within(() => {
+            cy.get('.project-options').click()
+            cy.contains('Un-Pin').should('exist').click()
+            cy.wait('@unpinProject')
+          })
+        })
       })
     })
   })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,6 +10,7 @@
 //
 //
 // -- This is a parent command --
+
 const omitBy = require('lodash/omitBy')
 const keys = require('lodash/keys')
 const isObject = require('lodash/isObject')
@@ -30,47 +31,6 @@ Cypress.Commands.add('createFixture', (name, url) => cy
   .then((response) => cy.writeFile(`cypress/fixtures/${name}.json`, filterTokens(response.body))));
 
 Cypress.Commands.add('createFixtures', (fixtures) => Object.entries(fixtures).forEach((entry) => cy.createFixture(entry[0], entry[1])));
-
-export const makeTestProject = () => {
-  return {
-    "id": "a122f2b2-8a43-41b7-a1db-35237223a45e",
-    "inviteToken": "f372b296-be5d-40da-a364-f21292e5c384",
-    "description": "The test project that does useful things",
-    "domain": "test-project",
-    "baseId": "929980a8-32fc-4ae7-a66f-dddb3ae4912c",
-    "private": false,
-    "likesCount": 0,
-    "suspendedAt": null,
-    "suspendedReason": "",
-    "lastAccess": "2019-05-21T21:30:58.599Z",
-    "avatarUpdatedAt": "2019-04-12T23:23:30.006Z",
-    "numEditorVisits": 6,
-    "numAppVisits": 36,
-    "visitsLastBackfilledAt": "2019-05-21T19:00:01.000Z",
-    "showAsGlitchTeam": false,
-    "isEmbedOnly": false,
-    "remixChain": [
-
-    ],
-    "notSafeForKids": false,
-    "createdAt": "2019-04-12T23:23:28.941Z",
-    "updatedAt": "2019-04-13T00:07:51.923Z",
-    "deletedAt": null,
-    "authUserIsMember": true,
-    "authUserIsTeamMember": false,
-    "pin": {
-    },
-    "permissions": [
-
-    ],
-    "features": [
-
-    ],
-    "teamIds": [
-
-    ],
-  }
-}
 
 Cypress.Commands.add('signIn', () => {
   const GLITCH_TOKEN = Cypress.env('GLITCH_TOKEN');
