27e518daf1c2eb0d6cb3e21eb40a2775e753a184
Glitch (community-staging) <none>
2019-05-22 17:05:35 +0000

    🐏🍀 Checkpoint ./src/presenters/pop-overs/add-collection-project-pop.js:271885/45653

--- src/presenters/pop-overs/add-collection-project-pop.js
+++ src/presenters/pop-overs/add-collection-project-pop.js
@@ -15,59 +15,13 @@ import ProjectsLoader from '../projects-loader';
 import { useNotifications, AddProjectToCollectionMsg } from '../notifications';
 import PopoverWithButton from './popover-with-button';
 
-const ProjectSearchResults = ({ projects, collection, onClick, projectName, excludedProjectsCount }) => {
-  if (projects.length > 0) {
-    const collectionProjectIds = collection.projects.map((project) => project.id);
-    projects = projects.filter((project) => !collectionProjectIds.includes(project.id));
-
-    return <ProjectResultsUL {...{ projects, collection, onClick }} />;
-  }
-
-  if (projectName) {
-    return (
-      <p className="results-empty">
-        {projectName} is already in this collection
-        <span role="img" aria-label="">
-          💫
-        </span>
-      </p>
-    );
-  }
-
-  return (
-    <p className="results-empty">
-      nothing found{' '}
-      <span role="img" aria-label="">
-        💫
-      </span>
-      <br />
-      {excludedProjectsCount > 0 && (
-        <span>
-          Excluded <Pluralize count={excludedProjectsCount} singular="search result" />
-        </span>
-      )}
-    </p>
-  );
-};
-
-ProjectSearchResults.propTypes = {
-  collection: PropTypes.object.isRequired,
-  projectName: PropTypes.string,
-  excludedProjectsCount: PropTypes.number,
-};
-
-ProjectSearchResults.defaultProps = {
-  projectName: '',
-  excludedProjectsCount: 0,
-};
-
 function parseQuery(query) {
   query = query.trim();
   try {
     const queryUrl = new URL(query);
     if (queryUrl.href.includes('me') && !queryUrl.href.includes('~')) {
       // https://add-to-alexa.glitch.me/
-      return  queryUrl.hostname.substring(0, queryUrl.hostname.indexOf('.'));
+      return queryUrl.hostname.substring(0, queryUrl.hostname.indexOf('.'));
     }
     // https://glitch.com/~add-to-alexa
     return queryUrl.pathname.substring(queryUrl.pathname.indexOf('~') + 1);
@@ -76,10 +30,10 @@ function parseQuery(query) {
   }
 }
 
-function AddCollectionProjectPop({ collection, togglePopover, addProjectToCollection }) {
+function AddCollectionProjectPop({ collection, initialProjects, togglePopover, addProjectToCollection }) {
   const [query, setQuery] = useState('');
-  const { isUrl, parsedQuery } = parseQuery(query);
-  const { projects, status } = useAlgoliaSearch(parsedQuery, { types: ['projects'] });
+  const parsedQuery = parseQuery(query);
+  const { projects: retrievedProjects, status } = useAlgoliaSearch(parsedQuery, { types: ['projects'] });
   const { createNotification } = useNotifications();
 
   const onClick = useTrackedFunc(
@@ -93,9 +47,11 @@ function AddCollectionProjectPop({ collection, togglePopover, addProjectToCollec
     { origin: 'Add Project collection' },
   );
 
+  const projects = parsedQuery.lengtth ? initialProjects : retrievedProjects; 
   const idsOfProjectsInCollection = new Set(collection.projects.map((p) => p.id));
   const [projectsAlreadyInCollection, newProjectsToAdd] = partition(projects, (project) => idsOfProjectsInCollection.has(project.id));
-  const 
+  const excludedProjectsCount = projectsAlreadyInCollection.length;
+  const excludingExactMatch = projectsAlreadyInCollection.some((p) => p.domain === parsedQuery);
 
   return (
     <PopoverDialog wide align="right">
@@ -131,141 +87,34 @@ function AddCollectionProjectPop({ collection, togglePopover, addProjectToCollec
           </ResultsList>
         </PopoverSection>
       )}
+      {status === 'ready' && newProjectsToAdd.length === 0 && excludingExactMatch && (
+        <PopoverInfo>
+          <p>
+            {parsedQuery} is already in this collection <Emoji name="sparkles" />
+          </p>
+        </PopoverInfo>
+      )}
+      {status === 'ready' && newProjectsToAdd.length === 0 && !excludingExactMatch && (
+        <PopoverInfo>
+          <p>
+            nothing found <Emoji name="sparkles" />
+          </p>
+          {projectsAlreadyInCollection.length > 0 && (
+            <p>
+              Excluded <Pluralize count={projectsAlreadyInCollection.length} singular="search result" />
+            </p>
+          )}
+        </PopoverInfo>
+      )}
     </PopoverDialog>
   );
 }
 
-class AddCollectionProjectPop extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      query: '', // The actual search text
-      maybeRequest: null, // The active request promise
-      maybeResults: null, // Null means still waiting vs empty,
-      projectName: '', // the project name if the search result is a Url
-      excludedProjectsCount: 0, // number of projects omitted from search
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.clearSearch = this.clearSearch.bind(this);
-    this.startSearch = debounce(this.startSearch.bind(this), 300);
-    this.onClick = this.onClick.bind(this);
-  }
-
-  handleChange(evt) {
-    const query = evt.currentTarget.value.trim();
-    this.setState({ query });
-    if (query) {
-      this.startSearch();
-    } else {
-      this.clearSearch();
-    }
-  }
-
-  clearSearch() {
-    this.setState({
-      maybeRequest: null,
-      maybeResults: null,
-      projectName: '',
-      excludedProjectsCount: 0,
-    });
-  }
-
-  async startSearch() {
-    if (!this.state.query) {
-      return this.clearSearch();
-    }
-
-    // reset the results
-    this.setState({ maybeResults: null });
-
-    let searchByUrl = false;
-    let { query } = this.state;
-    const collectionProjectIds = this.props.collection.projects.map((project) => project.id);
-
-    if (isUrl(query)) {
-      searchByUrl = true;
-      // check if the query is a URL or a name of a project
-      // Project URL pattern: https://add-to-alexa.glitch.me/, https://glitch.com/~add-to-alexa
-      const queryUrl = new URL(query);
-      if (queryUrl.href.includes('me') && !queryUrl.href.includes('~')) {
-        // https://add-to-alexa.glitch.me/
-        query = queryUrl.hostname.substring(0, queryUrl.hostname.indexOf('.'));
-      } else {
-        // https://glitch.com/~add-to-alexa
-        query = queryUrl.pathname.substring(queryUrl.pathname.indexOf('~') + 1);
-      }
-    }
-
-    let request = null;
-    if (!searchByUrl) {
-      request = this.props.api.get(`projects/search?q=${query}`);
-      this.setState({ maybeRequest: request });
-    } else {
-      request = this.props.api.get(`projects/${query}`);
-      this.setState({ maybeRequest: request });
-    }
-    let { data } = await request;
-
-    if (searchByUrl) {
-      data = [data];
-    }
-
-    const results = data || [];
-    const originalNumResults = results.length;
-
-    let nonCollectionResults = [];
-    if (searchByUrl) {
-      // get the single result that matches the URL exactly - check with https://community.glitch.me/
-      nonCollectionResults = results.filter((result) => result && result.domain === query);
-
-      // check if the project is already in the collection
-      if (nonCollectionResults.length > 0 && collectionProjectIds.includes(nonCollectionResults[0].id)) {
-        nonCollectionResults = [];
-        this.setState({ projectName: query });
-      }
-    } else {
-      // user is searching by project name  - filter out any projects currently in the collection
-      nonCollectionResults = results ? results.filter((result) => !collectionProjectIds.includes(result.id)) : [];
-
-      if (nonCollectionResults.length !== originalNumResults) {
-        if (originalNumResults === 1) {
-          // the single search result is already in the collection
-          this.setState({ projectName: query });
-        } else {
-          // multiple projects have been excluded from the search results
-          this.setState({ excludedProjectsCount: originalNumResults });
-        }
-      }
-    }
-
-    this.setState(({ maybeRequest }) => {
-      if (request === maybeRequest) {
-        return {
-          maybeRequest: null,
-          maybeResults: nonCollectionResults,
-          recentProjects: null,
-        };
-      }
-      return {};
-    });
-    return null;
-  }
-
-  render() {}
-}
-
 AddCollectionProjectPop.propTypes = {
-  api: PropTypes.func.isRequired,
   collection: PropTypes.object.isRequired,
   initialProjects: PropTypes.array.isRequired,
   addProjectToCollection: PropTypes.func.isRequired,
-  togglePopover: PropTypes.func, // required but added dynamically
-};
-
-AddCollectionProjectPop.defaultProps = {
-  togglePopover: null,
+  togglePopover: PropTypes.func.isRequired,
 };
 
 const useTeamProjects = createAPIHook(async (api, teamId) => {
@@ -278,7 +127,6 @@ const useTeamProjects = createAPIHook(async (api, teamId) => {
 
 function AddCollectionProject({ collection, addProjectToCollection }) {
   const teamResponse = useTeamProjects(collection.teamId);
-  const api = useAPI();
   const { currentUser } = useCurrentUser();
 
   let initialProjects = [];
@@ -292,7 +140,6 @@ function AddCollectionProject({ collection, addProjectToCollection }) {
     <PopoverWithButton buttonClass="add-project" buttonText="Add Project">
       {({ togglePopover }) => (
         <AddCollectionProjectPop
-          api={api}
           collection={collection}
           initialProjects={initialProjects.slice(0, 20)}
           addProjectToCollection={addProjectToCollection}
