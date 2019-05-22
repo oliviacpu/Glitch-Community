ded31c8313c15a4d693c9229d81b36984937337e
Glitch (community-staging) <none>
2019-05-22 13:51:53 +0000

    Refactor existing super user banner to move logic into currentUser

--- src/components/banners/super-user.js
+++ src/components/banners/super-user.js
@@ -1,36 +1,30 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 
-import { useCurrentUser } from '../../state/current-user';
-import useLocalStorage from '../../state/local-storage';
-import { useAPI } from '../../state/api';
+import { useCurrentUser } from 'State/current-user';
+
+import useLocalStorage from 'State/local-storage';
 
 import styles from './super-user.styl';
 
 const cx = classNames.bind(styles);
 
 const SuperUserBanner = () => {
-  const { currentUser, persistentToken } = useCurrentUser();
-  const api = useAPI();
+  const { superUserHelpers } = useCurrentUser();
+  const { superUserFeature, canBecomeSuperUser, toggleSuperUser } = superUserHelpers;
   const [showSupportBanner, setShowSupportBanner] = useLocalStorage('showSupportBanner', false);
-  const canBecomeSuperUser = currentUser && currentUser.projects && currentUser.projects.filter((p) => p.id === 'b9f7fbdd-ac07-45f9-84ea-d484533635ff').length > 0;
-  const superUser = currentUser.features && currentUser.features.find((feature) => feature.name === 'super_user');
 
-  if (persistentToken && (superUser || canBecomeSuperUser)) {
-    const expirationDate = superUser && new Date(superUser.expiresAt).toLocaleString();
-    const displayText = `SUPER USER MODE ${superUser ? `ENABLED UNTIL: ${expirationDate}` : 'DISABLED'} `;
-    const toggleSuperUser = async () => {
-      await api.post(`https://support-toggle.glitch.me/support/${superUser ? 'disable' : 'enable'}`);
-      window.location.reload();
-    };
+  if (superUserFeature || canBecomeSuperUser) {
+    const expirationDate = superUserFeature && new Date(superUserFeature.expiresAt).toLocaleString();
+    const displayText = `SUPER USER MODE ${superUserFeature ? `ENABLED UNTIL: ${expirationDate}` : 'DISABLED'} `;
 
-    if (superUser || showSupportBanner) {
-      const className = cx({ container: true, isDisabled: !superUser });
+    if (superUserFeature || showSupportBanner) {
+      const className = cx({ container: true, isDisabled: !superUserFeature });
       return (
         <div className={className}>
           {displayText}
-          <button onClick={toggleSuperUser}>Click to {superUser ? 'disable' : 'enable'}</button>
-          {!superUser && <button onClick={() => setShowSupportBanner(false)}>Hide</button>}
+          <button onClick={toggleSuperUser}>Click to {superUserFeature ? 'disable' : 'enable'}</button>
+          {!superUserFeature && <button onClick={() => setShowSupportBanner(false)}>Hide</button>}
         </div>
       );
     }
