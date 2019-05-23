547d2176642c3be7b94e8acefaa7f6bfbd652f81
Glitch (community-staging) <none>
2019-05-23 17:09:06 +0000

    🛎👔 Checkpoint ./src/presenters/pop-overs/delete-team-pop.js:271885/5687

--- src/presenters/pop-overs/delete-team-pop.js
+++ src/presenters/pop-overs/delete-team-pop.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import Loader from 'Components/loader';
-import { PopoverWithButton, PopoverDialog, ActionDescription } from 'Components/popover';
+import { PopoverWithButton, PopoverDialog, Pop ActionDescription } from 'Components/popover';
 import Button from 'Components/buttons/button';
 import Emoji from 'Components/images/emoji';
 import Image from 'Components/images/image';
@@ -12,7 +12,7 @@ import { useAPI } from 'State/api';
 
 const illustration = 'https://cdn.glitch.com/c53fd895-ee00-4295-b111-7e024967a033%2Fdelete-team.svg?1531267699621';
 
-const DeleteTeamPop = withRouter(({ history, team, focusFirstElement }) => {
+const DeleteTeamPop = withRouter(({ history, team }) => {
   const api = useAPI();
   const { createErrorNotification } = useNotifications();
   const [teamIsDeleting, setTeamIsDeleting] = useState(false);
@@ -32,9 +32,7 @@ const DeleteTeamPop = withRouter(({ history, team, focusFirstElement }) => {
 
   return (
     <PopoverDialog focusOnDialog align="left">
-      <PopoverTitle>
-        Delete {team.name}
-      </PopoverTitle>
+      <PopoverTitle>Delete {team.name}</PopoverTitle>
       <PopoverActions>
         <Image width="110px" src={illustration} alt="" />
         <ActionDescription>
@@ -43,7 +41,7 @@ const DeleteTeamPop = withRouter(({ history, team, focusFirstElement }) => {
       </PopoverActions>
       <PopoverActions type="dangerZone">
         <Button size="small" type="dangerZone" onClick={deleteTeam}>
-          Delete {team.name} <Emoji name="bomb"/>
+          Delete {team.name} <Emoji name="bomb" />
           {teamIsDeleting && <Loader />}
         </Button>
       </PopoverActions>
@@ -59,13 +57,12 @@ const DeleteTeamPop = withRouter(({ history, team, focusFirstElement }) => {
 
 DeleteTeamPop.propTypes = {
   team: PropTypes.object.isRequired,
-  focusFirstElement: PropTypes.func.isRequired,
 };
 
 const DeleteTeam = ({ team }) => (
   <section>
     <PopoverWithButton
-      buttonProps={{ size: "small", type: "dangerZone"}}
+      buttonProps={{ size: 'small', type: 'dangerZone' }}
       buttonText={
         <>
           Delete {team.name}
