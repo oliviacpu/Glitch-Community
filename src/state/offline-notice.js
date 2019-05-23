3405e6c659443a29650ca531acd4fbd4957a9331
Glitch (nova-chance) <none>
2019-05-23 18:05:04 +0000

    🐾🚛 Checkpoint ./src/state/api.js:97325/8 ./src/state/offline-notice.js:97325/327

---
+++ src/state/offline-notice.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+
+import { useNotifications } from '../presenters/notifications';
+
+const PersistentNotification = ({ children, className }) => {
+  const { createPersistentNotification } = useNotifications();
+  
+  useEffect(() => {
+    const {} = create
+  })
+  
+  return null;
+};
