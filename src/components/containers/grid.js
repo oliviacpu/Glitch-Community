e72005725111f232db655ca58e3f63ef517a15d4
Glitch (superb-guide) <none>
2019-05-24 15:11:16 +0000

    🐛👸 Checkpoint ./src/components/containers/grid.js:97325/2331

--- src/components/containers/grid.js
+++ src/components/containers/grid.js
@@ -4,6 +4,37 @@ import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 import classnames from 'classnames';
 import styles from './grid.styl';
 
+const GridContainer = ({ children, gap, minWidth, className, style }) => (
+  <ul
+    className={classnames(styles.grid, className)}
+    style={{
+      ...style,
+      '--row-gap': (gap && gap.row) || gap,
+      '--column-gap': (gap && gap.column) || gap,
+      '--min-width': minWidth,
+    }}
+  >
+    {children}
+  </ul>
+);
+
+GridContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  style: PropTypes.object,
+  gap: PropTypes.oneOfType([
+    PropTypes.shape({ row: PropTypes.node.isRequired, column: PropTypes.node.isRequired }).isRequired,
+    PropTypes.node.isRequired,
+  ]),
+  minWidth: PropTypes.node,
+};
+
+GridContainer.defaultProps = {
+  className: '',
+  style: {},
+  gap: undefined,
+  minWidth: undefined,
+};
 
 const GridItem = ({ children }) => (
   <li className={styles.item} tabIndex={0}>
@@ -15,29 +46,32 @@ GridItem.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
+const SortableGridContainer = SortableContainer(GridContainer);
 const SortableGridItem = SortableElement(GridItem);
 
-const Grid = ({ items, children, gap, minWidth, className, style }) => (
-  <ul
-    className={classnames(styles.grid, className)}
-    style={{
-      ...style,
-      '--row-gap': (gap && gap.row) || gap,
-      '--column-gap': (gap && gap.column) || gap,
-      '--min-width': minWidth,
-    }}
-  >
-    {items.map((item, index) => (
-      <SortableGridItem key={item.id} index={index}>
-        {children(item)}
-      </SortableGridItem>
-    ))}
-  </ul>
-);
+const Grid = ({ items, children, sortable, ...props }) => {
+  if (sortable) {
+    return (
+      <SortableGridContainer {...props} axis="xy">
+        {items.map((item, index) => (
+          <SortableGridItem key={item.id} index={index}>
+            {children(item)}
+          </SortableGridItem>
+        ))}
+      </SortableGridContainer>
+    );
+  }
+  return (
+    <GridContainer {...props}>
+      {items.map((item) => <GridItem key={item.id}>{children(item)}</GridItem>)}
+    </GridContainer>
+  );
+};
 
 Grid.propTypes = {
   items: PropTypes.arrayOf(PropTypes.shape({ id: PropTypes.node.isRequired })).isRequired,
   children: PropTypes.func.isRequired,
+  sortable: PropTypes.bool,
   className: PropTypes.string,
   style: PropTypes.object,
   gap: PropTypes.oneOfType([
@@ -48,12 +82,11 @@ Grid.propTypes = {
 };
 
 Grid.defaultProps = {
+  sortable: false,
   className: '',
   style: {},
   gap: undefined,
   minWidth: undefined,
 };
 
-const SortableGrid = SortableContainer(Grid);
-
-export default (props) => <SortableGrid axis="xy" {...props} />;
+export default Grid;
