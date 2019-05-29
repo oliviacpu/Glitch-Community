e15f7966427030422a45c5ea5c660855bcf4488e
Glitch (community-staging) <none>
2019-05-29 14:12:29 +0000

    🦀🏰 Checkpoint ./src/presenters/pop-overs/add-team-project-pop.js:271885/23565

--- src/presenters/pop-overs/add-team-project-pop.js
+++ src/presenters/pop-overs/add-team-project-pop.js
@@ -10,61 +10,21 @@ class AddTeamProjectPop extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      templateProjects: [],
       filteredProjects: [],
-      source: 'my-projects', // my-projects, templates
       filterPlaceholder: 'Filter my projects',
     };
     this.onClick = this.onClick.bind(this);
     this.updateFilter = this.updateFilter.bind(this);
-    this.filterInputIsBlank = this.filterInputIsBlank.bind(this);
     this.filterInput = React.createRef();
+    this.filterProjects = this.filterProjects.bind(this);
   }
 
   componentDidMount() {
-    this.getTemplateProjects();
     this.filterInput.current.focus();
-    this.updateFilter('');
+    // this.updateFilter('');
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.source !== this.state.source) {
-      this.updateFilter('');
-      this.filterInput.current.focus();
-    }
-  }
-
-  onClick(event, project) {
-    event.preventDefault();
-    this.props.togglePopover();
-    this.props.addProject(project);
-  }
-
-  getTemplateProjects() {
-    const templateIds = [
-      '9cd48134-1624-48f5-beaf-6c1b68bd9217', // 'timelink'
-      '712cc905-bfcb-454e-a47a-c729ab63c455', // 'poller'
-      '929980a8-32fc-4ae7-a66f-dddb3ae4912c', // 'hello-webpage'
-    ];
-    const projectsPath = `projects/byIds?ids=${templateIds.join(',')}`;
-    this.props.api.get(projectsPath).then(({ data }) => {
-      const projects = this.normalizeTemplateProjects(data);
-      this.setState({
-        templateProjects: projects,
-      });
-      this.updateFilter('');
-    });
-  }
-
-  normalizeTemplateProjects = (data) => {
-    const projects = data.map((project) => {
-      project.users = [];
-      return project;
-    });
-    return projects;
-  };
-
-  filterProjects = (query, projects, teamProjects) => {
+  filterProjects(query, projects, teamProjects) {
     query = query.toLowerCase().trim();
     const MAX_PROJECTS = 20;
     const teamProjectIds = teamProjects.map(({ id }) => id);
@@ -85,68 +45,28 @@ class AddTeamProjectPop extends React.Component {
       }
     }
     return filteredProjects;
-  };
-
-  updateFilter(query) {
-    let projects = [];
-    if (this.state.source === 'templates') {
-      projects = this.state.templateProjects;
-    } else {
-      projects = this.props.myProjects;
-    }
-    const filteredProjects = this.filterProjects(query, projects, this.props.teamProjects);
-    this.setState({
-      filteredProjects,
-    });
-  }
-
-  activeIfSourceIsTemplates() {
-    if (this.state.source === 'templates') {
-      return 'active';
-    }
-    return null;
-  }
-
-  activeIfSourceIsMyProjects() {
-    if (this.state.source === 'my-projects') {
-      return 'active';
-    }
-    return null;
-  }
-
-  async remixTemplate(projectId) {
-    const remixTemplatePath = `projects/${projectId}/remix`;
-    return this.props.api.post(remixTemplatePath);
-  }
-
-  async inviteUserToRemix(data) {
-    const inviteUserPath = `projects/${data.inviteToken}/join`;
-    return this.props.api.post(inviteUserPath);
-  }
-
-  sourceIsTemplates() {
-    this.setState({
-      source: 'templates',
-      filterPlaceholder: 'Filter templates',
-    });
-  }
-
-  sourceIsMyProjects() {
-    this.setState({
-      source: 'my-projects',
-      filterPlaceholder: 'Filter projects',
-    });
-  }
-
-  filterInputIsBlank() {
-    if (this.filterInput.current.value.length === 0) {
-      return true;
-    }
-    return false;
   }
 
   render() {
     const { filteredProjects } = this.state;
+
+    function filterInputIsBlank() {
+      return this.filterInput.current.value.length === 0;
+    }
+
+    function updateFilter(query) {
+      const projects = this.props.myProjects;
+      const filteredProjects = this.filterProjects(query, projects, this.props.teamProjects);
+      this.setState({
+        filteredProjects,
+      });
+    }
+    
+    function onClick(event, project) {
+      event.preventDefault();
+      this.props.togglePopover();
+      this.props.addProject(project);
+    }
 
     return (
       <dialog className="pop-over add-team-project-pop wide-pop">
@@ -154,7 +74,7 @@ class AddTeamProjectPop extends React.Component {
           <input
             ref={this.filterInput}
             onChange={(event) => {
-              this.updateFilter(event.target.value);
+              updateFilter(event.target.value);
             }}
             id="team-project-search"
             className="pop-over-input search-input pop-over-search"
@@ -164,7 +84,7 @@ class AddTeamProjectPop extends React.Component {
           />
         </section>
 
-        <section className="pop-over-actions results-list" data-source="templates">
+        <section className="pop-over-actions results-list">
           <ul className="results">
             {filteredProjects.map((project) => (
               <li key={project.id}>
@@ -177,7 +97,7 @@ class AddTeamProjectPop extends React.Component {
               </li>
             ))}
           </ul>
-          {this.state.filteredProjects.length === 0 && this.filterInputIsBlank && (
+          {this.state.filteredProjects.length === 0 && filterInputIsBlank() && (
             <p className="action-description no-projects-description">Create or Join projects to add them to the team</p>
           )}
         </section>
