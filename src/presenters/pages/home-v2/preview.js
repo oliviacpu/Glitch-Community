5b2b92aa796ab9a49e6e7f0f4cc75ede70cebcf8
Glitch (smiling-truck) <none>
2019-05-31 21:20:59 +0000

    🙇🌵 Checkpoint ./src/presenters/pages/home-v2/preview.js:271885/4322 ./src/presenters/project-editor.js:271885/6

--- src/presenters/pages/home-v2/preview.js
+++ src/presenters/pages/home-v2/preview.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import { pick } from 'lodash';
 import { withRouter } from 'react-router-dom';
 
@@ -7,133 +8,9 @@ import Button from 'Components/buttons/button';
 import { useAPI } from 'State/api';
 import { getAllPages, allByKeys } from 'Shared/api';
 
-import { featuredCollections } from '../../../curated/collections';
-import featuredProjects from '../../../curated/featured';
-import featuredEmbed from '../../../curated/featured-embed';
-import unifiedStories from '../../../curated/unified-stories';
-import { featureCallouts, buildingOnGlitch } from '../../../curated/home-features';
-
 import Layout from '../../layout';
 import { Home } from './index';
 import styles from './styles.styl';
-
-const userProps = [
-  'id',
-  'avatarUrl',
-  'avatarThumbnailUrl',
-  'login',
-  'name',
-  'location',
-  'color',
-  'description',
-  'hasCoverImage',
-  'coverColor',
-  'thanksCount',
-  'utcOffset',
-];
-const trimUserProps = (user) => pick(user, userProps);
-
-async function getCultureZine() {
-  // TODO: this can be fetched fresh here instead of being cached by the home page
-  return window.ZINE_POSTS.map((post) => ({
-    id: post.id,
-    title: post.title,
-    url: post.url,
-    img: post.feature_image,
-    source: post.primary_tag.name,
-  }));
-}
-
-async function getUniqueUsersInProjects(api, projects, maxCount) {
-  const users = {};
-  for await (const project of projects) {
-    const projectUsers = await getAllPages(api, `v1/projects/by/id/users?id=${project.id}&limit=100`);
-    for (const user of projectUsers) {
-      users[user.id] = user;
-    }
-    if (Object.keys(users).length > maxCount) break;
-  }
-  return Object.values(users);
-}
-
-async function getFeaturedCollections(api) {
-  const fullUrls = featuredCollections.map(({ owner, name }) => `fullUrl=${owner}/${name}`).join('&');
-  const { data: collections } = await api.get(`/v1/collections/by/fullUrl?${fullUrls}`);
-
-  // TODO: where should this actually be configured?
-  const styleNames = ['wavey', 'diagonal', 'triangle'];
-
-  const collectionsWithData = featuredCollections.map(async ({ owner, name, title, description, style }, i) => {
-    const fullUrl = `${owner}/${name}`;
-    const collection = collections[fullUrl];
-    const projects = await getAllPages(api, `/v1/collections/by/fullUrl/projects?fullUrl=${fullUrl}&limit=100`);
-    const users = await getUniqueUsersInProjects(api, projects, 5);
-
-    return {
-      title: title || collection.name,
-      description: description || collection.description,
-      fullUrl,
-      users: users.map(trimUserProps),
-      count: projects.length,
-      collectionStyle: style || styleNames[i],
-    };
-  });
-
-  return Promise.all(collectionsWithData);
-}
-
-async function getFeaturedProjects(api) {
-  const projectsWithDomains = featuredProjects.map((project) => ({ ...project, domain: project.link.split('~')[1] }));
-  const domains = projectsWithDomains.map((p) => `domain=${p.domain}`).join('&');
-  const { data: projectData } = await api.get(`/v1/projects/by/domain?${domains}`);
-  const projectsWithData = projectsWithDomains.map(async ({ title, img, domain, description }) => {
-    const project = projectData[domain];
-    const users = await getAllPages(api, `/v1/projects/by/domain/users?domain=${domain}&limit=100`);
-    return {
-      domain,
-      title,
-      img,
-      users: users.slice(0, 10).map(trimUserProps),
-      description: description || project.description,
-    };
-  });
-  return Promise.all(projectsWithData);
-}
-
-async function getFeaturedEmbed(api) {
-  const users = await getAllPages(api, `/v1/projects/by/domain/users?domain=${featuredEmbed.appDomain}&limit=100`);
-  return {
-    domain: featuredEmbed.appDomain,
-    title: featuredEmbed.title,
-    description: featuredEmbed.body,
-    users: users.slice(0, 10).map(trimUserProps),
-  };
-}
-
-async function getUnifiedStories() {
-  return unifiedStories;
-}
-
-async function getFeatureCallouts() {
-  return featureCallouts.map((callout) => ({ ...callout, id: callout.href }));
-}
-
-async function getBuildingOnGlitch() {
-  return buildingOnGlitch;
-}
-
-async function getHomeData(api) {
-  const data = await allByKeys({
-    cultureZine: getCultureZine(api),
-    curatedCollections: getFeaturedCollections(api),
-    appsWeLove: getFeaturedProjects(api),
-    featuredEmbed: getFeaturedEmbed(api),
-    unifiedStories: getUnifiedStories(api),
-    featureCallouts: getFeatureCallouts(api),
-    buildingOnGlitch: getBuildingOnGlitch(api),
-  });
-  return data;
-}
 
 const PreviewBanner = withRouter(({ history, data }) => {
   const api = useAPI();
@@ -157,7 +34,7 @@ const PreviewBanner = withRouter(({ history, data }) => {
 
 const HomePreview = () => (
   <Layout>
-    <DataLoader get={getHomeData}>
+    <DataLoader get={() => axios.get('https://ablaze-peacock.glitch.me/').then(res => res.data)}>
       {(data) => (
         <>
           <PreviewBanner data={data} />
