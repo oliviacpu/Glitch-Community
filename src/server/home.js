4f326dceb0d5832ab5071a9e4d64825f63dbde43
Glitch (smiling-truck) <none>
2019-05-31 18:24:55 +0000

    🌏🏎 Checkpoint ./src/server/home.js:271885/1065

--- src/server/home.js
+++ src/server/home.js
@@ -1,8 +1,11 @@
 const fs = require('fs').promises;
+const path = require('path');
 const axios = require('axios');
 
 const { API_URL } = require('./constants').current;
-const { getAllPages } = require('Shared/api');
+const { getSingleItem, getAllPages } = require('Shared/api');
+
+const GLITCH_TEAM_ID = 74;
 
 const api = axios.create({
   baseURL: API_URL,
@@ -10,5 +13,8 @@ const api = axios.create({
 });
 
 async function saveHomeDataToFile ({ data, persistentToken }) {
+  const teams = await getAllPages(api, `/v1/users/by/persistentToken/teams?persistentToken={persistentToken}&limit=100`)
+  if (!teams.some(team => team.id === GLITCH_TEAM_ID)) throw new Error('Forbidden')
   
+  await fs.writeFile(path.join(__dirname, '../src/curated/home.json'), JSON.stringify(data), { encoding: 'utf8' })
 }
