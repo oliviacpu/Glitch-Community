df2b207f7da3da93ca3373ca12d2dfeeb9645a65
Glitch (community-staging) <none>
2019-05-31 17:40:55 +0000

    🌅🐀 Checkpoint ./server/home.js:271885/141 ./src/presenters/pages/home-v2/preview.js:271885/158 ./server/routes.js:271885/217 ./src/curated/collections.js:271885/91

--- src/curated/collections.js
+++ src/curated/collections.js
@@ -2,11 +2,13 @@
 // Collections shown on the homepage and in the more ideas area
 
 // What collections to show in the expanded area on the homepage
-export const featuredCollections = [
+const featuredCollections = [
   { owner: 'glitch', name: 'glitch-this-week-may-29-2019' },
   { owner: 'glitch', name: 'draw-with-music' },
   { owner: 'glitch', name: 'colorful-creations' }
 ];
 
 // More ideas is populated from this team
-export const moreIdeasTeam = 'glitch';
+const moreIdeasTeam = 'glitch';
+
+module.exports = { featuredCollections, moreIdeasTeam }
