6ca230fd49a9452735a8a495250e49d04f54abbb
Glitch (community-staging) <none>
2019-05-31 17:07:54 +0000

    🙎⏰ Checkpoint ./src/presenters/pages/home-v2/index.js:271885/10465 ./src/curated/home-features.js:271885/2325 ./src/presenters/pages/home-v2/example-data.js:271885/1970 ./src/presenters/pages/home-v2/preview.js:271885/105

--- src/presenters/pages/home-v2/example-data.js
+++ src/presenters/pages/home-v2/example-data.js
@@ -144,7 +144,7 @@ const users = {
   },
 };
 
-const featureCallout = [
+const featureCallouts = [
   {
     id: 'discover',
     label: 'Discover the best stuff on the web',
@@ -303,11 +303,29 @@ const cultureZine = [
   },
 ];
 
+const buildingOnGlitch = [
+  {
+    id: 'create',
+    href: '/create',
+    img: 'https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2FfirstWebsite.svg?1492031604409',
+    title: 'Developers',
+    description: 'Whether you’re learning how to code or building a production-level app, find out how Glitch can power your next idea.'
+  },
+  {
+    id: 'teams',
+    href: '/teams',
+    img: 'https://cdn.glitch.com/02ae6077-549b-429d-85bc-682e0e3ced5c%2Fcollaborate.svg?1540583258925',
+    title: 'Teams',
+    description: 'Collaborate on apps with your teammates, create starter apps for your next hackathon, or use Glitch for managing your classroom.'
+  }
+]
+
 export default {
-  featureCallout,
+  featureCallouts,
   unifiedStories,
   featuredEmbed,
   appsWeLove,
   curatedCollections,
   cultureZine,
+  buildingOnGlitch,
 };
