a9b23abf6d86409130061d762d089dae9ee55368
Glitch (tundra-beat) <none>
2019-06-04 21:12:53 +0000

    🌏✨ Checkpoint ./src/components/account-settings-overlay/password.js:355867/10508 ./src/components/account-settings-overlay/index.js:355867/4012 ./src/components/account-settings-overlay/password-settings.js:355867/18163

--- src/components/account-settings-overlay/password-settings.js
+++ src/components/account-settings-overlay/password-settings.js
@@ -7,13 +7,10 @@ import Emoji from 'Components/images/emoji';
 import Button from 'Components/buttons/button';
 import Badge from 'Components/badges/badge';
 import TextInput from 'Components/inputs/text-input';
-import { Overlay, OverlaySection, OverlayTitle } from 'Components/overlays';
 import PasswordStrength from 'Components/password-strength';
 import useDebouncedValue from 'Hooks/use-debounced-value';
 import { useAPI } from 'State/api';
 import { useCurrentUser } from 'State/current-user';
-
-import PopoverContainer from '../pop-overs/popover-container';
 
 // top worst passwords from Splashdata (https://en.wikipedia.org/wiki/List_of_the_most_common_passwords#cite_note-splashdata2018-10)
 // edited to only include those with at least 8-character
@@ -138,47 +135,4 @@ const PasswordSettings = () => {
   );
 };
 
-const AccountSettingsOverlay = () => {
-  const { currentUser } = useCurrentUser();
-  const primaryEmail = currentUser.emails.find((email) => email.primary);
-  return (
-    <Overlay className="account-settings-overlay">
-      <OverlaySection type="info">
-        <OverlayTitle>
-          Account Settings <Emoji name="key" />
-        </OverlayTitle>
-      </OverlaySection>
-      <div>
-        <div data-cy="account-settings-options">
-          <Button type="primary" size="small">
-            Password
-          </Button>
-          <Button type="primary" size="small">
-            Two-Factor Authentication
-          </Button>
-        </div>
-        <OverlaySection type="actions">
-          <PasswordSettings />
-        </OverlaySection>
-      </div>
-      <OverlaySection type="info">
-        <Text>
-          Email notifications are sent to <b>{primaryEmail.email}</b>
-        </Text>
-      </OverlaySection>
-    </Overlay>
-  );
-};
-
-const AccountSettings = ({ children }) => (
-  <PopoverContainer>
-    {({ visible, setVisible }) => (
-      <details onToggle={(evt) => setVisible(evt.target.open)} open={visible} className="overlay-container">
-        <summary>{children}</summary>
-        <AccountSettingsOverlay />
-      </details>
-    )}
-  </PopoverContainer>
-);
-
-export default AccountSettings;
+export default PasswordSettings;
