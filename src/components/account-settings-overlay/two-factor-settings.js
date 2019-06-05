07d6624ac92a6af44461df13524f6120aff5797a
Glitch (tundra-beat) <none>
2019-06-05 19:21:31 +0000

    🎮☂️ Checkpoint ./src/components/account-settings-overlay/two-factor-settings.js:355867/917 ./package.json:355867/7284

--- src/components/account-settings-overlay/two-factor-settings.js
+++ src/components/account-settings-overlay/two-factor-settings.js
@@ -1,17 +1,33 @@
 import React from 'react';
-
+import 
 import Heading from 'Components/text/heading';
 import Text from 'Components/text/text';
 import Button from 'Components/buttons/button';
 
-const TwoFactorSettings = () => (
-  <>
-    <Heading tagName="h2">Two-Factor Authentication</Heading>
-    <Text>Protect your account with an additional layer of security.</Text>
-    <Button type="tertiary" onClick={() => console.log('hi')}>
-      Enable Authenticator App
-    </Button>
-  </>
-);
+import { useAPI } from 'State/api';
+
+const TwoFactorSettings = () => {
+  const api = useAPI();
+
+  const generateSecret = async (evt) => {
+    evt.preventDefault();
+    try {
+      const response = await api.post('user/tfa/generateSecret');
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <Heading tagName="h2">Two-Factor Authentication</Heading>
+      <Text>Protect your account with an additional layer of security.</Text>
+      <Button type="tertiary" onClick={generateSecret}>
+        Enable Authenticator App
+      </Button>
+    </>
+  );
+};
 
 export default TwoFactorSettings;
