f1e95da5288f988c40c6784c7b4b495433c4d641
Glitch (community-staging) <none>
2019-06-06 14:35:21 +0000

    🛣👌 Checkpoint ./src/presenters/notifications.js:301412/11965

--- src/presenters/notifications.js
+++ src/presenters/notifications.js
@@ -14,17 +14,21 @@ const Notification = ({ children, className, remove }) => (
   </aside>
 );
 
-function Notifications () {
+export function Notifications(props) {
   const [notifications, setNotifications] = useState([]);
-  
+
   const create = (content, className = '') => {
     const notification = {
       id: `${Date.now()}{Math.random()}`,
       className,
       content,
     };
-    setNotifications(prevNotifications => [...prevNotifications, notification]);
+    setNotifications((prevNotifications) => [...prevNotifications, notification]);
     return notification.id;
+  };
+
+  const remove = (id) => {
+    setNotifications((prevNotifications) => prevNotifications.filter((n) => n.id !== id));
   };
 
   const createError = (content = 'Something went wrong. Try refreshing?') => {
@@ -34,7 +38,7 @@ function Notifications () {
   const createPersistent = (content, className = '') => {
     const id = create(content, `notifyPersistent ${className}`);
     const updateNotification = (updatedContent) => {
-      setNotifications(prevNotifications => prevNotifications.map((n) => (n.id === id ? { ...n, updatedContent } : n)));
+      setNotifications((prevNotifications) => prevNotifications.map((n) => (n.id === id ? { ...n, updatedContent } : n)));
     };
     const removeNotification = () => {
       remove(id);
@@ -43,27 +47,21 @@ function Notifications () {
       updateNotification,
       removeNotification,
     };
-  }
-
-  const remove = (id) => {
-    this.setState(({ notifications }) => ({
-      notifications: notifications.filter((n) => n.id !== id),
-    }));
-  }
+  };
 
   const funcs = {
-    createNotification: this.create.bind(this),
-    createPersistentNotification: this.createPersistent.bind(this),
-    createErrorNotification: this.createError.bind(this),
+    createNotification: create,
+    createPersistentNotification: createPersistent,
+    createErrorNotification: createError,
   };
 
   return (
     <>
-      <Provider value={funcs}>{this.props.children}</Provider>
+      <Provider value={funcs}>{props.children}</Provider>
       {!!notifications.length && (
         <div className="notifications">
           {notifications.map(({ id, className, content }) => (
-            <Notification key={id} className={className} remove={this.remove.bind(this, id)}>
+            <Notification key={id} className={className} remove={() => remove(id)}>
               {content}
             </Notification>
           ))}
