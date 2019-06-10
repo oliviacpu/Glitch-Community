a36f64286fa3792e4f391a3948e74cab91839a51
Glitch (community-staging) <none>
2019-06-10 19:37:07 +0000

    💗🐔 Checkpoint ./src/presenters/pop-overs/team-analytics-time-pop.js:271885/3102

--- src/presenters/pop-overs/team-analytics-time-pop.js
+++ src/presenters/pop-overs/team-analytics-time-pop.js
@@ -1,81 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { PopoverWithButton, PopoverDialog, PopoverSection } from 'Components/popover';
+import ResultsList, { ResultItem, ResultInfo } from 'Components/containers/results-list';
 
-const TimeFrameItem = ({ selectTimeFrame, isActive, timeFrame }) => {
-  let resultClass = 'result button-unstyled';
-  if (isActive) {
-    resultClass += ' active';
-  }
-  return (
-    <button className={resultClass} onClick={selectTimeFrame} type="button">
-      <div className="result-container">
-        <div className="result-name">{timeFrame}</div>
-      </div>
-    </button>
-  );
-};
+const timeFrames = ['Last 4 Weeks', 'Last 2 Weeks', 'Last 24 Hours'].map(id => ({ id }));
 
-TimeFrameItem.propTypes = {
-  selectTimeFrame: PropTypes.func.isRequired,
-  isActive: PropTypes.bool.isRequired,
-  timeFrame: PropTypes.string.isRequired,
-};
+const TeamAnalyticsTimePop = ({ currentTimeFrame, updateTimeFrame }) => (
+  <PopoverDialog align="left">
+    <PopoverSection>
+      <ResultsList items={timeFrames}>
+        {(({ id: timeFrame }) => (
+          <ResultItem
+            onClick={() => updateTimeFrame(timeFrame)}
+            active={currentTimeFrame === timeFrame}
+          >
+            <ResultInfo>{timeFrame}</ResultInfo>
+          </ResultItem>
+        ))}
+      </ResultsList>
+    </PopoverSection>
+  </PopoverDialog>
+);
 
-const timeFrames = ['Last 4 Weeks', 'Last 2 Weeks', 'Last 24 Hours'];
+const TeamAnalyticsTimePopButton = ({ updateTimeFrame, currentTimeFrame }) => (
+  <PopoverWithButton
+    buttonProps={{ size: 'small', type: 'tertiary' }}
+    buttonText={
+      <>
+        {currentTimeFrame} <div className="down-arrow" aria-label="options" />
+      </>
+    }
+  >
+    {({ toggleAndCall }) => (
+      <TeamAnalyticsTimePop
+        updateTimeFrame={toggleAndCall(updateTimeFrame)}
+        currentTimeFrame={currentTimeFrame}
+      />
+    )}
+  </PopoverWithButton>
+);
 
-const TeamAnalyticsTimePop = (props) => {
-  const selectTimeFrame = (timeFrame) => () => {
-    props.updateTimeFrame(timeFrame);
-    props.togglePopover();
-  };
-
-  return (
-    <PopoverDialog align="left">
-      <PopoverSection>
-        <ResultsList items={ime}
-        <div className="results">
-          {timeFrames.map((timeFrame) => (
-            <TimeFrameItem
-              key={timeFrame}
-              selectTimeFrame={selectTimeFrame(timeFrame)}
-              isActive={props.currentTimeFrame === timeFrame}
-              timeFrame={timeFrame}
-            />
-          ))}
-        </div>
-      </PopoverSection>
-    </dialog>
-  );
-};
-
-TeamAnalyticsTimePop.propTypes = {
-  updateTimeFrame: PropTypes.func.isRequired,
-  currentTimeFrame: PropTypes.string.isRequired,
-  focusFirstElement: PropTypes.func.isRequired,
-};
-
-const TeamAnalyticsTimePopButton = ({ updateTimeFrame, currentTimeFrame }) => {
-  const dropdown = <div className="down-arrow" aria-label="options" />;
-  return (
-    <PopoverWithButton
-      buttonProps={{ size: 'small', type: 'tertiary' }}
-      buttonText={
-        <>
-          {currentTimeFrame} {dropdown}
-        </>
-      }
-    >
-      {({ togglePopover }) => (
-        <TeamAnalyticsTimePop
-          updateTimeFrame={updateTimeFrame}
-          currentTimeFrame={currentTimeFrame}
-          togglePopover={togglePopover}
-        />
-      )}
-    </PopoverWithButton>
-  );
-};
 
 TeamAnalyticsTimePopButton.propTypes = {
   updateTimeFrame: PropTypes.func.isRequired,
