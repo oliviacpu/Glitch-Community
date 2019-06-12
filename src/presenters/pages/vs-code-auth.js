42a13dc4365be6f5472e271b633a1054ad4ede76
Glitch (humdrum-triangle) <none>
2019-06-12 16:20:25 +0000

    🎻💸 Checkpoint ./src/presenters/pages/vs-code-auth.js:355867/3184

--- src/presenters/pages/vs-code-auth.js
+++ src/presenters/pages/vs-code-auth.js
@@ -1,19 +1,49 @@
 /**
- * Login page for Glitch OAuth
- *
- * Login via email only
- *
- * TODO: Allow login via username/password when that becomes available
- *
- * IMPORTANT: This is considered a proof of concept for OAuth. It is nearly a direct
- * copy of /pop-overs/sign-in-pop.
+ * Login page for Glitch VS Code Plugin
  */
-/* globals API_URL */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useAPI } from '../../state/api';
 import { useCurrentUser } from '../../state/current-user';
 import { captureException } from '../../utils/sentry';
+
+/* global GITHUB_CLIENT_ID, FACEBOOK_CLIENT_ID, APP_URL, API_URL */
+
+function githubAuthLink() {
+  const params = new URLSearchParams();
+  params.append('client_id', GITHUB_CLIENT_ID);
+  params.append('scope', 'user:email');
+  params.append('redirect_uri', `${APP_URL}/login/github`);
+  return `https://github.com/login/oauth/authorize?${params}`;
+}
+
+function facebookAuthLink() {
+  const params = new URLSearchParams();
+  params.append('client_id', FACEBOOK_CLIENT_ID);
+  params.append('scope', 'email');
+  params.append('redirect_uri', `${APP_URL}/login/facebook`);
+  return `https://www.facebook.com/v2.9/dialog/oauth?${params}`;
+}
+
+function googleAuthLink() {
+  const params = new URLSearchParams();
+  const callbackURL = `${APP_URL}/login/google`;
+  params.append('callbackURL', callbackURL);
+  return `${API_URL}/auth/google?${params}`;
+}
+
+function slackAuthLink() {
+  const params = new URLSearchParams();
+  const callbackURL = `${APP_URL}/login/slack`;
+  params.append('callbackURL', callbackURL);
+  return `${API_URL}/auth/slack?${params}`;
+}
+
+const SignInPopButton = ({ company, emoji, href, onClick }) => (
+  <Button href={href} onClick={onClick} size="small">
+    Sign in with {company} <Emoji name={emoji} />
+  </Button>
+);
 
 const VSCodeAuth = ({ insiders }) => {
   const api = useAPI();
@@ -21,6 +51,24 @@ const VSCodeAuth = ({ insiders }) => {
   const { persistentToken, login } = currentUser;
   const isSignedIn = persistentToken && login;
   
+  const slackAuthEnabled = useDevToggle('Slack Auth');
+  const [, setDestination] = useLocalStorage('destinationAfterAuth');
+  
+  const onClick = () =>
+    setDestination({
+      expires: dayjs()
+        .add(10, 'minutes')
+        .toISOString(),
+      to: {
+        pathname: location.pathname,
+        search: location.search,
+      },
+    });
+
+  const setDestinationAnd = (next) => () => {
+    onClick();
+    next();
+  };
   
   if (isSignedIn) {
     const scheme = insiders ? 'vscode-insiders' : 'vscode';
@@ -33,7 +81,16 @@ const VSCodeAuth = ({ insiders }) => {
     </div>;
   }
 
-  return <div>not signed in</div>;
+  return <div>
+    <SignInPopButton href={facebookAuthLink()} company="Facebook" emoji="facebook" onClick={onClick} />
+    <SignInPopButton href={githubAuthLink()} company="GitHub" emoji="octocat" onClick={onClick} />
+    <SignInPopButton href={googleAuthLink()} company="Google" emoji="google" onClick={onClick} />
+    {slackAuthEnabled && <SignInPopButton href={slackAuthLink()} company="Slack" emoji="slack" onClick={onClick} />}
+    <Button size="small" onClick={setDestinationAnd(showView.email)}>
+      Sign in with Email <Emoji name="email" />
+    </Button>
+    <SignInCodeSection onClick={setDestinationAnd(showView.signInCode)} />
+  </div>;
 };
 
 VSCodeAuth.propTypes = {
