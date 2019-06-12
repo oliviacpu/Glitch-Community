e503f5e381ab30c1160f207a9f53c1f6b33a9eb8
Glitch (humdrum-triangle) <none>
2019-06-12 21:57:38 +0000

    🌜⚡️ Checkpoint ./src/presenters/pages/vs-code-auth.js:355867/1853

--- src/presenters/pages/vs-code-auth.js
+++ src/presenters/pages/vs-code-auth.js
@@ -17,14 +17,17 @@ const VSCodeAuth = ({ insiders, openProject }) => {
 
   const redirectMessage = "You are being redirected. (If you aren't sent back to VS Code, try the \"Glitch: Sign In With Email\" command.)";
   const signInMessage = 'Please Sign In to continue.';
-
+  
+  if (isSignedIn) {
+   setTimeout(() => {
+      const scheme = insiders ? 'vscode-insiders' : 'vscode';
+      window.location.assign(`${scheme}://glitch.glitch/token?token=${persistentToken}&openProject=${openProject}`);
+    }, 3000) 
+  }
+  
   return (
     <div className={styles.content}>
       <p>{isSignedIn ? redirectMessage : signInMessage}</p>
-      {isSignedIn && setTimeout(() => {
-        const scheme = insiders ? 'vscode-insiders' : 'vscode';
-        window.location.assign(`${scheme}://glitch.glitch/token?token=${persistentToken}&openProject=${openProject}`);
-      }, 3000)}
       {!isSignedIn &&
         <PopoverContainer>
           {() => <SignInPop align="none" />}
