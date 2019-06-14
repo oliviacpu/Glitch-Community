ce3b6764ed8128d93c206d9f86e0c49e6e614769
Glitch (community-staging) <none>
2019-06-14 18:04:32 +0000

    🎻🍔 Checkpoint ./src/state/project.js:271885/10460

--- src/state/project.js
+++ src/state/project.js
@@ -1,67 +1,71 @@
-import  React, { useState, useMemo, createContext } from 'react'
+import React, { useState, useMemo, createContext } from 'react';
+import { keyBy, mapValues } from 'lodash';
 
-import { useAPI } from 'State/api'
-import { getAllPages } from 'Shared/api'
+import { useAPI } from 'State/api';
+import { getAllPages } from 'Shared/api';
 
-const ProjectContect = createContext()
+const ProjectContect = createContext();
 
-async function getMembers (api, projectId) {
+async function getMembers(api, projectId) {
   const [users, teams] = await Promise.all([
     getAllPages(api, `/v1/projects/by/id/users?id=${projectId}`),
     getAllPages(api, `/v1/projects/by/id/teams?id=${projectId}`),
   ]);
   return { users, teams };
-};
+}
 
-const loadingResponse = { status: 'loading' }
-
-const ProjectContextProvider = ({ children }) => {
-  const [projectResponses, setProjectResponses] = useState({})
-  const api = useAPI()
-  
-  function loadProjectMembers(projectIDs) {
+function loadProjectMembers(api, projectIds, setProjectResponses) {
+  // set selected projects to 'loading'
+  setProjectResponses((prev) => {
+    const next = { ...prev };
+    for (const projectId of projectIds) {
+      next[projectId] = { ...next[projectId], members: loadingResponse };
+    }
+    return next;
+  });
+  // update each project as it loads
+  projectIds.forEach(async (projectId) => {
+    const members = await getMembers(api, projectId);
     setProjectResponses((prev) => ({
       ...prev,
-      ...mapValues(keyBy(projectIDs),  () => loadingResponse)
-    })
-    
-          
-      setProjectResponses((prev) => ({
-        ...prev,
-        [projectId]: {
-          ...prev[projectId],
-          members: loadingResponse,
-        }
-      }));
-      getMembers(api, projectId).then(members => {
-        setProjectResponses((prev) => ({
-          ...prev,
-          [projectId]: {
-            ...prev[projectId],
-            members: { status: 'ready', value: members }
-          }
-        }));
-      })
-
-
-  }
-  
-  const value = useMemo(() => ({
-    getMembers: (projectId) => {
-      if (projectResponses[projectId] && projectResponses[projectId].members) {
-        return projectResponses[projectId].members;
-      }
-      loadProjectMembers([projectId]);
-      return loadingResponse;
-    },
-    reloadProjectMembers: (projectIds) => {
-      loadProjectMembers(projectIds)
-    }
-  }), [projectResponses]);
-  
-  return (
-    <ProjectContext.Provider value={value}>
-      {children}
-    </ProjectContext.Provider>
-  )
+      [projectId]: {
+        ...prev[projectId],
+        members: { status: 'ready', value: members },
+      },
+    }));
+  });
 }
+
+const loadingResponse = { status: 'loading' };
+
+const ProjectContextProvider = ({ children }) => {
+  const [projectResponses, setProjectResponses] = useState({});
+  const api = useAPI();
+
+  const value = useMemo(
+    () => ({
+      getMembers: (projectId) => {
+        if (projectResponses[projectId] && projectResponses[projectId].members) {
+          return projectResponses[projectId].members;
+        }
+        loadProjectMembers(api, [projectId], setProjectResponses);
+        return loadingResponse;
+      },
+      reloadProjectMembers: (projectIds) => {
+        loadProjectMembers(api, projectIds, setProjectResponses);
+      },
+    }),
+    [projectResponses],
+  );
+
+  return <ProjectContext.Provider value={value}>{children}</ProjectContext.Provider>;
+};
+
+export function useProjectMembers (projectId) {
+  const { getMembers } = useContext(ProjectContext);
+  const 
+}
+
+
+
+export default ProjectContextProvider;
