752106ea84cb0243b10fae060f574303bb6ccad1
Glitch (tundra-beat) <none>
2019-06-14 15:50:04 +0000

    🎡🌉 Checkpoint ./src/components/account-settings-overlay/two-factor-settings.js:97325/436

--- src/components/account-settings-overlay/two-factor-settings.js
+++ src/components/account-settings-overlay/two-factor-settings.js
@@ -17,6 +17,7 @@ const TwoFactorSettings = () => {
   const { twoFactorEnabled } = currentUser;
 
   const api = useAPI();
+  const [working, setWorking] = useState(false);
   const [secret, setSecret] = useState(undefined);
   const [code, setCode] = useState('');
   const [done, setDone] = useState(false);
@@ -24,29 +25,36 @@ const TwoFactorSettings = () => {
   const disableTwoFactor = async (evt) => {
     evt.preventDefault();
     setDone(false);
+    setWorking(true);
     try {
       await api.post('user/tfa/disable');
       await reload();
       setDone(true);
     } catch (error) {
       console.error(error);
+    } finally {
+      setWorking(false);
     }
   };
 
   const generateSecret = async (evt) => {
     evt.preventDefault();
     setDone(false);
+    setWorking(true);
     try {
       const response = await api.post('user/tfa/generateSecret');
       const qrcode = await QRCode.toDataURL(response.data.twoFactorKeyUri);
       setSecret(qrcode);
     } catch (error) {
       console.error(error);
+    } finally {
+      setWorking(false);
     }
   };
 
   const verifyCode = async (evt) => {
     evt.preventDefault();
+    setWorking(true);
     try {
       await api.post('user/tfa/verifyInitialCode', { code });
       await reload();
@@ -55,6 +63,8 @@ const TwoFactorSettings = () => {
       setDone(true);
     } catch (error) {
       console.error(error);
+    } finally {
+      setWorking(false);
     }
   };
 
@@ -62,15 +72,15 @@ const TwoFactorSettings = () => {
     <>
       <Heading tagName="h2">Two-Factor Authentication</Heading>
       <Text>Protect your account with an additional layer of security.</Text>
-      {twoFactorEnabled && <Button type="tertiary" size="small" onClick={disableTwoFactor}>Disable Authenticator App</Button>}
+      {twoFactorEnabled && <Button type="tertiary" size="small" disabled={working} onClick={disableTwoFactor}>Disable Authenticator App</Button>}
       {!twoFactorEnabled &&
         <>
-          <Button type="tertiary" size="small" disabled={!!secret} onClick={generateSecret}>Enable Authenticator App</Button>
+          <Button type="tertiary" size="small" disabled={!!secret || working} onClick={generateSecret}>Enable Authenticator App</Button>
           {secret &&
             <form className={styles.passwordForm} onSubmit={verifyCode}>
               <img alt="QR Code" src={secret} />
-              <TextInput labelText="Enter Authenticator Code" placeholder="Enter Authenticator Code" maxLength={6} value={code} onChange={setCode} />
-              <Button type="tertiary" size="small" disabled={code.length < 6} submit>Verify Initial Code</Button>
+              <TextInput labelText="Enter Authenticator Code" placeholder="Enter Authenticator Code" maxLength={6} value={code} disabled={working} onChange={setCode} />
+              <Button type="tertiary" size="small" disabled={code.length < 6 || working} submit>Verify Initial Code</Button>
             </form>
           }
         </>
