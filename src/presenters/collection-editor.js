c9c17a21a7abf7afa0717ba12a01aca07b9887e0
Glitch (community-staging) <none>
2019-06-14 20:12:05 +0000

    💿🔋 Checkpoint ./src/presenters/collection-editor.js:271885/45474

--- src/presenters/collection-editor.js
+++ src/presenters/collection-editor.js
@@ -4,7 +4,8 @@ import { useAPI } from 'State/api';
 import { useCurrentUser } from 'State/current-user';
 import useErrorHandlers from '../presenters/error-handlers';
 
-const updateCollection = (api, collection, changes) => api.patch(`collections/${collection.id}`, changes);
+export const updateCollection = (api, collection, changes) => api.patch(`collections/${collection.id}`, changes);
+export const deleteCollection = (api, collection) => api.delete(`/collections/${collection.id}`);
 
 function currentUserIsAuthor(currentUser, collection) {
   if (!currentUser) return false;
@@ -17,10 +18,10 @@ function currentUserIsAuthor(currentUser, collection) {
   return false;
 }
 
-export function useCollectionEditor (initialCollection) {
-  const [collection, setCollection] = useState(initialCollection) 
-  const { currentUser } = useCurrentUser()
-  const api = useAPI()
+export function useCollectionEditor(initialCollection) {
+  const [collection, setCollection] = useState(initialCollection);
+  const { currentUser } = useCurrentUser();
+  const api = useAPI();
   const { handleError, handleErrorForInput, handleCustomError } = useErrorHandlers();
 
   async function updateFields(changes) {
@@ -43,51 +44,6 @@ export function useCollectionEditor (initialCollection) {
     }
   }
 
-  async function removeProjectFromCollection(project) {
-    await api.patch(`collections/${collection.id}/remove/${project.id}`);
-    this.setState(({ projects }) => ({
-      projects: projects.filter((p) => p.id !== project.id),
-    }));
-  }
-
-  async function updateProjectOrder(project, filteredIndex) {
-    // the shown projects list doesn't include the featured project, bump the index to include it
-    const featuredIndex = collection.projects.findIndex((p) => p.id === collection.featuredProjectId);
-    const index = (featuredIndex >= 0 && filteredIndex > featuredIndex) ? filteredIndex + 1 : filteredIndex;
-    this.setState(({ projects }) => {
-      const sortedProjects = projects.filter((p) => p.id !== project.id);
-      sortedProjects.splice(index, 0, project);
-      return { projects: sortedProjects };
-    });
-    await api.post(`collections/${collection.id}/project/${project.id}/index/${index}`);
-  }
-
-  async function deleteCollection() {
-    await api.delete(`/collections/${collection.id}`);
-  }
-
-  async function updateNote({ note, projectId }) {
-    note = (note || "").trim();
-    await api.patch(`collections/${collection.id}/project/${projectId}`, { annotation: note });
-    this.updateProject({ note, isAddingANewNote: true }, projectId);
-  }
-
-  function displayNewNote(projectId) {
-    this.updateProject({ isAddingANewNote: true }, projectId);
-  }
-
-  function hideNote(projectId) {
-    this.updateProject({ isAddingANewNote: false }, projectId);
-  }
-
-  async function featureProject(id) {
-    if (collection.featuredProjectId) {
-      // this is needed to force an dismount of an optimistic state value of a note and to ensure the old featured collection goes where it's supposed to.
-      this.setState({ featuredProjectId: null });
-    }
-    await this.updateFields({ featuredProjectId: id });
-  }
-
   function updateProject(projectUpdates, projectId) {
     this.setState(({ projects }) => ({
       projects: projects.map((project) => {
@@ -98,23 +54,59 @@ export function useCollectionEditor (initialCollection) {
       }),
     }));
   }
-  
-  const withErrorHandler = (fn, handler) => 
+
+  const withErrorHandler = (fn, handler) => (...args) => fn(...args).catch(handler);
 
   const funcs = {
     addProjectToCollection: (project, collection) => this.addProjectToCollection(project, collection).catch(handleCustomError),
-    removeProjectFromCollection: (project) => this.removeProjectFromCollection(project).catch(handleError),
-    deleteCollection: () => this.deleteCollection().catch(handleError),
-    updateNameAndUrl: ({ name, url }) => this.updateFields({ name, url }).catch(handleErrorForInput),
-    displayNewNote: (projectId) => this.displayNewNote(projectId),
-    updateNote: ({ note, projectId }) => this.updateNote({ note, projectId }),
-    hideNote: (projectId) => this.hideNote(projectId),
-    updateDescription: (description) => this.updateFields({ description }).catch(handleErrorForInput),
-    updateColor: (color) => this.updateFields({ coverColor: color }),
-    updateProjectOrder: (project, index) => this.updateProjectOrder(project, index).catch(handleError),
-    featureProject: (id) => this.featureProject(id).catch(handleError),
+
+    removeProjectFromCollection: withErrorHandler(async (project) => {
+      await api.patch(`collections/${collection.id}/remove/${project.id}`);
+      setCollection((prev) => ({
+        ...prev,
+        projects: prrev.projects.filter((p) => p.id !== project.id),
+      }));
+    }, handleError),
+
+    deleteCollection: () => deleteCollection(api, collection).catch(handleError),
+
+    updateNameAndUrl: ({ name, url }) => updateFields({ name, url }).catch(handleErrorForInput),
+
+    displayNewNote: (projectId) => updateProject({ isAddingANewNote: true }, projectId),
+
+    updateNote: async ({ note, projectId }) => {
+      note = (note || '').trim();
+      await api.patch(`collections/${collection.id}/project/${projectId}`, { annotation: note });
+      updateProject({ note, isAddingANewNote: true }, projectId);
+    },
+
+    hideNote: (projectId) => updateProject({ isAddingANewNote: false }, projectId),
+
+    updateDescription: (description) => updateFields({ description }).catch(handleErrorForInput),
+
+    updateColor: (color) => updateFields({ coverColor: color }),
+
+    updateProjectOrder: withErrorHandler(async (project, filteredIndex) => {
+      // the shown projects list doesn't include the featured project, bump the index to include it
+      const featuredIndex = collection.projects.findIndex((p) => p.id === collection.featuredProjectId);
+      const index = featuredIndex >= 0 && filteredIndex > featuredIndex ? filteredIndex + 1 : filteredIndex;
+      setCollection((prev) => {
+        const sortedProjects = prev.projects.filter((p) => p.id !== project.id);
+        sortedProjects.splice(index, 0, project);
+        return { ...prev, projects: sortedProjects };
+      });
+      await api.post(`collections/${collection.id}/project/${project.id}/index/${index}`);
+    }, handleError),
+
+    featureProject: withErrorHandler(async (projectId) => {
+      if (collection.featuredProjectId) {
+        // this is needed to force an dismount of an optimistic state value of a note and to ensure the old featured collection goes where it's supposed to.
+        setCollection((prev) => ({ ...prev, featuredProjectId: null }));
+      }
+      await updateFields({ featuredProjectId: projectId });
+    }, handleError),
+
     unfeatureProject: () => updateFields({ featuredProjectId: null }).catch(handleError),
   };
   return [collection, funcs, currentUserIsAuthor(currentUser, collection)];
-  
 }
