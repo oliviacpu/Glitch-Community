4db9190cb840312ed40b1e333eaee933510c5be1
Glitch (community-staging) <none>
2019-06-14 17:42:32 +0000

    🕍📼 Checkpoint ./src/state/project.js:271885/1941

--- src/state/project.js
+++ src/state/project.js
@@ -1,8 +1,44 @@
-import  React, { useReducer, createContext } from 'react'
+import  React, { useState, useMemo, createContext } from 'react'
+import { getAllPages } from 'Shared/api'
 
 const ProjectContect = createContext()
 
-const ProjectContextProvider = () => {
-  const 
+async function getMembers (api, projectId) {
+  const [users, teams] = await Promise.all([
+    getAllPages(api, `/v1/projects/by/id/users?id=${projectId}`),
+    getAllPages(api, `/v1/projects/by/id/teams?id=${projectId}`),
+  ]);
+  return { users, teams };
+};
+
+const loadingResponse = { status: 'loading' }
+
+const ProjectContextProvider = ({ children }) => {
+  const [projectResponses, setProjectResponses] = useState({})
   
+  const value = useMemo(() => {
+  
+    return {
+      getMembers: (projectId) => {
+        if (projectResponses[projectId] && projectResponses[projectId].members) {
+          return projectResponses[projectId].members;
+        }
+        setProjectResponse((prev) => ({
+          ...prev,
+          [projectId]: {
+            ...prev[projectId],
+            members: loadingResponse,
+          }
+        }))
+        
+        return loadingResponse;
+      }  
+    } 
+  }, [projectResponses]);
+  
+  return (
+    <ProjectContext.Provider value={value}>
+      {children}
+    </ProjectContext.Provider>
+  )
 }
