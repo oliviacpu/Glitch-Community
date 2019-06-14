ad1658e4cb67fc4eb284f8de5600d9e99902e81a
Glitch (tundra-beat) <none>
2019-06-14 17:50:03 +0000

    📷🎽 Checkpoint ./src/presenters/pages/reset-password.js:97325/2543

--- src/presenters/pages/reset-password.js
+++ src/presenters/pages/reset-password.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import dayjs from 'dayjs';
 
+import useLocalStorage from 'State/local-storage';
 import { EmailTokenLoginPage } from './login';
 
+const ResetPasswordLogin = ({ loginToken, resetPasswordToken }) => {
+  const [ready, setReady] = React.useState(false);
+  const [, setDestination] = useLocalStorage('destinationAfterAuth');
+  React.useEffect(() => {
+    setDestination({
+      expires: dayjs()
+        .add(10, 'minutes')
+        .toISOString(),
+      to: {
+        pathname: '/reset-password',
+        search: `resetPasswordToken=${resetPasswordToken}`,
+      },
+    });
+    setReady(true);
+  }, []);
+  return ready ? <EmailTokenLoginPage token={loginToken} /> : null;
+};
+
+const ResetPasswordForm = ({ resetPasswordToken }) => {
+};
+
 const ResetPasswordPage = ({ loginToken, resetPasswordToken }) => {
-  return <EmailTokenLoginPage token={loginToken} />;
+  return loginToken
+    ? <ResetPasswordLogin loginToken={loginToken} resetPasswordToken={resetPasswordToken} />
+    : <ResetPasswordForm loginToken={loginToken} resetPasswordToken={resetPasswordToken} />;
 };
 
 ResetPasswordPage.propTypes = {
-  loginToken: PropTypes.string.isRequired,
+  loginToken: PropTypes.string,
   resetPasswordToken: PropTypes.string.isRequired,
+};
+
+ResetPasswordPage.defaultProps = {
+  loginToken: null,
 };
 
 export default ResetPasswordPage;
