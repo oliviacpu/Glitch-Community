4352b9acf31cd322ca18a06330d3cfafec6c4db3
Glitch (community-staging) <none>
2019-06-14 20:02:03 +0000

    🍮⚾️ Checkpoint ./src/presenters/collection-editor.js:271885/4081

--- src/presenters/collection-editor.js
+++ src/presenters/collection-editor.js
@@ -1,96 +1,94 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import _ from 'lodash';
+import { useState } from 'react';
 
-import { useAPI } from '../state/api';
-import { useCurrentUser } from '../state/current-user';
-import useErrorHandlers from './error-handlers';
+import { useAPI } from 'State/api';
+import { useCurrentUser } from 'State/current-user';
+import useErrorHandlers from '../presenters/error-handlers';
 
-class CollectionEditor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ...props.initialCollection,
-    };
+const updateCollection = (api, collection, changes) => api.patch(`collections/${collection.id}`, changes);
+
+function currentUserIsAuthor(currentUser, collection) {
+  if (!currentUser) return false;
+  if (collection.teamId > 0) {
+    return currentUser.teams.some((team) => team.id === collection.teamId);
   }
-
-  currentUserIsAuthor() {
-    if (!this.props.currentUser) return false;
-    if (this.state.teamId > 0) {
-      return this.props.currentUser.teams.some((team) => team.id === this.state.teamId);
-    }
-    if (this.state.userId > 0) {
-      return this.props.currentUser.id === this.state.userId;
-    }
-    return false;
+  if (collection.userId > 0) {
+    return currentUser.id === collection.userId;
   }
+  return false;
+}
 
-  async updateFields(changes) {
+export function useCollectionEditor (initialCollection) {
+  const [collection, setCollection] = useState(initialCollection) 
+  const { currentUser } = useCurrentUser()
+  const api = useAPI()
+  const { handleError, handleErrorForInput, handleCustomError } = useErrorHandlers();
+
+  async function updateFields(changes) {
     // A note here: we don't want to setState with the data from the server from this call, as it doesn't return back the projects in depth with users and notes and things
     // maybe a sign we want to think of something a little more powerful for state management, as we're getting a little hairy here.
-    this.setState(changes);
-    await this.props.api.patch(`collections/${this.state.id}`, changes);
+    setCollection((prev) => ({ ...prev, ...changes }));
+    await updateCollection(api, collection, changes);
   }
 
-  async addProjectToCollection(project, collection) {
-    if (collection.id === this.state.id) {
+  async function addProjectToCollection(project, selectedCollection) {
+    if (selectedCollection.id === collection.id) {
       // add project to collection page
       this.setState(({ projects }) => ({
         projects: [project, ...projects],
       }));
     }
-    await this.props.api.patch(`collections/${collection.id}/add/${project.id}`);
-    if (collection.id === this.state.id) {
-      await this.props.api.post(`collections/${collection.id}/project/${project.id}/index/0`);
+    await api.patch(`collections/${selectedCollection.id}/add/${project.id}`);
+    if (selectedCollection.id === collection.id) {
+      await api.post(`collections/${selectedCollection.id}/project/${project.id}/index/0`);
     }
   }
 
-  async removeProjectFromCollection(project) {
-    await this.props.api.patch(`collections/${this.state.id}/remove/${project.id}`);
+  async function removeProjectFromCollection(project) {
+    await api.patch(`collections/${collection.id}/remove/${project.id}`);
     this.setState(({ projects }) => ({
       projects: projects.filter((p) => p.id !== project.id),
     }));
   }
 
-  async updateProjectOrder(project, filteredIndex) {
+  async function updateProjectOrder(project, filteredIndex) {
     // the shown projects list doesn't include the featured project, bump the index to include it
-    const featuredIndex = this.state.projects.findIndex((p) => p.id === this.state.featuredProjectId);
+    const featuredIndex = collection.projects.findIndex((p) => p.id === collection.featuredProjectId);
     const index = (featuredIndex >= 0 && filteredIndex > featuredIndex) ? filteredIndex + 1 : filteredIndex;
     this.setState(({ projects }) => {
       const sortedProjects = projects.filter((p) => p.id !== project.id);
       sortedProjects.splice(index, 0, project);
       return { projects: sortedProjects };
     });
-    await this.props.api.post(`collections/${this.state.id}/project/${project.id}/index/${index}`);
+    await api.post(`collections/${collection.id}/project/${project.id}/index/${index}`);
   }
 
-  async deleteCollection() {
-    await this.props.api.delete(`/collections/${this.state.id}`);
+  async function deleteCollection() {
+    await api.delete(`/collections/${collection.id}`);
   }
 
-  async updateNote({ note, projectId }) {
-    note = _.trim(note);
-    await this.props.api.patch(`collections/${this.state.id}/project/${projectId}`, { annotation: note });
+  async function updateNote({ note, projectId }) {
+    note = (note || "").trim();
+    await api.patch(`collections/${collection.id}/project/${projectId}`, { annotation: note });
     this.updateProject({ note, isAddingANewNote: true }, projectId);
   }
 
-  displayNewNote(projectId) {
+  function displayNewNote(projectId) {
     this.updateProject({ isAddingANewNote: true }, projectId);
   }
 
-  hideNote(projectId) {
+  function hideNote(projectId) {
     this.updateProject({ isAddingANewNote: false }, projectId);
   }
 
-  async featureProject(id) {
-    if (this.state.featuredProjectId) {
+  async function featureProject(id) {
+    if (collection.featuredProjectId) {
       // this is needed to force an dismount of an optimistic state value of a note and to ensure the old featured collection goes where it's supposed to.
       this.setState({ featuredProjectId: null });
     }
     await this.updateFields({ featuredProjectId: id });
   }
 
-  updateProject(projectUpdates, projectId) {
+  function updateProject(projectUpdates, projectId) {
     this.setState(({ projects }) => ({
       projects: projects.map((project) => {
         if (project.id === projectId) {
@@ -100,52 +98,23 @@ class CollectionEditor extends React.Component {
       }),
     }));
   }
+  
+  const withErrorHandler = (fn, handler) => 
 
-  render() {
-    const { handleError, handleErrorForInput, handleCustomError } = this.props;
-    const funcs = {
-      addProjectToCollection: (project, collection) => this.addProjectToCollection(project, collection).catch(handleCustomError),
-      removeProjectFromCollection: (project) => this.removeProjectFromCollection(project).catch(handleError),
-      deleteCollection: () => this.deleteCollection().catch(handleError),
-      updateNameAndUrl: ({ name, url }) => this.updateFields({ name, url }).catch(handleErrorForInput),
-      displayNewNote: (projectId) => this.displayNewNote(projectId),
-      updateNote: ({ note, projectId }) => this.updateNote({ note, projectId }),
-      hideNote: (projectId) => this.hideNote(projectId),
-      updateDescription: (description) => this.updateFields({ description }).catch(handleErrorForInput),
-      updateColor: (color) => this.updateFields({ coverColor: color }),
-      updateProjectOrder: (project, index) => this.updateProjectOrder(project, index).catch(handleError),
-      featureProject: (id) => this.featureProject(id).catch(handleError),
-      unfeatureProject: () => this.updateFields({ featuredProjectId: null }).catch(handleError),
-    };
-    return this.props.children(this.state, funcs, this.currentUserIsAuthor());
-  }
+  const funcs = {
+    addProjectToCollection: (project, collection) => this.addProjectToCollection(project, collection).catch(handleCustomError),
+    removeProjectFromCollection: (project) => this.removeProjectFromCollection(project).catch(handleError),
+    deleteCollection: () => this.deleteCollection().catch(handleError),
+    updateNameAndUrl: ({ name, url }) => this.updateFields({ name, url }).catch(handleErrorForInput),
+    displayNewNote: (projectId) => this.displayNewNote(projectId),
+    updateNote: ({ note, projectId }) => this.updateNote({ note, projectId }),
+    hideNote: (projectId) => this.hideNote(projectId),
+    updateDescription: (description) => this.updateFields({ description }).catch(handleErrorForInput),
+    updateColor: (color) => this.updateFields({ coverColor: color }),
+    updateProjectOrder: (project, index) => this.updateProjectOrder(project, index).catch(handleError),
+    featureProject: (id) => this.featureProject(id).catch(handleError),
+    unfeatureProject: () => updateFields({ featuredProjectId: null }).catch(handleError),
+  };
+  return [collection, funcs, currentUserIsAuthor(currentUser, collection)];
+  
 }
-CollectionEditor.propTypes = {
-  api: PropTypes.any.isRequired,
-  children: PropTypes.func.isRequired,
-  currentUser: PropTypes.object,
-  handleError: PropTypes.func.isRequired,
-  handleErrorForInput: PropTypes.func.isRequired,
-  initialCollection: PropTypes.object.isRequired,
-};
-
-CollectionEditor.defaultProps = {
-  currentUser: null,
-};
-
-const CollectionEditorContainer = ({ children, initialCollection }) => {
-  const { currentUser } = useCurrentUser();
-  const api = useAPI();
-  const errorFuncs = useErrorHandlers();
-  return (
-    <CollectionEditor {...{ api, currentUser, initialCollection }} {...errorFuncs}>
-      {children}
-    </CollectionEditor>
-  );
-};
-CollectionEditorContainer.propTypes = {
-  children: PropTypes.func.isRequired,
-  initialCollection: PropTypes.object.isRequired,
-};
-
-export default CollectionEditorContainer;
