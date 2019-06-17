09b85f56b2ef3a2d2e92a45517900fa9c7be853a
Glitch (community-staging) <none>
2019-06-17 17:18:15 +0000

    inline konami

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,36 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import Helmet from 'react-helmet';
+import ReactKonami from 'react-konami';
 
 import Header from 'Components/header';
 import Footer from 'Components/footer';
 
 import NewStuffContainer from '../../presenters/overlays/new-stuff';
 import ErrorBoundary from '../../presenters/includes/error-boundary';
-import Konami from '../../presenters/includes/konami';
 
 import styles from './styles.styl';
 
-const Layout = ({ children, searchQuery }) => (
-  <div className={styles.content}>
-    <Helmet title="Glitch" />
-    <NewStuffContainer>
-      {(showNewStuffOverlay) => (
-        <div className={styles.headerWrap}>
-          <Header searchQuery={searchQuery} showNewStuffOverlay={showNewStuffOverlay} />
-        </div>
-      )}
-    </NewStuffContainer>
-    <ErrorBoundary>{children}</ErrorBoundary>
-    <Footer />
-    <ErrorBoundary fallback={null}>
-      <Konami>
-        <Redirect to="/secret" push />
-      </Konami>
-    </ErrorBoundary>
-  </div>
-);
+const Layout = ({ children, searchQuery }) => {
+  const [secretActive, setSecretActive] = useState(false);
+  return (
+    <div className={styles.content}>
+      <Helmet title="Glitch" />
+      <NewStuffContainer>
+        {(showNewStuffOverlay) => (
+          <div className={styles.headerWrap}>
+            <Header searchQuery={searchQuery} showNewStuffOverlay={showNewStuffOverlay} />
+          </div>
+        )}
+      </NewStuffContainer>
+      <ErrorBoundary>{children}</ErrorBoundary>
+      <Footer />
+      <ErrorBoundary fallback={null}>
+        <ReactKonami easterEgg={() => setSecretActive(true)} />
+        {secretActive && <Redirect to="/secret" push />}
+      </ErrorBoundary>
+    </div>
+  );
+};
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
   searchQuery: PropTypes.string,
