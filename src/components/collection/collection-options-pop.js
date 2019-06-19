0bea5e6efcc4d59e6aea58bfd04a301947e45d29
Glitch (community-staging) <none>
2019-06-19 20:27:18 +0000

    ✊⛺️ Checkpoint ./src/components/collection/collection-options-pop.js:9/1253

--- src/components/collection/collection-options-pop.js
+++ src/components/collection/collection-options-pop.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { PopoverMenu, PopoverMenuButton, PopoverDialog, PopoverActions } from 'Components/popover';
+import { PopoverMenu, PopoverDialog, PopoverActions } from 'Components/popover';
 import DeleteCollection from 'Components/collection/delete-collection-pop';
 
 export default function CollectionOptions({ deleteCollection, collection }) {
   if (!deleteCollection) {
     return null;
-  }
-  function confirmThenDelete() {
-    <DeleteCollection collection={collection} />
-    if (!window.confirm('Are you sure you want to delete your collection?')) {
-      return;
-    }
-    deleteCollection(collection.id);
   }
 
   return (
@@ -20,7 +13,7 @@ export default function CollectionOptions({ deleteCollection, collection }) {
       {() => (
         <PopoverDialog focusOnDialog align="right">
           <PopoverActions type="dangerZone">
-            <PopoverMenuButton onClick={confirmThenDelete} label="Delete Collection" emoji="bomb" />
+            <DeleteCollection collection={collection} />
           </PopoverActions>
         </PopoverDialog>
       )}
