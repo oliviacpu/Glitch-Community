cce709baa8d2cf6eee712adae4d735e167517fc5
Glitch (community-staging) <none>
2019-06-21 19:49:26 +0000

    🍥🦂 Checkpoint ./.glitch-assets:301412/33280 ./src/presenters/pages/create.js:301412/350 ./src/presenters/pages/create.styl:301412/178

--- src/presenters/pages/create.js
+++ src/presenters/pages/create.js
@@ -21,16 +21,22 @@ const Mark = ({ color, children }) => (
 const Unmarked = ({ children }) => <span className={styles.unmarked}>{children}</span>;
 
 const bannerIllustration = 'https://cdn.glitch.com/50f784d9-9995-4fa4-a185-b4b1ea6e77c0%2Fillustration.svg?v=1561144073293'
+const bannerShape = 'https://cdn.glitch.com/50f784d9-9995-4fa4-a185-b4b1ea6e77c0%2Fshape-pattern.svg?v=1561146220750';
 const Banner = () => (
   <div className={styles.banner}>
-    <Heading tagName="h1">
-      <Mark color="#fbf2b8">Create</Mark><Unmarked>the app of your dreams</Unmarked>
-    </Heading>
-    <Text>
-      Whether you're new to code or an experienced developer, Glitch is the fastest tool for turning your ideas into web aps.
-    </Text>
-    <Button href={getRemixUrl(name)} type="cta">Remix Hello World</Button>
-    <Image src={bannerIllustration} alt="" />
+    <div className={styles.bannerText}>
+      <Heading tagName="h1">
+        <Mark color="#fbf2b8">Create</Mark><Unmarked>the app of your dreams</Unmarked>
+      </Heading>
+      <Text>
+        Whether you're new to code or an experienced developer, Glitch is the fastest tool for turning your ideas into web aps.
+      </Text>
+      <Button href={getRemixUrl(name)} type="cta">Remix Hello World</Button>
+    </div>
+    
+    <div className={styles.bannerIllustration}>
+      <Image src={bannerIllustration} alt="" />
+    </div>
   </div>
 )
 
