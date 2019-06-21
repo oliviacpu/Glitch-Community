77263920eae863d911c2f4db37060083b0b82a66
Glitch (community-staging) <none>
2019-06-21 19:27:27 +0000

    🌅🐑 Checkpoint ./src/presenters/pages/create.js:301412/322 ./src/presenters/pages/create.styl:301412/382

--- src/presenters/pages/create.js
+++ src/presenters/pages/create.js
@@ -8,6 +8,7 @@ import Button from 'Components/buttons/button';
 import Link from 'Components/link';
 import Embed from 'Components/project/embed';
 import Layout from 'Components/layout';
+import { getRemixUrl } from '../../models/project';
 
 import styles from './create.styl';
 
@@ -20,12 +21,13 @@ const Mark = ({ color, children }) => (
 const bannerIllustration = 'https://cdn.glitch.com/50f784d9-9995-4fa4-a185-b4b1ea6e77c0%2Fillustration.svg?v=1561144073293'
 const Banner = () => (
   <div className={styles.banner}>
-    <Heading name="h1">
+    <Heading tagName="h1">
       <Mark>Create</Mark> the app of your dreams
     </Heading>
     <Text>
       Whether you're new to code or an experienced developer, Glitch is the fastest tool for turning your ideas into web aps.
     </Text>
+    <Button href={getRemixUrl(name)} type="cta">Remix Hello World</Button>
     <Image src={bannerIllustration} alt="" />
   </div>
 )
