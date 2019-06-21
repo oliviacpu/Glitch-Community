9a7a55681a1d31b06e9644fe561e414abb4156a1
Glitch (community-staging) <none>
2019-06-21 19:16:27 +0000

    🌦👡 Checkpoint ./.glitch-assets:301412/395 ./src/presenters/pages/create.js:301412/928

--- src/presenters/pages/create.js
+++ src/presenters/pages/create.js
@@ -11,18 +11,29 @@ import Layout from 'Components/layout';
 
 import styles from './create.styl';
 
+const Mark = ({ color, children }) => (
+  <span className={styles.mark} style={{ '--mark-color': color }}>
+    <span className={styles.markText}>{children}</span>
+  </span>
+);
+
+const bannerIllustration = 'https://cdn.glitch.com/50f784d9-9995-4fa4-a185-b4b1ea6e77c0%2Fillustration.svg?v=1561144073293'
 const Banner = () => (
   <div className={styles.banner}>
-    
+    <Heading name="h1">
+      <Mark>Create</Mark> the app of your dreams
+    </Heading>
+    <Text>
+      Whether you're new to code or an experienced developer, Glitch is the fastest tool for turning your ideas into web aps.
+    </Text>
+    <Image src={bannerIllustration} alt="" />
   </div>
 )
 
 const CreatePage = () => {
   return (
     <Layout>
-      <main>
-        /create page
-      </main>
+      <Banner />
     </Layout>
   );
 };
