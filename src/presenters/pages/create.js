bf572edfa91ef9d75aa526c6ac111cf14f67ab88
Glitch (community-staging) <none>
2019-06-21 20:11:27 +0000

    🕌🎬 Checkpoint ./src/components/text/heading.js:301412/304 ./src/presenters/pages/create.js:301412/139 ./src/presenters/pages/create.styl:301412/3

--- src/presenters/pages/create.js
+++ src/presenters/pages/create.js
@@ -25,11 +25,9 @@ const bannerShape = 'https://cdn.glitch.com/50f784d9-9995-4fa4-a185-b4b1ea6e77c0
 const Banner = () => (
   <div className={styles.banner}>
     <div className={styles.bannerText}>
-      <span className={styles.bannerTagline}>
-        <Heading tagName="h1">
-          <Mark color="#fbf2b8">Create</Mark><Unmarked>the app of your dreams</Unmarked>
-        </Heading>
-      </span>
+      <Heading className={styles.bannerTagline} tagName="h1">
+        <Mark color="#fbf2b8">Create</Mark><Unmarked>the app of your dreams</Unmarked>
+      </Heading>
       <Text>
         Whether you're new to code or an experienced developer, Glitch is the fastest tool for turning your ideas into web aps.
       </Text>
