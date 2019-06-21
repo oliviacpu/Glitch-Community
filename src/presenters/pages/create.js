ed41169c6358bdf750845ef3ea8b953deb0f59f6
Glitch (community-staging) <none>
2019-06-21 22:33:29 +0000

    💪🌧 Checkpoint ./src/presenters/pages/create.js:301412/5867 ./src/presenters/pages/create.styl:301412/428

--- src/presenters/pages/create.js
+++ src/presenters/pages/create.js
@@ -20,22 +20,23 @@ const Mark = ({ color, children }) => (
 
 const Unmarked = ({ children }) => <span className={styles.unmarked}>{children}</span>;
 
-const bannerIllustration = 'https://cdn.glitch.com/50f784d9-9995-4fa4-a185-b4b1ea6e77c0%2Fillustration.svg?v=1561144073293'
+const bannerIllustration = 'https://cdn.glitch.com/50f784d9-9995-4fa4-a185-b4b1ea6e77c0%2Fillustration.svg?v=1561144073293';
 const bannerShape = 'https://cdn.glitch.com/50f784d9-9995-4fa4-a185-b4b1ea6e77c0%2Fshape-pattern.svg?v=1561146220750';
 const Banner = () => (
   <section className={styles.banner}>
     <div className={styles.bannerText}>
       <Heading className={styles.bannerTagline} tagName="h1">
-        <Mark color="#fbf2b8">Create</Mark><Unmarked>the app of your dreams</Unmarked>
+        <Mark color="#fbf2b8">Create</Mark>
+        <Unmarked>the app of your dreams</Unmarked>
       </Heading>
-      <Text>
-        Whether you're new to code or an experienced developer, Glitch is the fastest tool for turning your ideas into web apps.
-      </Text>
+      <Text>Whether you're new to code or an experienced developer, Glitch is the fastest tool for turning your ideas into web apps.</Text>
       <span className={styles.bannerRemixBtn}>
-        <Button href={getRemixUrl(name)} type="cta">Remix Hello World</Button>
+        <Button href={getRemixUrl(name)} type="cta">
+          Remix Hello World
+        </Button>
       </span>
     </div>
-    
+
     <div className={styles.bannerIllustration}>
       <Image src={bannerIllustration} alt="" />
     </div>
@@ -45,20 +46,24 @@ const Banner = () => (
 const WhatIsGlitch = () => (
   <section className={styles.whatIsGlitch}>
     <div>
-      <Heading className={styles.h2} tagName="h2"><Mark color="#d7a6f9">What is Glitch?</Mark></Heading>
+      <Heading className={styles.h2} tagName="h2">
+        <Mark color="#d7a6f9">What is Glitch?</Mark>
+      </Heading>
       <div className={styles.whatIsGlitchDescription}>
         <Text size="15px">Glitch is a collaborative programming environment that lives in your browser and deploys your code as you type.</Text>
         <Text size="15px">Use Glitch to build anything from static webpages to fullstack Node apps.</Text>
       </div>
     </div>
-    <div className={styles.whatIsGlitchVideo}></div>
+    <div className={styles.whatIsGlitchVideo} />
   </section>
 );
 
 const Starters = () => (
   <section className={styles.starters}>
+    <Heading className={styles.h2}><Mark color="#F9DB91">Starters for all developers</Mark></Heading>
     <Text fontSize="15px">
-      Remixable working apps mean you never have to start from scratch. You can even clone a git repo from services like GitHub and GitLab to make a copy and deploy on Glitch.
+      Remixable working apps mean you never have to start from scratch. You can even{' '}
+      <Link to="https://glitch.com/help/import-git/">clone a git repo from services like GitHub and GitLab</Link> to make a copy and deploy on Glitch.
     </Text>
   </section>
 );
