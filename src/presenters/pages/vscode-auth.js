d5940996c22fbdc686afe65020fe8053b505c77b
Glitch (community-staging) <none>
2019-06-26 20:30:16 +0000

    useEffect

--- src/presenters/pages/vscode-auth.js
+++ src/presenters/pages/vscode-auth.js
@@ -1,9 +1,8 @@
 /**
  * Login page for Glitch VS Code Plugin
  */
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
 
 import PopoverContainer from 'Components/popover/container';
 import Text from 'Components/text/text';
@@ -12,7 +11,7 @@ import { useCurrentUser } from 'State/current-user';
 
 import styles from './vscode-auth.styl';
 
-const VSCodeAuth = withRouter(({ insiders, openProject, history }) => {
+const VSCodeAuth = ({ insiders, openProject }) => {
   const { currentUser } = useCurrentUser();
   const { persistentToken, login } = currentUser;
   const isSignedIn = persistentToken && login;
@@ -20,25 +19,23 @@ const VSCodeAuth = withRouter(({ insiders, openProject, history }) => {
   const redirectMessage = "You are being redirected. (If you aren't sent back to VSCode, try signing in with an email code.)";
   const signInMessage = 'Please Sign In to continue.';
 
-  if (isSignedIn) {
-    setTimeout(() => {
-      const scheme = insiders ? 'vscode-insiders' : 'vscode';
-      history.push('/');
-      window.location.assign(`${scheme}://glitch.glitch/token?token=${persistentToken}&openProject=${openProject}`);
-    }, 3000);
-  }
+  useEffect(() => {
+    if (isSignedIn) {
+      setTimeout(() => {
+        const scheme = insiders ? 'vscode-insiders' : 'vscode';
+        const redirectUrl = `${scheme}://glitch.glitch/token?token=${persistentToken}&openProject=${openProject}`;
+        window.location.assign(redirectUrl);
+      }, 3000);
+    }
+  }, []);
 
   return (
     <div className={styles.content}>
       <Text>{isSignedIn ? redirectMessage : signInMessage}</Text>
-      {!isSignedIn &&
-        <PopoverContainer>
-          {() => <SignInPop align="none" />}
-        </PopoverContainer>
-      }
+      {!isSignedIn && <PopoverContainer>{() => <SignInPop align="none" />}</PopoverContainer>}
     </div>
   );
-});
+};
 
 VSCodeAuth.propTypes = {
   insiders: PropTypes.bool,
