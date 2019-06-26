0f2b8e6590dbdbd20302801236bc88d46afb8057
Glitch (smiling-truck) <none>
2019-06-26 22:00:53 +0000

    🌗💃 Checkpoint ./src/presenters/pages/home-v2/index.js:271885/3069 ./src/presenters/pages/home-v2/styles.styl:271885/567

--- src/presenters/pages/home-v2/index.js
+++ src/presenters/pages/home-v2/index.js
@@ -52,18 +52,36 @@ const Video = ({ src, poster }) => {
       )}
     </div>
   )
-} 
+}
+
+const Chrome = () => (
+  <svg viewBox="0 0 580 60" >
+    <g  stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+        <g  transform="translate(-621.000000, -148.000000)" fillRule="nonzero">
+            <g  transform="translate(621.643282, 148.087262)">
+                <g >
+                    <rect fill="#FFFFFF" x="-2.27373675e-13" y="0" width="578.550562" height="396" rx="5"></rect>
+                    <g>
+                        <path d="M5,-2.50111043e-12 L573.550562,-2.50111043e-12 C576.311986,-2.5016177e-12 578.550562,2.23857625 578.550562,5 L578.550562,57 L0,57 L0,5 C-1.2263553e-15,2.23857625 2.23857625,-2.50060316e-12 5,-2.50111043e-12 Z" id="Rectangle-Copy-10" fill="#6356C7" opacity="0.806152344"></path>
+                        <path d="M2.27373675e-13,24.2075809 L54.3951641,24.2075809 L54.3951641,11.912738 C54.3951641,9.15131422 56.6337404,6.91273797 59.3951641,6.91273797 L122.98116,6.91273797 C125.742584,6.91273797 127.98116,9.15131422 127.98116,11.912738 L127.98116,24.2075809 L578.550562,24.2075809 L578.550562,58.3511734 L2.27373675e-13,58.3511734 L2.27373675e-13,24.2075809 Z" id="Rectangle-Copy-11" fill="#EBEDFF" opacity="0.806152344"></path>
+                        <circle id="Oval" fill="#E8E8E8" cx="15.1" cy="13.755152" r="4.02860638"></circle>
+                        <circle id="Oval-Copy-3" fill="#E8E8E8" cx="27" cy="13.755152" r="4.02860638"></circle>
+                        <circle id="Oval-Copy-5" fill="#E8E8E8" cx="38.9" cy="13.755152" r="4.02860638"></circle>
+                    </g>
+                </g>
+            </g>
+        </g>
+    </g>
+</svg>
+);
 
 const BannerVideo = () => (
   <div className={styles.bannerVideoWrap}>
-    <div className={styles.bannerVideoChrome}>
-      <div className={styles.bannerVideoInnerWrap}>
-        <Video
-          poster="https://cdn.glitch.com/616994fe-f0e3-4501-89a7-295079b3cb8c%2Fjenn_poster_small.jpg?v=1561584125641"
-          src="https://cdn.glitch.com/616994fe-f0e3-4501-89a7-295079b3cb8c%2Fhomepage_v4.mp4?v=1561583730313"
-        />
-      </div>
-    </div>
+    <Chrome />
+    <Video
+      poster="https://cdn.glitch.com/616994fe-f0e3-4501-89a7-295079b3cb8c%2Fjenn_poster_small.jpg?v=1561584125641"
+      src="https://cdn.glitch.com/616994fe-f0e3-4501-89a7-295079b3cb8c%2Fhomepage_v4.mp4?v=1561583730313"
+    />
   </div>
 );
 
