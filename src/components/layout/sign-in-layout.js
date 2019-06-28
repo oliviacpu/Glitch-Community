b6f31505aade529f589d00859079b79a38c17352
Glitch (tundra-beat) <none>
2019-06-28 21:16:57 +0000

    🚊🌛 Checkpoint ./src/components/layout/sign-in-layout.styl:355867/1534 ./src/components/layout/sign-in-layout.js:355867/2003

--- src/components/layout/sign-in-layout.js
+++ src/components/layout/sign-in-layout.js
@@ -7,6 +7,56 @@ import Heading from 'Components/text/heading';
 
 import styles from './auth-layout.styl';
 
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import styles from './overlays.styl';
+
+export const Overlay = ({ children, className }) => {
+  const overlayClass = classNames(styles.overlay, className);
+  return (
+    <div className={overlayClass}>
+      {children}
+    </div>
+  );
+};
+Overlay.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  ariaLabelledBy: PropTypes.string,
+  ariaModal: PropTypes.bool,
+};
+Overlay.defaultProps = {
+  className: null,
+  ariaLabelledBy: '',
+  ariaModal: false,
+};
+
+export const OverlaySection = ({ children, type }) => {
+  const sectionClass = classNames(styles.section, styles[type]);
+  return <section className={sectionClass}>{children}</section>;
+};
+OverlaySection.propTypes = {
+  children: PropTypes.node.isRequired,
+  type: PropTypes.oneOf(['info', 'actions']).isRequired,
+};
+
+export const OverlayTitle = ({ children, id }) => (
+  <h1 className={styles.title} id={id}>
+    {children}
+  </h1>
+);
+OverlayTitle.propTypes = {
+  children: PropTypes.node.isRequired,
+  id: PropTypes.string,
+};
+OverlayTitle.defaultProps = {
+  id: '', // for aria labelled by purposes only
+};
+
+export const OverlayBackground = () => <div className={styles.overlayBackground} role="presentation" tabIndex={-1} />;
+
 const SignInLayout = ({ children }) => (
   <div className={styles.layout}>
     <div className={styles.logo}>
