e689d46cce2a95dad31959165c2e7ef31ee61bad
Glitch (community-staging) <none>
2019-07-02 19:51:42 +0000

    🏭🎬 Checkpoint ./src/components/data-loader/index.js:433767/158 ./src/presenters/pages/router.js:433767/12

--- src/components/data-loader/index.js
+++ src/components/data-loader/index.js
@@ -7,7 +7,9 @@ import { captureException } from '../../utils/sentry';
 const DataLoader = ({ children, get, renderError, renderLoader, captureException: shouldCaptureException }) => {
   const [{ status, value }, setState] = useState({ status: 'loading', value: null });
   const api = useAPI();
-  console.log("data loader is rendering")
+  if (value === null || !Array.isArray(value)) {
+    console.log("data loader is rendering and the status is", status, "and the value is", value)    
+  }
   useEffect(() => {
     console.log("inside useEffect")
     let isCurrent = true;
