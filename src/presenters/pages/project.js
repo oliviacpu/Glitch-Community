6c2b897e889623e85fb0dfe54d451017af24160a
Glitch (community-staging) <none>
2019-07-02 14:36:08 +0000

    🍕🚋 Checkpoint ./src/presenters/pages/project.js:271885/1694 ./src/presenters/pages/project.styl:271885/141

--- src/presenters/pages/project.js
+++ src/presenters/pages/project.js
@@ -153,12 +153,12 @@ const ProjectPage = ({ project: initialProject }) => {
                   placeholder="Name your project"
                 />
               </Heading>
-              <div className={styles.privateToggleWrap}><PrivateToggle isPrivate={project.private} setPrivate={updatePrivate} /></div>
+              <PrivateToggle isPrivate={project.private} setPrivate={updatePrivate} />
             </div>
           ) : (
             <div className={styles.headingWrap}>
               <Heading tagName="h1">{!currentUser.isSupport && suspendedReason ? 'suspended project' : domain}</Heading>
-              {project.private && <div className={styles.privateToggleWrap}><PrivateBadge /></div>}
+              {project.private && <PrivateBadge />}
             </div>
           )}
           {users.length + teams.length > 0 && (
