a91338ba0a0d890290f7c7fcb12beab67a54dee6
Glitch (superb-guide) <none>
2019-07-03 16:18:15 +0000

    🛀👗 Checkpoint ./stories/util.js:97325/664 ./src/components/containers/profile/story.js:97325/72

--- stories/util.js
+++ stories/util.js
@@ -1,7 +1,5 @@
 import { Context as CurrentUserContext } from 'State/current-user';
 import { Context as APIContext } from 'State/api';
-import { Context as UserPrefsContext } from 'State/user-prefs';
-import { Context as DevTogglesContext } from 'State/dev-toggles';
 
 /* eslint-disable import/prefer-default-export */
 
@@ -15,30 +13,13 @@ export const withState = (initState, Component) => {
   return () => <WrappedComponent />;
 };
 
-const log = console.log.bind(console);
-
-const defaultUserPrefs = {
-  prefs: {},
-  set: log,
-};
-
-const defaultDevToggles = {
-  enabledToggles: [],
-  toggleData: log,
-  setEnabledToggles: log,
-};
-
 export const provideContext = (
-  { userPrefs = defaultUserPrefs, devToggles = defaultDevToggles, currentUser = {}, currentUserFetched = true, api = {} } = {},
+  { currentUser = {}, currentUserFetched = true, api = {} } = {},
   Component,
 ) => () => (
-  <UserPrefsContext.Provider value={userPrefs}>
-    <DevTogglesContext.Provider value={devToggles}>
-      <CurrentUserContext.Provider value={{ currentUser, fetched: currentUserFetched }}>
-        <APIContext.Provider value={api}>
-          <Component />
-        </APIContext.Provider>
-      </CurrentUserContext.Provider>
-    </DevTogglesContext.Provider>
-  </UserPrefsContext.Provider>
+  <CurrentUserContext.Provider value={{ currentUser, fetched: currentUserFetched }}>
+    <APIContext.Provider value={api}>
+      <Component />
+    </APIContext.Provider>
+  </CurrentUserContext.Provider>
 );
