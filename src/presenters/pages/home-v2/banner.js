04b4ec6eac250bd0989c81d8ceb6dd4267ca5c9e
Glitch (smiling-truck) <none>
2019-07-03 21:58:47 +0000

    focus

--- src/presenters/pages/home-v2/banner.js
+++ src/presenters/pages/home-v2/banner.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import classnames from 'classnames';
 
 import Button from 'Components/buttons/button';
-import TransparentButton from 'Components/buttons/transparent-button';
 import { Overlay, OverlaySection, OverlayBackground } from 'Components/overlays';
 import { PopoverContainer } from 'Components/popover';
 import Mark from 'Components/mark';
@@ -61,10 +60,12 @@ const InlineVideo = () => {
         <WistiaVideo videoId="z2ksbcs34d" />
       ) : (
         <>
-          <div className={styles.bannerVideoButtonWrap} onClick={onClick}>
-            <img
+          <div className={styles.bannerVideoButtonWrap}>
+            <img // eslint-disable-line
+              onClick={onClick}
               src="https://cdn.glitch.com/616994fe-f0e3-4501-89a7-295079b3cb8c%2Ftablet_scene_30.jpg?v=1561660860795"
               alt=""
+              aria-hidden="true"
             />
           </div>
           <span className={styles.bannerVideoButton}>
