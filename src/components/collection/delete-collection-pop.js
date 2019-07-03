02424b1c42be9c9885ea0ed8f8a43090de39cecc
Glitch (butter-sunspot) <none>
2019-07-03 18:21:44 +0000

    🎫👋 Checkpoint ./src/components/collection/delete-collection-pop.js:9/2723

--- src/components/collection/delete-collection-pop.js
+++ src/components/collection/delete-collection-pop.js
@@ -5,7 +5,7 @@ import { withRouter } from 'react-router-dom';
 import { getOwnerLink, getLink } from 'Models/collection';
 import Image from 'Components/images/image';
 import Loader from 'Components/loader';
-import { PopoverDialog, PopoverActions, PopoverTitle, MultiPopover, MultiPopoverTitle, ActionDescription, PopoverWithButton, PopoverMenuButton } from 'Components/popover';
+import { PopoverDialog, PopoverActions, PopoverTitle, ActionDescription, PopoverWithButton, PopoverMenuButton } from 'Components/popover';
 import { deleteCollection as deleteCollectionViaState } from 'State/collection';
 import { useNotifications } from 'State/notifications';
 import { useAPI } from 'State/api';
@@ -49,85 +49,6 @@ const DeleteCollectionPop = withRouter(({ history, collection, animateAndDeleteC
     </PopoverDialog>
   );
 });
-/*
-function TeamUserRemovePop({ user, onRemoveUser, userTeamProjects }) {
-  const [selectedProjects, setSelectedProjects] = useState([]);
-  function selectAllProjects() {
-    setSelectedProjects(userTeamProjects.map((p) => p.id));
-  }
-
-  function unselectAllProjects() {
-    setSelectedProjects([]);
-  }
-
-  const allProjectsSelected = userTeamProjects.every((p) => selectedProjects.includes(p.id));
-
-  return (
-    <PopoverDialog align="left" focusOnPopover>
-      <MultiPopoverTitle>Remove {getDisplayName(user)}</MultiPopoverTitle>
-
-      {!userTeamProjects && (
-        <PopoverActions>
-          <Loader />
-        </PopoverActions>
-      )}
-      {userTeamProjects && userTeamProjects.length > 0 && (
-        <PopoverActions>
-          <ActionDescription>Also remove them from these projects</ActionDescription>
-          <ProjectsList options={userTeamProjects} value={selectedProjects} onChange={setSelectedProjects} />
-          {userTeamProjects.length > 1 && allProjectsSelected && (
-            <Button size="small" onClick={unselectAllProjects}>
-              Unselect All
-            </Button>
-          )}
-          {userTeamProjects.length > 1 && !allProjectsSelected && (
-            <Button size="small" onClick={selectAllProjects}>
-              Select All
-            </Button>
-          )}
-        </PopoverActions>
-      )}
-
-      <PopoverActions type="dangerZone">
-        <Button type="dangerZone" onClick={() => onRemoveUser(selectedProjects)}>
-          Remove{' '}
-          <span className={styles.tinyAvatar}>
-            <UserAvatar user={user} />
-          </span>
-        </Button>
-      </PopoverActions>
-    </PopoverDialog>
-  );
-}
-
-    <PopoverContainer>
-      {({ visible, togglePopover, toggleAndCall }) => (
-        <div style={{ position: 'relative' }}>
-          <TransparentButton onClick={togglePopover}>
-            <UserAvatar user={user} suffix={adminStatusDisplay(team.adminIds, user)} withinButton />
-          </TransparentButton>
-
-          {visible && (
-            <MultiPopover
-              views={{
-                remove: () => <TeamUserRemovePop user={user} userTeamProjects={userTeamProjects} onRemoveUser={toggleAndCall(removeUser)} />,
-              }}
-            >
-              {(showViews) => (
-                <TeamUserInfo
-                  user={user}
-                  team={team}
-                  onRemoveAdmin={toggleAndCall(onRemoveAdmin)}
-                  onMakeAdmin={toggleAndCall(onMakeAdmin)}
-                  onRemoveUser={() => onOrShowRemoveUser(showViews.remove, togglePopover)}
-                />
-              )}
-            </MultiPopover>
-          )}
-        </div>
-      )}
-    </PopoverContainer>
-*/
 
 const DeleteCollection = ({ collection, deleteCollection }) => (
   <PopoverWithButton buttonProps={{ size: 'small', type: 'dangerZone', emoji: 'bomb' }} buttonText={`Delete ${collection.name}`}>
