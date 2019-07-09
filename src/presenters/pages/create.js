c4ff99174701ddd5e4999d3688eaf672448e67df
Glitch (steadfast-ticket) <none>
2019-07-09 21:36:26 +0000

    👩🗃 Checkpoint ./src/presenters/pages/create.js:301412/1

--- src/presenters/pages/create.js
+++ src/presenters/pages/create.js
@@ -83,7 +83,7 @@ function WhatIsGlitch() {
         </Heading>
         <div className={classNames(styles.sectionDescription, styles.whatIsGlitchDescription)}>
           <Text size="16px">Glitch is a collaborative programming environment that lives in your browser and deploys code as you type.</Text>
-          <Text size="16px">Use Glitch to build anything from a good ol’ static webpage to fullstack Node apps.</Text>
+          <Text size="16px">Use Glitch to build anything from a good ol’ static webpage to full-stack Node apps.</Text>
         </div>
       </div>
       <div className={styles.whatIsGlitchVideoContainer}>
