709714d7da24ebcbd843f65f36ebb579b760dd39
Glitch (tundra-beat) <none>
2019-07-09 05:58:55 +0000

    🌦🌿 Checkpoint ./src/components/layout/sign-in-layout.js:355867/8494 ./src/components/layout/sign-in-layout.styl:355867/83

--- src/components/layout/sign-in-layout.js
+++ src/components/layout/sign-in-layout.js
@@ -3,12 +3,19 @@ import classNames from 'classnames';
 
 import Link from 'Components/link';
 import Logo from 'Components/header/logo';
+import Text from 'Components/text/text';
 import PasswordLogin from 'Components/sign-in/password-login';
 import SignInButton, { companyNames } from 'Components/buttons/sign-in-button';
 
 import styles from './sign-in-layout.styl';
 
 const SignInButtons = () => companyNames.map((companyName) => <SignInButton companyName={companyName} />);
+
+const TermsAndConditions = () => (
+  <div className={styles.termsAndConditions}>
+    By signing into Glitch, you agree to our <Link to="/legal/#tos">Terms of Services</Link> and <Link to="/legal/#privacy">Privacy Statement</Link>
+  </div>
+);
 
 const SignInLayout = () => (
   <div className={styles.layout}>
@@ -25,7 +32,9 @@ const SignInLayout = () => (
         <div className={styles.content}>
           <PasswordLogin />
           <div>
+            <Text>You can sign in without a password too.</Text>
             <SignInButtons />
+            <TermsAndConditions />
           </div>
         </div>
       </section>
