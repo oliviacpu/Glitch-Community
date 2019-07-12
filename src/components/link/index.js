a3e7ad98b370f05fa3e754751535a491dde3f50b
Glitch (superb-guide) <none>
2019-07-12 16:58:36 +0000

    🖼💸 Checkpoint ./src/components/link/index.js:97325/177

--- src/components/link/index.js
+++ src/components/link/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Link as RouterLink, withRouter } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
 
 import { getLink as getCollectionLink } from 'Models/collection';
 import { getLink as getProjectLink } from 'Models/project';
@@ -14,9 +14,9 @@ export { WrappingLink, TrackedExternalLink };
 
 const external = window.EXTERNAL_ROUTES ? Array.from(window.EXTERNAL_ROUTES) : [];
 
-const Link = withRouter(React.forwardRef(({ to, children, location, ...props }, ref) => {
+const Link = React.forwardRef(({ to, children, ...props }, ref) => {
   if (typeof to === 'string') {
-    const currentUrl = new URL(location.href);
+    const currentUrl = new URL(window.location.href);
     const targetUrl = new URL(to, currentUrl);
 
     if (targetUrl.origin !== currentUrl.origin || external.some((route) => targetUrl.pathname.startsWith(route))) {
@@ -38,7 +38,7 @@ const Link = withRouter(React.forwardRef(({ to, children, location, ...props }, 
       {children}
     </RouterLink>
   );
-}));
+});
 Link.propTypes = {
   to: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
   children: PropTypes.node.isRequired,
