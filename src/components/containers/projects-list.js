00e168e33fc90ac205c79559517d66b3979c6933
Glitch (community-staging) <none>
2019-07-16 14:07:51 +0000

    🌴🍋 Checkpoint ./src/components/containers/projects-list.js:301412/1698

--- src/components/containers/projects-list.js
+++ src/components/containers/projects-list.js
@@ -45,60 +45,6 @@ const ProjectsUL = ({ collection, projects, sortable, onReorder, noteOptions, la
   );
 };
 
-const FilterController = ({ enabled, placeholder, projects, children }) => {
-  const [filter, setFilter] = useState('');
-  const [filteredProjects, setFilteredProjects] = useState([]);
-  const [isDoneFiltering, setIsDoneFiltering] = useState(false);
-
-  const validFilter = filter.length > 1;
-
-  function filterProjects() {
-    setIsDoneFiltering(false);
-    if (validFilter) {
-      const lowercaseFilter = filter.toLowerCase();
-      setFilteredProjects(projects.filter((p) => p.domain.includes(lowercaseFilter) || p.description.toLowerCase().includes(lowercaseFilter)));
-      setIsDoneFiltering(true);
-    } else {
-      setFilteredProjects([]);
-    }
-  }
-
-  useEffect(() => filterProjects(), [projects]);
-  useEffect(() => debounce(filterProjects, 400)(), [filter]);
-
-  const filtering = validFilter && isDoneFiltering;
-  const displayedProjects = filtering ? filteredProjects : projects;
-
-  return children({
-    filterInput: enabled && (
-      <TextInput
-        data-cy="projects-filter"
-        className={styles.headerSearch}
-        name="filter"
-        onChange={setFilter}
-        opaque
-        placeholder="find a project"
-        labelText="project search"
-        type="search"
-        value={filter}
-      />
-    ),
-    renderProjects: (renderFn) => {
-      if (displayedProjects.length) return renderFn(displayedProjects);
-
-      if (filtering) {
-        return (
-          <div className={styles.filterResultsPlaceholder}>
-            <Image alt="" src="https://cdn.glitch.com/c117d5df-3b8d-4389-9e6b-eb049bcefcd6%2Fcompass-not-found.svg?1554146070630" />
-            <Text>No projects found</Text>
-          </div>
-        );
-      }
-      return placeholder;
-    },
-  });
-};
-
 function ProjectsList({
   projects,
   layout,
