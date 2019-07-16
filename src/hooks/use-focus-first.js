e9d2db9076fae53ca8613892674f33c60b34d3e6
Glitch (cassey-community) <none>
2019-07-16 20:44:13 +0000

    🚘🍬 Checkpoint ./src/hooks/use-focus-first.js:708175/829

--- src/hooks/use-focus-first.js
+++ src/hooks/use-focus-first.js
@@ -1,11 +1,14 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 
-export const useFocusFirst = useEffect(() => {
-  const [url, hash] = window.location.href.split('#');
-  if (hash) {
-    const firstHeading  = document.querySelectorAll(`#${hash} > h1:first-of-type, #${hash} > h2:first-of-type`)[0];
-
-    firstHeading.setAttribute('tabIndex', -1);
-    firstHeading.focus();
-  }
-}, []);
+export const useFocusFirst = () => {
+  useEffect(() => {
+    const [url, hash] = window.location.href.split('#');
+    if (hash) {
+      const firstHeading = document.querySelectorAll(`#${hash} h1:first-of-type, #${hash} h2:first-of-type`)[0];
+      if (firstHeading) {
+        firstHeading.setAttribute('tabIndex', -1);
+        firstHeading.focus();
+      }
+    }
+  }, []);
+};
