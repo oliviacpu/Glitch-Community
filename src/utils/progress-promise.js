66cbb3808384e5fa33a89cd6521ff9d73058a3a3
Glitch (nova-chance) <none>
2019-07-17 17:56:16 +0000

    🚊💷 Checkpoint ./src/utils/progress-promise.js:97325/1925

--- src/utils/progress-promise.js
+++ src/utils/progress-promise.js
@@ -1,45 +1,7 @@
-/* eslint-disable no-extend-native, no-underscore-dangle */
+/* eslint-disable no-underscore-dangle */
 
 // Extend promises with `finally`
 // From: https://github.com/domenic/promises-unwrapping/issues/18
-if (Promise.prototype.finally == null) {
-  Promise.prototype.finally = function _finally(callback) {
-    // We don’t invoke the callback in here,
-    // because we want then() to handle its exceptions
-    return this.then(
-      // Callback fulfills: pass on predecessor settlement
-      // Callback rejects: pass on rejection (=omit 2nd arg.)
-      (value) => Promise.resolve(callback()).then(() => value),
-      (reason) =>
-        Promise.resolve(callback()).then(() => {
-          throw reason;
-        }),
-    );
-  };
-}
-
-if (Promise.prototype._notify == null) {
-  Promise.prototype._notify = function _notify(event) {
-    return this._progressHandlers.forEach((handler) => {
-      try {
-        handler(event);
-      } catch (error) {
-        // empty
-      }
-    });
-  };
-}
-
-if (Promise.prototype.progress == null) {
-  Promise.prototype.progress = function progress(handler) {
-    if (this._progressHandlers == null) {
-      this._progressHandlers = [];
-    }
-    this._progressHandlers.push(handler);
-
-    return this;
-  };
-}
 
 function ProgressPromise(fn) {
   const p = new Promise((resolve, reject) => {
@@ -64,6 +26,39 @@ function ProgressPromise(fn) {
     p.progress(result._notify.bind(result));
 
     return result;
+  };
+
+  p.finally = function _finally(callback) {
+    // We don’t invoke the callback in here,
+    // because we want then() to handle its exceptions
+    return this.then(
+      // Callback fulfills: pass on predecessor settlement
+      // Callback rejects: pass on rejection (=omit 2nd arg.)
+      (value) => Promise.resolve(callback()).then(() => value),
+      (reason) =>
+        Promise.resolve(callback()).then(() => {
+          throw reason;
+        }),
+    );
+  };
+
+  p._notify = function _notify(event) {
+    return this._progressHandlers.forEach((handler) => {
+      try {
+        handler(event);
+      } catch (error) {
+        // empty
+      }
+    });
+  };
+
+  p.progress = function progress(handler) {
+    if (this._progressHandlers == null) {
+      this._progressHandlers = [];
+    }
+    this._progressHandlers.push(handler);
+
+    return this;
   };
 
   return p;
