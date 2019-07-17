27704d55e2966d9972aeddcd28bfcf9f0f37b796
Glitch (cassey-community) <none>
2019-07-17 20:13:47 +0000

    👌🏅 Checkpoint ./src/hooks/use-focus-first.js:708175/165 ./src/components/search-results/index.js:708175/48

--- src/components/search-results/index.js
+++ src/components/search-results/index.js
@@ -19,7 +19,7 @@ import NotFound from 'Components/errors/not-found';
 import Loader from 'Components/loader';
 import styles from './search-results.styl';
 
-const FilterContainer = ({ filters, activeFilter, setFilter, query }) => {
+const FilterContainer = ({ filters, activeFilter, setFilter }) => {
   const buttons = filters.map((filter) => ({
     name: filter.id,
     contents: (
@@ -30,11 +30,7 @@ const FilterContainer = ({ filters, activeFilter, setFilter, query }) => {
     ),
   }));
 
-  return (
-    <>
-      <SegmentedButtons value={activeFilter} buttons={buttons} onChange={setFilter} />
-    </>
-  );
+  return <SegmentedButtons value={activeFilter} buttons={buttons} onChange={setFilter} />;
 };
 
 const groups = [
@@ -125,7 +121,7 @@ function SearchResults({ query, searchResults, activeFilter, setActiveFilter }) 
   return (
     <main className={styles.page} id="main">
       {ready && searchResults.totalHits > 0 && (
-        <FilterContainer filters={filters} setFilter={setActiveFilter} activeFilter={activeFilter} query={query} />
+        <FilterContainer filters={filters} setFilter={setActiveFilter} activeFilter={activeFilter} />
       )}
       {activeFilter === 'all' && <h1>All results for {query}</h1>}
       {!ready && <Loader />}
