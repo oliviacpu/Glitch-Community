19b508a62ef776b5b601e4b38e87447b346a230a
Cassey Lottman <>
2019-07-17 14:09:31 -0500

    why

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import Helmet from 'react-helmet';
--- src/hooks/use-focus-first.js
+++ src/hooks/use-focus-first.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 const useFocusFirst = () => {
   React.useEffect(() => {
+    console.log(window.location.href);
     const [, hash] = window.location.href.split('#');
+    console.log(hash);
     if (hash) {
       const firstHeading = document.querySelectorAll(`#${hash} h1:first-of-type, #${hash} h2:first-of-type`)[0];
+      console.log(firstHeading);
       if (firstHeading) {
-        firstHeading.setAttribute('tabIndex', -1);
+        firstHeading.setAttribute('tabindex', -1);
+        firstHeading.setAttribute('style', 'background-color: red');
         firstHeading.focus();
       }
     }
