6a059063ed47752ba599d8b61fe188ebf635feb8
Glitch (cassey-community) <none>
2019-07-18 22:23:05 +0000

    🏰🌶 Checkpoint ./src/components/containers/skip-section-buttons.js:708175/607 ./src/components/buttons/button.js:708175/10196

--- src/components/containers/skip-section-buttons.js
+++ src/components/containers/skip-section-buttons.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import Button from 'Components/buttons/button';
 import styles from './skip-section-buttons.styl';
 import { snakeCase} from 'lodash';
@@ -6,14 +6,23 @@ import { snakeCase} from 'lodash';
 const SkipSectionButtons = ({ children, sectionName }) => {
   const beforeId = `before-${snakeCase(sectionName)}`;
   const afterId = `after-${snakeCase(sectionName)}`;
+  const beforeRef = useRef();
+  const afterRef = useRef();
+  const moveFocusToAfter = () => {
+    afterRef.current.focus();
+  }
+  
+  const moveFocusToBefore = () => {
+    beforeRef.current.focus();
+  }
   
   return (
     <>
-      <Button href={`#${afterId}`} id={beforeId} className={styles.visibleOnFocus}>
+      <Button ref={beforeRef} onClick={moveFocusToAfter} id={beforeId} className={styles.visibleOnFocus}>
         Skip to After {sectionName}
       </Button>
       {children}
-      <Button href={`#${beforeId}`} id={afterId} className={styles.visibleOnFocus}>
+      <Button ref={afterRef} onClick={moveFocusToBefore} id={afterId} className={styles.visibleOnFocus}>
         Skip to Before {sectionName}
       </Button>
     </>
