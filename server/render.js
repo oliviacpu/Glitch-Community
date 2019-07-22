22b9f9b083b2abb760cfd8c9dd6de8117d7e7e25
Glitch (fallacious-slice) <none>
2019-07-22 15:57:38 +0000

    👢👋 Checkpoint ./server/render.js:97325/747

--- server/render.js
+++ server/render.js
@@ -28,14 +28,11 @@ const { GlobalsProvider } = require('State/globals');
 
 const { default: App } = require('../src/app');
 
-const render = ({ url, EXTERNAL_ROUTES, HOME_CONTENT, ZINE_POSTS }) => {
-  return ReactDOMServer.renderToString(
-    React.createElement(StaticRouter, { location: url.pathname + url.search + url.hash },
-      React.createElement(GlobalsProvider, { origin: url.origin, ZINE_POSTS, HOME_CONTENT, EXTERNAL_ROUTES },
-        React.createElement(App),
-      ),
-    )
-  );
+const render = async ({ url, EXTERNAL_ROUTES, HOME_CONTENT, ZINE_POSTS }) => {
+  const app = React.createElement(App);
+  const globalsProvider = React.createElement(GlobalsProvider, { origin: url.origin, ZINE_POSTS, HOME_CONTENT, EXTERNAL_ROUTES }, app);
+  const root = React.createElement(StaticRouter, { location: url.pathname + url.search + url.hash }, globalsProvider);
+  return ReactDOMServer.renderToString(root);
 };
 
 module.exports = render;
