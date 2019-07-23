d0ccd89e88b19a8b3a510859eb114574bd63965f
Glitch (community-staging) <none>
2019-07-23 14:47:09 +0000

    🖖🏛 Checkpoint ./src/components/project/add-project-to-collection-pop.js:433767/52

--- src/components/project/add-project-to-collection-pop.js
+++ src/components/project/add-project-to-collection-pop.js
@@ -97,7 +97,6 @@ const NoResults = ({ project, collectionsWithProject, query }) => {
   return <InfoDescription>Create collections to organize your favorite projects.</InfoDescription>;
 };
 
-// if dev toggle mystuff, add mystuff to collection
 function useCollectionSearch(query, project, collectionType) {
   const { currentUser } = useCurrentUser();
   const debouncedQuery = useDebouncedValue(query, 200);
