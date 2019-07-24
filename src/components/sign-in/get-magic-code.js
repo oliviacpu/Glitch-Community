ab76560dc7f74aeece991703caba3184fcd00eed
Glitch (tundra-beat) <none>
2019-07-24 18:15:37 +0000

    🚐👩 Checkpoint ./src/components/sign-in/get-magic-code.js:355867/119 ./src/components/sign-in-pop/index.js:355867/60

--- src/components/sign-in/get-magic-code.js
+++ src/components/sign-in/get-magic-code.js
@@ -1,12 +1,10 @@
-import React, { useState, useMemo } from 'react';
-import { parseOneAddress } from 'email-addresses';
+import React, { useState } from 'react';
 
 import Text from 'Components/text/text';
 import Button from 'Components/buttons/button';
 import TextInput from 'Components/inputs/text-input';
 import Notification from 'Components/notification';
 import Loader from 'Components/loader';
-import useDebouncedValue from 'Hooks/use-debounced-value';
 import useEmail from 'Hooks/use-email';
 import { useAPI } from 'State/api';
 import { captureException } from 'Utils/sentry';
