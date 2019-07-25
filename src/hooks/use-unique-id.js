872f2241af0bdb2a572d5a2af8b218ce5b4671d1
Glitch (nova-chance) <none>
2019-07-25 15:06:51 +0000

    🏐🚉 Checkpoint ./src/hooks/use-unique-id.js:97325/581

--- src/hooks/use-unique-id.js
+++ src/hooks/use-unique-id.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
+
+const createCounter = () => {
+  let counter = 0;
+  return () => {
+    counter += 1;
+    return counter;
+  }
+};
+
+const Context = createContext(createCounter());
 
 let counter = 0;
 
 const useUniqueId = () => {
-  const [uniqueId] = React.useState(() => {
-    counter += 1;
-    return counter;
-  });
+  const getCounter = useContext(Context);
+  const [uniqueId] = useState(getCounter);
   return `unique-${uniqueId}`;
+};
+
+const UniqueIdProvider = ({ children }) => {
+  const getCounter = useState(createCounter);
+  return <Context.Provider value={getCounter}>{children}
 };
 
 export default useUniqueId;
