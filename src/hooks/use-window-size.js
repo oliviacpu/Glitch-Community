b3b068b85b115ac2676bab573368112775c70fed
Glitch (nova-chance) <none>
2019-07-26 15:23:43 +0000

    🐆😬 Checkpoint

--- src/hooks/use-window-size.js
+++ src/hooks/use-window-size.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 const useWindowSize = () => {
-  const [windowSize, setWindowSize] = useState([800, 600]);
+  const [windowSize, setWindowSize] = useState([0, 0]);
   useEffect(() => {
     const handleResize = () => setWindowSize([window.innerWidth, window.innerHeight]);
     window.addEventListener('resize', handleResize);
