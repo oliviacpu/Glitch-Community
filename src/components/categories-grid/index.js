0ec21beb0c47afde61cff23b3a6f18f035642dce
Glitch (community-staging) <none>
2019-08-07 18:08:55 +0000

    🏪🙏 Checkpoint ./src/components/categories-grid/index.js:301412/4194

--- src/components/categories-grid/index.js
+++ src/components/categories-grid/index.js
@@ -5,61 +5,62 @@ import { CDN_URL } from 'Utils/constants';
 
 import styles from './styles.styl';
 
-const categories = {
-  games: {
+const allCategories = [
+  {
     name: 'Games',
     color: '#fae3d1',
-    path: '/games',
+    path: 'games',
     icon: `${CDN_URL}/50f784d9-9995-4fa4-a185-b4b1ea6e77c0%2Ftetris.svg`,
   },
-  handyBots: {
+  {
     name: 'Bots',
     color: '#c7bff0',
-    path: '/handy-bots',
+    path: 'handy-bots',
     icon: `${CDN_URL}/50f784d9-9995-4fa4-a185-b4b1ea6e77c0%2Fbot.svg`,
   },
-  music: {
+  {
     name: 'Music',
     color: '#a9c4f7',
-    path: '/music',
+    path: 'music',
     icon: `${CDN_URL}/50f784d9-9995-4fa4-a185-b4b1ea6e77c0%2Fmusic.svg`,
   },
-  art: {
+  {
     name: 'Art',
     color: '#f2a7bb',
-    path: '/art',
+    path: 'art',
     icon: `${CDN_URL}/50f784d9-9995-4fa4-a185-b4b1ea6e77c0%2Fart.svg`,
   },
-  productivity: {
+  {
     name: 'Productivity',
     color: '#7aa4d3',
-    path: '/tools-for-work',
+    path: 'tools-for-work',
     icon: `${CDN_URL}/50f784d9-9995-4fa4-a185-b4b1ea6e77c0%2Fwork.svg`,
   },
-  hardware: {
+  {
     name: 'Hardware',
     color: '#6cd8a9',
     path: '/hardware',
     icon: `${CDN_URL}/50f784d9-9995-4fa4-a185-b4b1ea6e77c0%2Fhardware.svg`,
   },
-  buildingBlocks: {
+  {
     name: 'Building Blocks',
     color: '#65cad2',
-    path: '/building-blocks',
+    path: 'building-blocks',
     icon: `${CDN_URL}/50f784d9-9995-4fa4-a185-b4b1ea6e77c0%2Fbuilding-blocks.svg?v=1561575219123`,
   },
-  learnToCode: {
+  {
     name: 'Learn to Code',
     color: '#f8d3c8',
-    path: '/learn-to-code',
+    path: 'learn-to-code',
     icon: `${CDN_URL}/50f784d9-9995-4fa4-a185-b4b1ea6e77c0%2Flearn.svg?v=1561575404279`,
   },
-};
+];
 
 function CategoriesGrid({ categories }) {
+  const categoriesToRender = useMemo(() => allCategories.filter(() => categories === 'all' || ))
   return (
     <ul className={styles.categoriesGrid}>
-      {categories.map((category) => (
+      {categories.filter(categories === 'all' map((category) => (
         <li key={category.path} className={styles.categoriesGridItem} style={{ '--bg-color': category.color }}>
           <Link to={category.url}>
             <Image src={category.icon} alt="" />
