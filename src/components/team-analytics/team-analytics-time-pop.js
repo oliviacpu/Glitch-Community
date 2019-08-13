a968eef9d5e7deb50ca25f0e60e443e65495866f
Glitch (community-staging) <none>
2019-08-13 20:52:51 +0000

    now its a select

--- src/components/team-analytics/team-analytics-time-pop.js
+++ src/components/team-analytics/team-analytics-time-pop.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'Components/buttons/button';
+import popoverStyles from 'Components/popover/styles.styl';
 import styles from './styles.styl';
-import popoverStyles from 'Components/popover/'
 
 const timeFrames = ['Last 4 Weeks', 'Last 2 Weeks', 'Last 24 Hours'];
-const getTimeFrameId = (timeFrame) => `team-analytics-timeframe-${timeFrame.toLowerCase().replace(/\s/g, '-')}`;
 
 const TeamAnalyticsTimePopButton = ({ updateTimeFrame, currentTimeFrame }) => (
-  <Button decorative size="small" type="tertiary" emoji="downArrow">
-    <select className={styles.timeFrameSelect} value={currentTimeFrame} onChange={(e) => updateTimeFrame(e.target.value)}>
-      {timeFrames.map(t => (
-        <option key={t} value={t}>{t}</option>
-      ))}
-      <span className={popoverStyles.arrowPadding}>
-        <span className={popoverStyles.downArrow} />
-      </span>
-    </select>
-  </Button>
+  <div className={styles.timeFramePopWrap}>
+    <Button decorative size="small" type="tertiary">
+      <select className={styles.timeFrameSelect} value={currentTimeFrame} onChange={(e) => updateTimeFrame(e.target.value)}>
+        {timeFrames.map((t) => (
+          <option key={t} value={t}>{t}</option>
+        ))}
+      </select>
+      <span className={`${popoverStyles.downArrow} ${styles.downArrow}`} />
+    </Button>
+  </div>
 );
 
 TeamAnalyticsTimePopButton.propTypes = {
