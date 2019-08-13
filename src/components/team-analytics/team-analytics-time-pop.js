4c8888bc6abf9a3d50be789c401c8e0c0c2cb574
Glitch (community-staging) <none>
2019-08-13 20:44:03 +0000

    🏫🛢 Checkpoint ./src/components/team-analytics/team-analytics-time-pop.js:271885/6740 ./src/components/team-analytics/styles.styl:271885/420

--- src/components/team-analytics/team-analytics-time-pop.js
+++ src/components/team-analytics/team-analytics-time-pop.js
@@ -1,69 +1,23 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { PopoverWithButton, PopoverDialog, PopoverSection } from 'Components/popover';
-import TransparentButton from 'Components/buttons/transparent-button';
+import Button from 'Components/buttons/button';
 import styles from './styles.styl';
+import popoverStyles from 'Components/popover/'
 
 const timeFrames = ['Last 4 Weeks', 'Last 2 Weeks', 'Last 24 Hours'];
 const getTimeFrameId = (timeFrame) => `team-analytics-timeframe-${timeFrame.toLowerCase().replace(/\s/g, '-')}`;
 
-const TeamAnalyticsTimePop = ({ currentTimeFrame, updateTimeFrame }) => {
-  const [selected, setSelected] = useState(currentTimeFrame);
-  const refs = useRef({});
-  useEffect(() => {
-    refs.current[selected].focus();
-  }, [selected]);
-
-  const onKeyDown = (e) => {
-    if (e.key === 'ArrowDown') {
-      e.preventDefault();
-      const nextIndex = (timeFrames.indexOf(selected) + 1) % timeFrames.length;
-      setSelected(timeFrames[nextIndex]);
-    } else if (e.key === 'ArrowUp') {
-      e.preventDefault();
-      const nextIndex = (timeFrames.indexOf(selected) + timeFrames.length - 1) % timeFrames.length;
-      setSelected(timeFrames[nextIndex]);
-    }
-  };
-
-  return (
-    <PopoverDialog align="left">
-      <PopoverSection
-        role="listbox"
-        aria-label="Team analytics timeframe"
-        aria-activedescendant={getTimeFrameId(selected)}
-      >
-        {timeFrames.map((timeFrame, index) => (
-          <TransparentButton
-            key={timeFrame}
-            id={getTimeFrameId(timeFrame)}
-            ref={(el) => { refs.current[timeFrame] = el; }}
-            tabIndex={timeFrame === selected ? 0 : -1}
-            onKeyDown={(e) => onKeyDown(e, index)}
-            onClick={() => updateTimeFrame(timeFrame)}
-            className={styles.timeFrameButton}
-            role="option"
-            aria-selected={timeFrame === selected}
-          >
-            {timeFrame}
-          </TransparentButton>
-        ))}
-      </PopoverSection>
-    </PopoverDialog>
-  );
-};
-
 const TeamAnalyticsTimePopButton = ({ updateTimeFrame, currentTimeFrame }) => (
-  <PopoverWithButton
-    buttonProps={{ size: 'small', type: 'tertiary' }}
-    buttonText={
-      <>
-        {currentTimeFrame} <div className="down-arrow" aria-label="options" />
-      </>
-    }
-  >
-    {({ toggleAndCall }) => <TeamAnalyticsTimePop updateTimeFrame={toggleAndCall(updateTimeFrame)} currentTimeFrame={currentTimeFrame} />}
-  </PopoverWithButton>
+  <Button decorative size="small" type="tertiary" emoji="downArrow">
+    <select className={styles.timeFrameSelect} value={currentTimeFrame} onChange={(e) => updateTimeFrame(e.target.value)}>
+      {timeFrames.map(t => (
+        <option key={t} value={t}>{t}</option>
+      ))}
+      <span className={popoverStyles.arrowPadding}>
+        <span className={popoverStyles.downArrow} />
+      </span>
+    </select>
+  </Button>
 );
 
 TeamAnalyticsTimePopButton.propTypes = {
