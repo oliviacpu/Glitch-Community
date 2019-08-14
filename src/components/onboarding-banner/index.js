4cad09ed1008b59573ac84d898b3f1c2445194e4
Glitch (community-staging) <none>
2019-08-14 05:44:54 +0000

    👩🍨 Checkpoint ./src/components/onboarding-banner/index.js:301412/443 ./src/components/onboarding-banner/styles.styl:301412/131

--- src/components/onboarding-banner/index.js
+++ src/components/onboarding-banner/index.js
@@ -3,12 +3,15 @@ import Image from 'Components/images/image';
 import Text from 'Components/text/text';
 import Link from 'Components/link';
 import NewProjectPop from 'Components/header/new-project-pop';
+import { getUserProfileStyle } from 'Models/user';
+import { useCurrentUser } from 'State/current-user';
 
 import styles from './styles.styl';
 
 function OnboardingBanner () {
+  const user = useCurrentUser();
   return (
-    <div className={styles.banner}>
+    <div className={styles.banner} style={getUserProfileStyle(user)}>
       <div className={styles.illustration}>
         <h1>Welcome to Glitch!</h1>
         <Image src='https://cdn.glitch.com/02ae6077-549b-429d-85bc-682e0e3ced5c%2Fcollaborate.svg?v=1540583258925' alt="" />
