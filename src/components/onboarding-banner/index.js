a4801d21dc67d1c305af53cd2e8745ec0fd52871
Glitch (community-staging) <none>
2019-08-14 05:34:35 +0000

    🌸🏫 Checkpoint ./src/components/onboarding-banner/index.js:301412/701 ./src/components/onboarding-banner/styles.styl:301412/148

--- src/components/onboarding-banner/index.js
+++ src/components/onboarding-banner/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import Image from 'Components/images/image';
+import Text from 'Components/text/text';
+import Link from 'Components/link';
+import NewProjectPop from 'Components/header/new-project-pop';
 
 import styles from './styles.styl';
 
-function OnboardingBanner ({}) {
+function OnboardingBanner () {
   return (
     <div className={styles.banner}>
-      <div>
-        Welcome to Glitch!
-        <Image src=
+      <div className={styles.illustration}>
+        <h1>Welcome to Glitch!</h1>
+        <Image src='https://cdn.glitch.com/02ae6077-549b-429d-85bc-682e0e3ced5c%2Fcollaborate.svg?v=1540583258925' alt="" />
+      </div>
+      
+      <div className={styles.actions}>
+        <div>
+          <h2>Create your first project</h2>
+          <Text>Jump into the editor by creating your very own app.</Text>
+          <NewProjectPop />
+          <Link to="/create">Learn about creating on Glitch</Link>
+        </div>
       </div>
     </div>
   );
