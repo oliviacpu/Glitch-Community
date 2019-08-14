006451efa005055fd42832c4dad42824a4411c96
Glitch (community-staging) <none>
2019-08-14 14:58:03 +0000

    Fix take me there link on first creation of my stuff

--- src/state/collection.js
+++ src/state/collection.js
@@ -30,7 +30,7 @@ export const toggleBookmark = async ({
         myStuffCollection = await createCollection({ api, name: 'My Stuff', createNotification, myStuffEnabled });
       }
       await addProjectToCollection({ project, collection: myStuffCollection });
-      const url = myStuffCollection.fullUrl || `${currentUser.login}/${myStuff.url}`
+      const url = myStuffCollection.fullUrl || `${currentUser.login}/${myStuffCollection.url}`;
       createNotification(
         <AddProjectToCollectionMsg projectDomain={project.domain} collectionName="My Stuff" url={`/@${url}`} />,
         { type: 'success' },
