3c442ee2d874e73d7a5dbe3fa8c1373eb8b4d252
Cassey Lottman <clottman@users.noreply.github.com>
2019-08-22 10:07:30 -0500

    Add react axe to community (#841)
    * 💳🛎 Checkpoint
    ./package.json:708175/8235
    ./server/server.js:708175/426
    ./src/client.js:708175/3900
    ./src/components/profile-list/index.js:708175/1
    
    * 🍜🏤 Checkpoint
    ./src/client.js:708175/8219
    ./src/app.js:708175/129
    
    * 🎊🍉 Checkpoint
    ./src/client.js:708175/53
    ./src/app.js:708175/63
    
    * attempts
    
    * attempts
    
    * 🐈💐 Checkpoint
    
    * 📺🌿 Checkpoint
    ./src/client.js:708175/66
    
    * 🏪🍚 Checkpoint
    ./src/components/profile-list/index.js:708175/12
    ./src/client.js:708175/126

--- src/client.js
+++ src/client.js
@@ -5,7 +5,7 @@ import dayjs from 'dayjs';
 import relativeTimePlugin from 'dayjs/plugin/relativeTime';
 
 import React from 'react';
-import { hydrate, render } from 'react-dom';
+import ReactDOM, { hydrate, render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import convertPlugin from 'Shared/dayjs-convert';
@@ -19,6 +19,7 @@ dayjs.extend(convertPlugin);
 
 // This function is used in index.ejs to set up the app
 window.bootstrap = (container) => {
+  console.log('called bootstrap');
   if (location.hash.startsWith('#!/')) {
     window.location.replace(EDITOR_URL + window.location.hash);
     return;
@@ -45,7 +46,19 @@ window.bootstrap = (container) => {
     </BrowserRouter>
   );
 
-  if (container.hasChildNodes()) {
+  if (window.ENVIRONMENT !== 'production') {
+    import('react-axe').then(({ default: axe }) => {
+      if (!window.axeInitialized) {
+        axe(React, ReactDOM, 1000);
+        window.axeInitialized = true;
+      }
+      if (container.hasChildNodes()) {
+        hydrate(element, container);
+      } else {
+        render(element, container);
+      }
+    });
+  } else if (container.hasChildNodes()) {
     hydrate(element, container);
   } else {
     render(element, container);
