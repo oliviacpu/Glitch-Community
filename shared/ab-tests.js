c6cb1f250ce21199e3a667e83e28b796812407c2
Glitch (profuse-tea) <none>
2019-08-26 18:09:23 +0000

    🎀🌜 Checkpoint ./shared/ab-tests.js:97325/522

--- shared/ab-tests.js
+++ shared/ab-tests.js
@@ -1,9 +1,11 @@
 const COOKIE_NAME = 'ab-tests';
 
 // a test group's weight determines how likely someone is to be placed in it
-// the exact probability for a group is weight / sum of all weights
+// the exact probability for a group is its weight / the sum of all weights
 // two groups with the same weight will be assigned roughly the same number of users
-// 
+
+// the value is sent into the react code in response to useTestValue(name)
+// the type doesn't matter, so it can a set of flags, alternate text, etc
 
 const tests = {
   'Just-A-Test': {
