a043578fa7371507049c3c39c3261e997334fb93
Glitch (community-staging) <none>
2019-08-27 15:30:05 +0000

    🐛👖 Checkpoint ./state/store.js:271885/1427

--- state/store.js
+++ state/store.js
@@ -1,6 +1,30 @@
 import { configureStore } from 'redux-starter-kit'
 import { Provider } from 'react-redux'
 
+
+const createHandlerMiddleware = (...handlerGroups) => {
+  const combinedHandlers = {}
+  for (const handlerGroup of handlerGroups) {
+    for (const [actionType, handler] of Object.entries(handlerGroup)) {
+      if (combinedHandlers[actionType]) {
+        combinedHandlers[actionType].push(handler)
+      } else {
+        combinedHandlers[actionType] = [handler]
+      }
+    }
+  }
+  
+  return (store) => (next) => (action) => {
+    const actionProcessedByMiddleware = next(action)
+    if (!actionProcessedByMiddleware) return
+    
+    const handlersForAction = combinedHandlers[actionProcessedByMiddleware.type] 
+    if (!handlersForAction) return
+    handlersForAction.forEach(handler => handler(actionProcessedByMiddleware, store))
+    return actionProcessedByMiddleware
+  }
+} 
+
 const store = configureStore({
   reducer: (state = {}, action) => state,
 })
