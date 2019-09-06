3efa69b44c3b666dcb395550ccccdf5a89fb04d1
Glitch (profuse-tea) <none>
2019-09-06 19:13:30 +0000

    ⛳️🚥 Checkpoint ./shared/aliases.js:97325/68

--- shared/aliases.js
+++ shared/aliases.js
@@ -1,16 +1,16 @@
 const path = require('path');
 
 const makeAliases = (SRC) => ({
-  Components: path.resolve(SRC, 'components'),
-  Utils: path.resolve(SRC, 'utils'),
-  Curated: path.resolve(SRC, 'curated'),
-  Models: path.resolve(SRC, 'models'),
-  State: path.resolve(SRC, 'state'),
-  Hooks: path.resolve(SRC, 'hooks'),
+  Components: path.resolve(__dirname, SRC + 'components'),
+  Utils: path.resolve(__dirname, SRC + 'utils'),
+  Curated: path.resolve(__dirname, SRC + 'curated'),
+  Models: path.resolve(SRC + 'models'),
+  State: path.resolve(SRC + 'state'),
+  Hooks: path.resolve(SRC + 'hooks'),
   Shared: path.resolve(__dirname),
 });
 
-const clientPath = path.resolve(__dirname, '../src/');
+const clientPath = path.resolve('../src/');
 const builtPath = path.resolve(__dirname, '../build/node/');
 
 let serverPath = clientPath;
