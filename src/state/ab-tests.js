ef4dfa2503b7c697052054d08a988d97c56bef85
Glitch (quixotic-rocket) <none>
2019-09-12 17:22:45 +0000

    🐫🎈 Checkpoint ./src/state/ab-tests.js:97325/2385

--- src/state/ab-tests.js
+++ src/state/ab-tests.js
@@ -1,19 +1,31 @@
+import React, { createContext, useState } from 'react';
+import PropTypes from 'prop-types';
+
 import { COOKIE_NAME, tests } from 'Shared/ab-tests';
-import { useGlobals } from 'State/globals';
+
+const Context = createContext();
+
+const AbTestProvider = ({ AB_TESTS, children }) => {
+  const [assignments, setAssignments] = useState(AB_TESTS);
+  return <Context.Provider value={assignments}>{children}</Context.Provider>;
+};
+AbTestProvider.propTypes = {
+  AB_TESTS: PropTypes.object.isRequired,
+  children: PropTypes.node.isRequired,
+};
 
 const useTestAssignments = () => {
   const { AB_TESTS } = useGlobals();
-  return AB_TESTS;
+  const setAssignment = (name, group) => {
+    document.cookie = `${COOKIE_NAME}=${JSON.stringify({ ...AB_TESTS, [name]: group })}`;
+    window.location.reload();
+  };
+  return [AB_TESTS, setAssignment];
 };
 
 const useTestValue = (name) => {
-  const assignment = useTestAssignments()[name];
-  return tests[name][assignment].value;
-};
-
-const resetTests = () => {
-  document.cookie = `${COOKIE_NAME}=; expires=${new Date()}`;
+  const [assignments] = useTestAssignments();
+  return tests[name][assignments[name]].value;
 };
 
 export default useTestValue;
-export { resetTests };
